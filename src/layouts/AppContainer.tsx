5b56422fdbc198a9bfe77bcfb0f0750bdbc8cfcf
Tibet Sprague <tibet.sprague@gmail.com>
2018-02-08 14:14:00 -0800

    Fix route to CreateProposalContainer.tsx

--- src/layouts/AppContainer.tsx
+++ src/layouts/AppContainer.tsx
@@ -60,7 +60,7 @@ class AppContainer extends React.Component<IProps, null> {
               <Route exact path="/" component={HomeContainer}/>
               <Route exact path="/dao/create" component={CreateDaoContainer}/>
               <Route path="/dao/:daoAddress" component={ViewDaoContainer}/>
-              <Route path="/proposition/create/:daoAddress" component={CreateProposalContainer}/>
+              <Route path="/proposal/create/:daoAddress" component={CreateProposalContainer}/>
             </Switch>
           </div>
           <div className={css.background}></div>
