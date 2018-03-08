33a6ed647f7b454f83d157ac2b3aa16ab7486886
Tibet Sprague <tibet.sprague@gmail.com>
2018-03-07 19:12:47 -0800

    Exclude node_modules folder from typescript loader

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -40,7 +40,8 @@ module.exports = {
       // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
       {
         test: /\.tsx?$/,
-        loader: ['react-hot-loader/webpack', "awesome-typescript-loader" ]
+        loader: ['react-hot-loader/webpack', "awesome-typescript-loader" ],
+        exclude: /node_modules/
       },
 
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
