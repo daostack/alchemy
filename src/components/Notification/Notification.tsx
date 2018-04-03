3605d94bd90d03232a79590508922434ea73e6c4
Matan Tsuberi <dev.matan.tsuberi>
2018-04-03 16:46:36 +0300

    intergate styling into notification component

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -2,6 +2,7 @@ import * as moment from "moment";
 import * as React from "react";
 import * as css from "./Notification.scss";
 import { IOperation, OperationsStatus } from "reducers/operations";
+import classNames = require("classnames");
 
 interface IProps {
   operation: IOperation;
@@ -9,36 +10,60 @@ interface IProps {
   close: () => any;
 }
 
-export default class Notification extends React.Component<IProps, null> {
+export default class Notification extends React.Component<IProps, { minimized: boolean }> {
+  constructor(props: any) {
+    super(props);
+    this.state = { minimized: false };
+  }
+
   public handleClose = (e: any) => {
-    const {close} = this.props;
+    const { close } = this.props;
     close();
   }
 
   public render() {
     const { operation } = this.props;
     const { message, status, totalSteps, timestamp, step } = operation;
+    const { minimized } = this.state;
+
+    const transactionClass = classNames({
+      [css.pendingTransaction]: true,
+      [css.clearfix]: true,
+      [css.pending]: status === OperationsStatus.Pending,
+      [css.error]: status === OperationsStatus.Failure,
+      [css.success]: status === OperationsStatus.Success,
+      [css.minimized]: status === OperationsStatus.Pending && minimized,
+    });
 
     return (
-      <div className={css.notification}>
-        <span className={css.message}>{message}</span>
-        <br/>
-        <span className={css.message}>status: {status}</span>
-        <br/>
-        { totalSteps ?
-          (totalSteps === 1 ?
-            '' :
-            <span>step {step} out of {totalSteps}</span>
-          ) :
-          <span>step {step}</span> }
-        <br/>
-        <small className={css.timestamp}>{moment(timestamp).fromNow()}</small>
-        { operation.status !== OperationsStatus.Pending ? <img
-          src="/assets/images/Icon/Close.svg"
-          alt="close"
-          className={css.close}
-          onClick={this.handleClose}/>
-          : ''}
+      <div className={transactionClass} onClick={() => status === OperationsStatus.Pending && minimized ? this.setState({minimized: false}) : null }>
+        <div className={css.statusIcon}>
+          <img className={css.pending} src="/assets/images/Icon/Loading-white.svg" />
+          <img className={css.success} src="/assets/images/Icon/Success-notification.svg" />
+          <img className={css.error} src="/assets/images/Icon/Error-notification.svg" />
+        </div>
+        <div className={css.transactionMessage}>
+          <div className={css.clearfix}>
+            <div className={css.left}>
+              <span className={css.pending}>PENDING TRANSACTION</span>
+              <span className={css.success}>TRANSACTION SUCCESSFUL</span>
+              <span className={css.error}>TRANSACTION FAILED</span>
+            </div>
+            <div className={css.right}>
+              <span className={css.pending}>{totalSteps !== 1 ? `Step ${step}` : ''} {totalSteps && totalSteps !== 1 ? `OF ${totalSteps}` : ''}</span>
+              <span className={css.success}>{totalSteps !== 1 ? `Step ${step}` : ''} {totalSteps && totalSteps !== 1 ? `OF ${totalSteps}` : ''}</span>
+              <span className={css.error}>ERROR</span>
+            </div>
+          </div>
+          <div className={css.notificationMessage}>
+            <span>{message}</span>
+          </div>
+        </div>
+        <div className={css.notificationControls}>
+          <button className={css.pending} onClick={() => this.setState({minimized: true})}><img src="/assets/images/Icon/Minimize-notification.svg" /></button>
+          <button className={css.success} onClick={this.handleClose}><img src="/assets/images/Icon/Close.svg" /></button>
+          <button className={css.error} onClick={this.handleClose}><img src="/assets/images/Icon/Close.svg" /></button>
+        </div>
       </div>
     );
   }
--- src/layouts/AppContainer.tsx
+++ src/layouts/AppContainer.tsx
@@ -15,11 +15,11 @@ import * as arcActions from "actions/arcActions";
 import * as operationsActions from 'actions/operationsActions';
 
 import CreateDaoContainer from "components/CreateDao/CreateDaoContainer";
+import Notification from "components/Notification/Notification";
 import CreateProposalContainer from "components/CreateProposal/CreateProposalContainer";
 import NoEthAccountContainer from "components/Errors/NoEthAccountContainer";
 import NoWeb3Container from "components/Errors/NoWeb3Container";
 import HomeContainer from "components/Home/HomeContainer";
-import Notification from "components/Notification/Notification";
 import ViewDaoContainer from "components/ViewDao/ViewDaoContainer";
 import HeaderContainer from "layouts/HeaderContainer";
 
@@ -64,7 +64,7 @@ class AppContainer extends React.Component<IProps, null> {
   }
 
   public async componentDidMount() {
-    const { initializeWeb3, loadCachedState} = this.props;
+    const { initializeWeb3, loadCachedState } = this.props;
     initializeWeb3();
 
     // If not using local testnet then load cached blockchain data from S3
@@ -82,7 +82,7 @@ class AppContainer extends React.Component<IProps, null> {
         clearInterval(this.accountInterval);
       }
 
-      this.accountInterval = setInterval(async function(accountAddress: string) {
+      this.accountInterval = setInterval(async function (accountAddress: string) {
         const newAccount = await Arc.Utils.getDefaultAccount();
         if (newAccount !== accountAddress) {
           this.props.changeAccount(newAccount);
@@ -97,157 +97,166 @@ class AppContainer extends React.Component<IProps, null> {
     return (
       (connectionStatus === ConnectionStatus.Pending
         ? <div className={css.loading}>Loading...</div>
-      : connectionStatus === ConnectionStatus.Failed
-        ? <NoWeb3Container />
-      : ethAccountAddress === null
-        ? <NoEthAccountContainer />
-      : connectionStatus == ConnectionStatus.Connected
-        ? <div className={css.outer}>
-            <div className={css.container}>
-              <Route path="/dao/:daoAddress" children={(props) => (
-                <HeaderContainer daoAddress={props.match ? props.match.params.daoAddress : null} />
-              )} />
-              <Switch>
-                <Route exact path="/" component={HomeContainer}/>
-                <Route exact path="/dao/create" component={CreateDaoContainer}/>
-                <Route path="/dao/:daoAddress" component={ViewDaoContainer}/>
-                <Route exact path="/proposal/create/:daoAddress" component={CreateProposalContainer}/>
-              </Switch>
-            </div>
-            <div className={css.pendingTransactions}>
-              <div className={css.transactionWrapper}>
-              <div className={css.pendingTransaction + " " + css.clearfix + " " + css.pending + " " + css.minimized}>
-                <div className={css.statusIcon}> 
-                  <img className={css.pending} src="/assets/images/Icon/Loading-white.svg"/>
-                  <img className={css.success} src="/assets/images/Icon/Success-notification.svg"/>
-                  <img className={css.error} src="/assets/images/Icon/Error-notification.svg"/>
+        : connectionStatus === ConnectionStatus.Failed
+          ? <NoWeb3Container />
+          : ethAccountAddress === null
+            ? <NoEthAccountContainer />
+            : connectionStatus == ConnectionStatus.Connected
+              ? <div className={css.outer}>
+                <div className={css.container}>
+                  <Route path="/dao/:daoAddress" children={(props) => (
+                    <HeaderContainer daoAddress={props.match ? props.match.params.daoAddress : null} />
+                  )} />
+                  <Switch>
+                    <Route exact path="/" component={HomeContainer} />
+                    <Route exact path="/dao/create" component={CreateDaoContainer} />
+                    <Route path="/dao/:daoAddress" component={ViewDaoContainer} />
+                    <Route exact path="/proposal/create/:daoAddress" component={CreateProposalContainer} />
+                  </Switch>
                 </div>
-                <div className={css.transactionMessage}>
-                  <div className={css.clearfix}>
-                    <div className={css.left}>
-                      <span className={css.pending}>PENDING TRANSACTION</span>
-                      <span className={css.success}>TRANSACTION SUCCESSFUL</span>
-                      <span className={css.error}>TRANSACTION FAILED</span>
+                <div className={css.pendingTransactions}>
+                  {Object.keys(operations).map((k) =>
+                    <div key={k}>
+                      <Notification
+                        operation={operations[k]}
+                        close={() => dismissOperation(k)}
+                      />
+                      <br />
                     </div>
-                    <div className={css.right}>
-                      <span className={css.pending}>1 OF 1</span>
-                      <span className={css.success}>1 OF 1</span>
-                      <span className={css.error}>ERROR</span>
+
+                  )}
+
+                  {/* <div className={css.pendingTransaction + " " + css.clearfix + " " + css.error}>
+                    <div className={css.statusIcon}>
+                      <img className={css.pending} src="/assets/images/Icon/Loading-white.svg" />
+                      <img className={css.success} src="/assets/images/Icon/Success-notification.svg" />
+                      <img className={css.error} src="/assets/images/Icon/Error-notification.svg" />
+                    </div>
+                    <div className={css.transactionMessage}>
+                      <div className={css.clearfix}>
+                        <div className={css.left}>
+                          <span className={css.pending}>PENDING TRANSACTION</span>
+                          <span className={css.success}>TRANSACTION SUCCESSFUL</span>
+                          <span className={css.error}>TRANSACTION FAILED</span>
+                        </div>
+                        <div className={css.right}>
+                          <span className={css.pending}>1 OF 1</span>
+                          <span className={css.success}>1 OF 1</span>
+                          <span className={css.error}>ERROR</span>
+                        </div>
+                      </div>
+                      <div className={css.notificationMessage}>
+                        <span>Fail stake on <a href="#">DAOstack meetup</a></span>
+                      </div>
+                    </div>
+                    <div className={css.notificationControls}>
+                      <button className={css.pending}><img src="/assets/images/Icon/Minimize-notification.svg" /></button>
+                      <button className={css.success}><img src="/assets/images/Icon/Close.svg" /></button>
+                      <button className={css.error}><img src="/assets/images/Icon/Close.svg" /></button>
                     </div>
                   </div>
-                  <div className={css.notificationMessage}>
-                    <span>Fail stake on <a href="#">DAOstack meetup</a></span>
+
+                  <br />
+
+                  <div className={css.pendingTransaction + " " + css.clearfix + " " + css.pending + " " + css.minimized}>
+                    <div className={css.statusIcon}>
+                      <img className={css.pending} src="/assets/images/Icon/Loading-white.svg" />
+                      <img className={css.success} src="/assets/images/Icon/Success-notification.svg" />
+                      <img className={css.error} src="/assets/images/Icon/Error-notification.svg" />
+                    </div>
+                    <div className={css.transactionMessage}>
+                      <div className={css.clearfix}>
+                        <div className={css.left}>
+                          <span className={css.pending}>PENDING TRANSACTION</span>
+                          <span className={css.success}>TRANSACTION SUCCESSFUL</span>
+                          <span className={css.error}>TRANSACTION FAILED</span>
+                        </div>
+                        <div className={css.right}>
+                          <span className={css.pending}>1 OF 1</span>
+                          <span className={css.success}>1 OF 1</span>
+                          <span className={css.error}>ERROR</span>
+                        </div>
+                      </div>
+                      <div className={css.notificationMessage}>
+                        <span>Fail stake on <a href="#">DAOstack meetup</a></span>
+                      </div>
+                    </div>
+                    <div className={css.notificationControls}>
+                      <button className={css.pending}><img src="/assets/images/Icon/Minimize-notification.svg" /></button>
+                      <button className={css.success}><img src="/assets/images/Icon/Close.svg" /></button>
+                      <button className={css.error}><img src="/assets/images/Icon/Close.svg" /></button>
+                    </div>
                   </div>
+
+                  <br />
+
+                  <div className={css.pendingTransaction + " " + css.clearfix + " " + css.success}>
+                    <div className={css.statusIcon}>
+                      <img className={css.pending} src="/assets/images/Icon/Loading-white.svg" />
+                      <img className={css.success} src="/assets/images/Icon/Success-notification.svg" />
+                      <img className={css.error} src="/assets/images/Icon/Error-notification.svg" />
+                    </div>
+                    <div className={css.transactionMessage}>
+                      <div className={css.clearfix}>
+                        <div className={css.left}>
+                          <span className={css.pending}>PENDING TRANSACTION</span>
+                          <span className={css.success}>TRANSACTION SUCCESSFUL</span>
+                          <span className={css.error}>TRANSACTION FAILED</span>
+                        </div>
+                        <div className={css.right}>
+                          <span className={css.pending}>1 OF 1</span>
+                          <span className={css.success}>1 OF 1</span>
+                          <span className={css.error}>ERROR</span>
+                        </div>
+                      </div>
+                      <div className={css.notificationMessage}>
+                        <span>Fail stake on <a href="#">Travel, hotels and registration for Denver Eth</a></span>
+                      </div>
+                    </div>
+                    <div className={css.notificationControls}>
+                      <button className={css.pending}><img src="/assets/images/Icon/Minimize-notification.svg" /></button>
+                      <button className={css.success}><img src="/assets/images/Icon/Close.svg" /></button>
+                      <button className={css.error}><img src="/assets/images/Icon/Close.svg" /></button>
+                    </div>
+                  </div>
+
+                  <br />
+
+                  <div className={css.pendingTransaction + " " + css.clearfix + " " + css.pending}>
+                    <div className={css.statusIcon}>
+                      <img className={css.pending} src="/assets/images/Icon/Loading-white.svg" />
+                      <img className={css.success} src="/assets/images/Icon/Success-notification.svg" />
+                      <img className={css.error} src="/assets/images/Icon/Error-notification.svg" />
+                    </div>
+                    <div className={css.transactionMessage}>
+                      <div className={css.clearfix}>
+                        <div className={css.left}>
+                          <span className={css.pending}>PENDING TRANSACTION</span>
+                          <span className={css.success}>TRANSACTION SUCCESSFUL</span>
+                          <span className={css.error}>TRANSACTION FAILED</span>
+                        </div>
+                        <div className={css.right}>
+                          <span className={css.pending}>1 OF 1</span>
+                          <span className={css.success}>1 OF 1</span>
+                          <span className={css.error}>ERROR</span>
+                        </div>
+                      </div>
+                      <div className={css.notificationMessage}>
+                        <span>Vote on <a href="#">Consultants to help with explainer videos</a></span>
+                      </div>
+                    </div>
+                    <div className={css.notificationControls}>
+                      <button className={css.pending}><img src="/assets/images/Icon/Minimize-notification.svg" /></button>
+                      <button className={css.success}><img src="/assets/images/Icon/Close.svg" /></button>
+                      <button className={css.error}><img src="/assets/images/Icon/Close.svg" /></button>
+                    </div>
+                  </div>
+ */}
+
                 </div>
-                <div className={css.notificationControls}>
-                  <button className={css.pending}><img src="/assets/images/Icon/Minimize-notification.svg"/></button>
-                  <button className={css.success}><img src="/assets/images/Icon/Close.svg"/></button>
-                  <button className={css.error}><img src="/assets/images/Icon/Close.svg"/></button>
-                </div>
+                <div className={css.background}></div>
               </div>
-              </div>
-
-              <br/>
-
-              <div className={css.pendingTransaction + " " + css.clearfix + " " + css.error}>
-                <div className={css.statusIcon}>
-                  <img className={css.pending} src="/assets/images/Icon/Loading-white.svg"/>
-                  <img className={css.success} src="/assets/images/Icon/Success-notification.svg"/>
-                  <img className={css.error} src="/assets/images/Icon/Error-notification.svg"/>
-                </div>
-                <div className={css.transactionMessage}>
-                  <div className={css.clearfix}>
-                    <div className={css.left}>
-                      <span className={css.pending}>PENDING TRANSACTION</span>
-                      <span className={css.success}>TRANSACTION SUCCESSFUL</span>
-                      <span className={css.error}>TRANSACTION FAILED</span>
-                    </div>
-                    <div className={css.right}>
-                      <span className={css.pending}>1 OF 1</span>
-                      <span className={css.success}>1 OF 1</span>
-                      <span className={css.error}>ERROR</span>
-                    </div>
-                  </div>
-                  <div className={css.notificationMessage}>
-                    <span>Fail stake on <a href="#">DAOstack meetup</a></span>
-                  </div>
-                </div>
-                <div className={css.notificationControls}>
-                  <button className={css.pending}><img src="/assets/images/Icon/Minimize-notification.svg"/></button>
-                  <button className={css.success}><img src="/assets/images/Icon/Close.svg"/></button>
-                  <button className={css.error}><img src="/assets/images/Icon/Close.svg"/></button>
-                </div>
-              </div>
-
-              <br/>
-
-              <div className={css.pendingTransaction + " " + css.clearfix + " " + css.success}>
-                <div className={css.statusIcon}>
-                  <img className={css.pending} src="/assets/images/Icon/Loading-white.svg"/>
-                  <img className={css.success} src="/assets/images/Icon/Success-notification.svg"/>
-                  <img className={css.error} src="/assets/images/Icon/Error-notification.svg"/>
-                </div>
-                <div className={css.transactionMessage}>
-                  <div className={css.clearfix}>
-                    <div className={css.left}>
-                      <span className={css.pending}>PENDING TRANSACTION</span>
-                      <span className={css.success}>TRANSACTION SUCCESSFUL</span>
-                      <span className={css.error}>TRANSACTION FAILED</span>
-                    </div>
-                    <div className={css.right}>
-                      <span className={css.pending}>1 OF 1</span>
-                      <span className={css.success}>1 OF 1</span>
-                      <span className={css.error}>ERROR</span>
-                    </div>
-                  </div>
-                  <div className={css.notificationMessage}>
-                    <span>Fail stake on <a href="#">Travel, hotels and registration for Denver Eth</a></span>
-                  </div>
-                </div>
-                <div className={css.notificationControls}>
-                  <button className={css.pending}><img src="/assets/images/Icon/Minimize-notification.svg"/></button>
-                  <button className={css.success}><img src="/assets/images/Icon/Close.svg"/></button>
-                  <button className={css.error}><img src="/assets/images/Icon/Close.svg"/></button>
-                </div>
-              </div>
-
-              <br/>
-
-              <div className={css.pendingTransaction + " " + css.clearfix + " " + css.pending}>
-                <div className={css.statusIcon}>
-                  <img className={css.pending} src="/assets/images/Icon/Loading-white.svg"/>
-                  <img className={css.success} src="/assets/images/Icon/Success-notification.svg"/>
-                  <img className={css.error} src="/assets/images/Icon/Error-notification.svg"/>
-                </div>
-                <div className={css.transactionMessage}>
-                  <div className={css.clearfix}>
-                    <div className={css.left}>
-                      <span className={css.pending}>PENDING TRANSACTION</span>
-                      <span className={css.success}>TRANSACTION SUCCESSFUL</span>
-                      <span className={css.error}>TRANSACTION FAILED</span>
-                    </div>
-                    <div className={css.right}>
-                      <span className={css.pending}>1 OF 1</span>
-                      <span className={css.success}>1 OF 1</span>
-                      <span className={css.error}>ERROR</span>
-                    </div>
-                  </div>
-                  <div className={css.notificationMessage}>
-                    <span>Vote on <a href="#">Consultants to help with explainer videos</a></span>
-                  </div>
-                </div>
-                <div className={css.notificationControls}>
-                  <button className={css.pending}><img src="/assets/images/Icon/Minimize-notification.svg"/></button>
-                  <button className={css.success}><img src="/assets/images/Icon/Close.svg"/></button>
-                  <button className={css.error}><img src="/assets/images/Icon/Close.svg"/></button>
-                </div>
-              </div>
-
-
-            </div>
-            <div className={css.background}></div>
-          </div>
-        : <div className={css.loading}>Something weird happened, please contact the DAOstack team...</div>
+              : <div className={css.loading}>Something weird happened, please contact the DAOstack team...</div>
       )
     );
   }
