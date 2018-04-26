fd5d2d5115dc4240b5875862f84fcb9d7164c855
Matan Tsuberi <dev.matan.tsuberi@gmail.com>
2018-04-26 16:39:53 +0300

    Use `TransactionService` from Arc.js (#61)
    * use TransactionService to dispatch pending operations
    
    * show notifications for operations with only one transaction
    
    * refactor subscribing into a util function + handle unsubscribing
    
    * abstract away the generation of the key
    
    * refactor onPendingTransactions to handle errors properly
    
    * `>=` -> `==`
    
    * refactor performAction
    
    * unsubscribe workaround
    
    * don't check for failed transactions & get rid of `onError`
    
    * WEB3_CONNECT totalSteps: 1

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,5 +1,6 @@
 import * as Arc from "@daostack/arc.js";
 import { BigNumber } from "bignumber.js";
+import { IEventSubscription } from "@daostack/arc.js";
 
 export default class Util {
 
@@ -30,6 +31,46 @@ export default class Util {
         return "Ganache"
       default:
         return "Unknown network"
+    }
+  }
+
+  /**
+   * Performs an Arc.js action.
+   * @param topic Arc.js TransactionService topic to listen to for pending transactions
+   * @param action Function that will invoke the action to perform
+   * @param opts options to pass to @f
+   * @param onPending callback that's called on every pending transaction
+   * @param onError callback that's called upon any error / failed transaction
+   */
+  public static async performAction<T>(
+    topic: string,
+    action: (opts: any) => Promise<T>,
+    opts: any,
+    onPending: (txCount: number) => any
+  ): Promise<T> {
+
+    let sub: IEventSubscription;
+    const unsubscribe = () => {
+      if (sub) {
+        // workaround to get last transaction notification before unsubscribing.
+        setTimeout(sub.unsubscribe, 0);
+      }
+    }
+
+    try {
+      const key = Arc.TransactionService.generateInvocationKey(`${topic}.pendingTransactions`);
+      sub = Arc.TransactionService.subscribe(topic, (topic, info) => {
+        if (info.options.key === key && info.tx) {
+          onPending(info.txCount);
+        }
+      });
+      const result = await action({ ...opts, key });
+      unsubscribe();
+      return result;
+    } catch (e) {
+      console.error(e);
+      unsubscribe();
+      throw e;
     }
   }
 }
