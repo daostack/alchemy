20d0991c8c892aea8b85533f121872b19cd8c46c
Tibet Sprague <tibet.sprague@gmail.com>
2018-05-18 14:03:10 -0700

    Add copy to clipboard functionality to every account info popup

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -78,4 +78,13 @@ export default class Util {
       throw e;
     }
   }
+
+  public static copyToClipboard(value : any) {
+    const el = document.createElement('textarea');
+    el.value = value;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+  }
 }
