22186fd89b556e81fa97cefd65c100b671f68d6e
Aaron Brodeur <aaronbrodeur@Aarons-MacBook-Pro.local>
2018-06-15 11:03:11 -0700

    Fixes to homepage, modal and z-index`

--- src/components/Home/HomeContainer.tsx
+++ src/components/Home/HomeContainer.tsx
@@ -61,16 +61,16 @@ class HomeContainer extends React.Component<IProps, null> {
         </div>
         <div className={css.joinAlchemy}>
           <div>
-            <h2>Join Alchemy Alpha</h2>
-            <p>Alchemy is currently in its Alpha version. To join our first experiment and be part of Genesis DAO, sign up here:</p>
+            <h2>Alchemy 101</h2>
+            <p>Alchemy is an app for budgeting and resource allocation, designed for Decentralized Autonomous Organizations (DAOs). Alchemy is currently in its Alpha version.</p>
             <div className={css.bottomCta}>
-              <a href="https://t.me/joinchat/BMgbsAxOJrZhu79TKB7Y8g" target='_blank'>Join our pilot</a>
+              <a href="https://medium.com/daostack/new-introducing-alchemy-budgeting-for-decentralized-organizations-b81ba8501b23" target='_blank'>Learn more</a>
             </div>
           </div>
         </div>
         <div className={css.footer}>
           <Link to='/'>View Alchemy</Link>
-          <Link to='/daos'>View Alchemy</Link>
+          <a href='https://alchemy.daostack.io/#/dao/0x7b11dfb29504abc8c0dfa60dc7e0aa2aae836db0'>Genesis Alpha</a>
           <a href="https://docs.google.com/document/d/1M1erC1TVPPul3V_RmhKbyuFrpFikyOX0LnDfWOqO20Q/" target='_blank'>FAQ</a>
           <a href="https://medium.com/daostack/new-introducing-alchemy-budgeting-for-decentralized-organizations-b81ba8501b23" target='_blank'>Alchemy 101</a>
           <a href="https://www.daostack.io/" target='_blank'>About DAOstack</a>
