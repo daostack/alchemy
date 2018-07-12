4c19f76eeaddd6e8b2a0aae832d55c731be94e4c
Matan Tsuberi <dev.matan.tsuberi>
2018-07-12 18:49:59 +0300

    better detect out of gas messages

--- src/reducers/operations.ts
+++ src/reducers/operations.ts
@@ -79,7 +79,7 @@ const errorType = (error: Error) => {
     return OperationError.Canceled;
   } else if (message.includes('revert')) {
     return OperationError.Reverted;
-  } else if (message.includes('out of gas') || message.includes('base fee exceeds gas limit')) {
+  } else if (message.includes('gas')) {
     return OperationError.OutOfGas;
   } else {
     return error.message;
