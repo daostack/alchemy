8aa5e15206e08dfd375cd2f29339b419fe953f38
Aaron <aaronbrodeur@Aarons-iMac.local>
2018-07-17 16:04:00 -0700

    Fixed styling of metamask modal and added incentives:
    https://trello.com/c/abA5o4mR/303-how-do-we-incentivize-clearly-and-show-what-potential-outcomes-could-b

--- src/components/Shared/PreTransactionModal.tsx
+++ src/components/Shared/PreTransactionModal.tsx
@@ -80,6 +80,125 @@ export default class PreTransactionModal extends React.Component<IProps> {
                 </button>
               </div>
             </div>
+            <div className={css.incentives}>
+              <span className={css.outcomes}>OUTCOMES</span>
+              <span className={css.passIncentive}><strong>PASS</strong> <span>YOU GAIN GEN & REPUTATION</span></span>
+              {/* 
+
+              PASS CONDITIONS:
+
+              Action
+              Copy
+
+              ----
+
+              Voting up a regular proposal
+              YOU GAIN GEN & REPUTATION
+
+              Voting up a boosted proposal
+              NO REWARDS
+
+              Vote down a regular proposal
+              LOSE 1% YOUR REPUTATION
+
+              Vote down a boosted proposal
+              NO REWARDS
+
+              Propose
+              GAIN REPUTATION & [if target of proposal, list token and reputation]
+
+              Stake up regular proposal
+              YOU GAIN GEN AND REPUTATION
+
+              Stake down a regular proposal
+              NO REWARDS & LOSE YOUR STAKE
+
+              */}
+              <span className={css.failIncentive}><strong>FAIL</strong> <span>YOU GAIN GEN</span></span>
+
+              {/*
+
+              FAIL CONDITIONS:
+
+              Action
+              Copy
+
+              ----
+
+              Voting up a regular proposal
+              LOSE 1% OF YOUR REPUTATION
+
+              Voting up a boosted proposal
+              NO REWARDS
+
+              Vote down a regular proposal
+              GAIN GEN
+
+              Vote down a boosted proposal
+              NO REWARDS
+
+              Propose
+              NO REWARDS
+
+              Stake up regular proposal
+              NO REWARDS & LOSE YOUR STAKE
+
+              Stake down a regular proposal
+              YOU GAIN GEN AND REPUTATION
+
+              */}
+              <span className={css.help}>
+                <img src="/assets/images/Icon/Help.svg"/>
+                <img className={css.hover} src="/assets/images/Icon/Help-hover.svg"/>
+                <div className={css.helpBox}>
+                  <div className={css.pointer}></div>
+                  <div className={css.bg}></div>
+                  <div className={css.bridge}></div>
+                  <div className={css.header}>
+                    <h2>Genesis Protocol</h2>
+                    <h3>RULES FOR YES VOTES</h3>
+                  </div>                
+                  <div className={css.body}>
+
+                    {/*
+
+                    HELP TEXT:
+
+                    ACTION
+                    Copy
+
+                    ----
+
+                    VOTING UP A REGULAR PROPOSAL, VOTING DOWN A REGULAR PROPOSAL, VOTING ON BOOSTED PROPOSALS
+                    
+                    <p>When you vote, 1% of your reputation is taken away for the duration of the vote. If you vote for something to pass and it does, you gain reputation in addition to the reputation you lost.</p>
+
+                    <p>If you vote for something to pass and it does, you will be given a portion of whatever GEN have been staked on the proposal.</p>
+
+                    <p>If you vote for something to fail and it does, you will be given a portionof whatever GEN have been staked on the proposal.</p>
+
+                    <p>You will not receive reputation or GEN for voting on a boosted proposal.</p>
+
+                    STAKING UP OR DOWN
+
+                    <p>When you place a stake, GEN are taken from your wallet and held in a smart contract for the duration of the vote. If your stake is correct, you receive your GEN+ a portion of whatever has been staked on the incorrect outcome.</p>
+
+                    <p>If your stake is correct, you will also receive reputation within the DAO.</p>
+
+                    PROPOSING
+
+                    <p>If a proposal you submit passes, you will be awarded reputation. If you are also the benificiary of the proposal, when the proposal passes the allocated tokens will be given to you.
+
+                    <p>If a proposal you submit fails, you do not lose anything.</p>
+
+                    */}
+
+
+                  </div>
+                  <a href="https://docs.google.com/document/d/1LMe0S4ZFWELws1-kd-6tlFmXnlnX9kfVXUNzmcmXs6U/edit?usp=drivesdk">View the Gensis Protocol</a>
+                </div>
+              </span>
+            </div>
             { !proposalEnded(proposal) ?
               <div className={css.decisionGraph}>
                   <span className={css.forLabel}>{proposal.votesYes} ({yesPercentage}%) PASS</span>
