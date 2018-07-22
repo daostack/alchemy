d1863edc01468642e8135cbbc0491b6d9e579820
Matan Tsuberi <dev.matan.tsuberi@gmail.com>
2018-07-23 00:07:02 +0300

    watch for confirmation insteaf of mining (#134)

--- src/selectors/operations.ts
+++ src/selectors/operations.ts
@@ -10,10 +10,12 @@ export const isVotePending = (proposalId: string, vote: VoteOptions) =>
     [operations],
     (operations: IOperationsState) => {
       return Object.keys(operations)
-        .filter((k) => operations[k].status !== OperationStatus.Mined && !operations[k].error)
-        .filter((k) => operations[k].functionName === 'GenesisProtocol.vote')
-        .filter((k) => operations[k].options.proposalId === proposalId)
-        .filter((k) => operations[k].options.vote === vote)
+        .filter((k) =>
+          operations[k].status !== OperationStatus.Complete && !operations[k].error &&
+          operations[k].functionName === 'GenesisProtocol.vote' &&
+          operations[k].options.proposalId === proposalId &&
+          operations[k].options.vote === vote
+        )
         .length > 0
     }
   )
@@ -23,10 +25,12 @@ export const isStakePending = (proposalId: string, vote: VoteOptions) =>
     [operations],
     (operations: IOperationsState) => {
       return Object.keys(operations)
-        .filter((k) => operations[k].status !== OperationStatus.Mined && !operations[k].error)
-        .filter((k) => operations[k].functionName === 'GenesisProtocol.stake')
-        .filter((k) => operations[k].options.proposalId === proposalId)
-        .filter((k) => operations[k].options.vote === vote)
+        .filter((k) =>
+          operations[k].status !== OperationStatus.Complete && !operations[k].error &&
+          operations[k].functionName === 'GenesisProtocol.stake' &&
+          operations[k].options.proposalId === proposalId &&
+          operations[k].options.vote === vote
+        )
         .length > 0
     }
   )
