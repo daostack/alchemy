6f4d4bc9ed5e2b7dad1048f7dbe73eb79e60095f
Tibet Sprague <tibet.sprague@gmail.com>
2018-07-26 00:29:02 -0700

    Make bg_cache_script more synchronous and fix bugs
    - burning reputation doesnt crash
    - proposals expiring should really work
    - more logging

--- src/bg_cache_worker.ts
+++ src/bg_cache_worker.ts
@@ -9,7 +9,7 @@ import * as Arc from "@daostack/arc.js";
 import * as aws from 'aws-sdk';
 import promisify = require("es6-promisify");
 import * as moment from "moment";
-import { normalize } from "normalizr";
+import { denormalize, normalize } from "normalizr";
 import * as Redis from 'redis';
 import * as Redux from 'redux';
 import loggerMiddleware from "redux-logger";
@@ -33,39 +33,38 @@ import Util from "./lib/util";
 
 require('dotenv').config();
 
+const redisClient = Redis.createClient(process.env.REDIS_URL || "redis://127.0.0.1:6379");
+const redisGet = promisify(redisClient.get.bind(redisClient));
+const redisSet = promisify(redisClient.set.bind(redisClient));
+
+const arcjsNetwork = process.env.arcjs_network;
+
+if (process.env.NODE_ENV == 'production') {
+  // Use Infura on production
+  const infuraNetwork = arcjsNetwork == 'live' ? 'mainnet' : arcjsNetwork;
+
+  // The default account in reading from Infura, I'm not totally sure why this is needed (but it is), or what account it should be
+  const mnemonic = process.env.DEFAULT_ACCOUNT_MNEMONIC;
+  const infuraKey = process.env.INFURA_KEY;
+  const provider = new HDWalletProvider(mnemonic, "https://" + infuraNetwork + ".infura.io/" + infuraKey);
+
+  // Setup web3 ourselves so we can use Infura instead of letting Arc.js setup web3
+  global.web3 = new Web3(provider.engine);
+
+  process.env.API_URL = process.env.API_URL || "https://daostack-alchemy.herokuapp.com";
+} else {
+  process.env.API_URL = process.env.API_URL || "http://127.0.0.1:3001";
+}
+
+aws.config.region = 'us-west-2';
+const s3 = new aws.S3();
+const s3FileName = 'initialArcState-' + arcjsNetwork + '.json';
+const s3FileType = 'application/json';
+
 async function updateCache() {
-  const startTime = moment();
-
-  const arcjsNetwork = process.env.arcjs_network;
-
-  if (process.env.NODE_ENV == 'production') {
-    // Use Infura on production
-    const infuraNetwork = arcjsNetwork == 'live' ? 'mainnet' : arcjsNetwork;
-
-    // The default account in reading from Infura, I'm not totally sure why this is needed (but it is), or what account it should be
-    const mnemonic = process.env.DEFAULT_ACCOUNT_MNEMONIC;
-    const infuraKey = process.env.INFURA_KEY;
-    const provider = new HDWalletProvider(mnemonic, "https://" + infuraNetwork + ".infura.io/" + infuraKey);
-
-    // Setup web3 ourselves so we can use Infura instead of letting Arc.js setup web3
-    global.web3 = new Web3(provider.engine);
-
-    process.env.API_URL = process.env.API_URL || "https://daostack-alchemy.herokuapp.com";
-  } else {
-    process.env.API_URL = process.env.API_URL || "http://127.0.0.1:3001";
-  }
-
   await Arc.InitializeArcJs();
 
-  aws.config.region = 'us-west-2';
-  const s3 = new aws.S3();
-  const s3FileName = 'initialArcState-' + arcjsNetwork + '.json';
-  const s3FileType = 'application/json';
-
-  const redisClient = Redis.createClient(process.env.REDIS_URL || "redis://127.0.0.1:6379");
-
-  const redisGet = promisify(redisClient.get.bind(redisClient));
-  const redisSet = promisify(redisClient.set.bind(redisClient));
+  const startTime = moment();
 
   // Block number to start looking fromn:
   //   first look argument passed in to script, otherwise get last block cached from redis, otherwise we start over from block 0
@@ -128,7 +127,7 @@ async function updateCache() {
 
   if (lastCachedBlock == 0) {
     console.log("Starting to cache the "  + arcjsNetwork + " blockchain from the beginning");
-    store.dispatch(arcActions.getDAOs(0, latestBlock));
+    await store.dispatch(arcActions.getDAOs(0, latestBlock));
   } else {
     console.log("Pulling current cached state for " + arcjsNetwork + " from S3 bucket " + process.env.S3_BUCKET);
     try {
@@ -151,97 +150,96 @@ async function updateCache() {
 
     // Watch for new, confirmed proposals coming in
     const contributionRewardInstance = await Arc.ContributionRewardFactory.deployed();
-    this.proposalEventWatcher = contributionRewardInstance.NewContributionProposal({ }, { fromBlock: lastCachedBlock, toBlock: latestBlock });
-    await this.proposalEventWatcher.get(async (error: Error, eventsArray: Array<Arc.DecodedLogEntryEvent<Arc.NewContributionProposalEventResult>>) => {
-      for (let index = 0; index < eventsArray.length; index++) {
-        const event = eventsArray[index];
-        console.log("Got new proposal", event.args);
-        await store.dispatch(arcActions.onProposalCreateEvent(event.args));
-      }
-    });
+    const proposalEventWatcher = contributionRewardInstance.NewContributionProposal({ }, { fromBlock: lastCachedBlock, toBlock: latestBlock });
+    const getProposalEvents = promisify(proposalEventWatcher.get.bind(proposalEventWatcher));
+    const proposalEvents: Array<Arc.DecodedLogEntryEvent<Arc.NewContributionProposalEventResult>> = await getProposalEvents();
+    for (let index = 0; index < proposalEvents.length; index++) {
+      const event = proposalEvents[index];
+      console.log("Got new proposal", event.args);
+      await store.dispatch(arcActions.onProposalCreateEvent(event.args));
+    }
 
     console.log("Done looking for new proposals, now looking for new stakes");
 
     // TODO: look for allEvents on the voting machine instance to process these in the exact order they happened
     const votingMachineInstance = await Arc.GenesisProtocolFactory.deployed();
 
-    this.stakeEventWatcher = votingMachineInstance.Stake({ }, { fromBlock: lastCachedBlock, toBlock: latestBlock });
-    await this.stakeEventWatcher.get(async (error: Error, eventsArray: Array<Arc.DecodedLogEntryEvent<Arc.StakeEventResult>>) => {
-      for (let index = 0; index < eventsArray.length; index++) {
-        const event = eventsArray[index];
-        console.log("Got new stake", event.args);
-        await store.dispatch(arcActions.onStakeEvent(event.args._avatar, event.args._proposalId, event.args._staker, Number(event.args._vote), Util.fromWei(event.args._amount)));
-      }
-    });
+    const stakeEventWatcher = votingMachineInstance.Stake({ }, { fromBlock: lastCachedBlock, toBlock: latestBlock });
+    const getStakeEvents = promisify(stakeEventWatcher.get.bind(stakeEventWatcher));
+    const stakeEvents: Array<Arc.DecodedLogEntryEvent<Arc.StakeEventResult>> = await getStakeEvents();
+    for (let index = 0; index < stakeEvents.length; index++) {
+      const event = stakeEvents[index];
+      console.log("Got new stake", event.args);
+      await store.dispatch(arcActions.onStakeEvent(event.args._avatar, event.args._proposalId, event.args._staker, Number(event.args._vote), Util.fromWei(event.args._amount)));
+    }
 
     console.log("Done looking for new stakes, now looking for new votes");
 
-    this.voteEventWatcher = votingMachineInstance.VoteProposal({ }, { fromBlock: lastCachedBlock, toBlock: latestBlock });
-    await this.voteEventWatcher.get(async (error: Error, eventsArray: Array<Arc.DecodedLogEntryEvent<Arc.VoteProposalEventResult>>) => {
-      if (error) {
-        console.error("Error getting votes", error);
-      } else {
-        for (let index = 0; index < eventsArray.length; index++) {
-          const event = eventsArray[index];
-          console.log("Got new vote", event.args);
-          await store.dispatch(arcActions.onVoteEvent(event.args._avatar, event.args._proposalId, event.args._voter, Number(event.args._vote), Util.fromWei(event.args._reputation)));
-        }
-      }
-    });
+    const voteEventWatcher = votingMachineInstance.VoteProposal({ }, { fromBlock: lastCachedBlock, toBlock: latestBlock });
+    const getVoteEvents = promisify(voteEventWatcher.get.bind(voteEventWatcher));
+    const voteEvents:Array<Arc.DecodedLogEntryEvent<Arc.VoteProposalEventResult>> = await voteEventWatcher.get();
+    for (let index = 0; index < voteEvents.length; index++) {
+      const event = voteEvents[index];
+      console.log("Got new vote", event.args);
+      await store.dispatch(arcActions.onVoteEvent(event.args._avatar, event.args._proposalId, event.args._voter, Number(event.args._vote), Util.fromWei(event.args._reputation)));
+    }
 
     console.log("Done looking for new votes, now looking for executed proposals");
 
-    this.executeProposalEventWatcher = votingMachineInstance.ExecutedProposals({}, { fromBlock: lastCachedBlock, toBlock: latestBlock });
-    await this.executeProposalEventWatcher.get(async (error: Error, eventsArray: Arc.ExecutedGenesisProposal[]) => {
-      for (let index = 0; index < eventsArray.length; index++) {
-        const event = eventsArray[index];
-        console.log("Proposal executed", event);
-        const { avatarAddress, proposalId, decision, totalReputation, executionState } = event;
-        await store.dispatch(arcActions.onProposalExecuted(avatarAddress, proposalId, executionState, Number(decision), Util.fromWei(totalReputation)));
-      }
-    });
+    const executeProposalEventWatcher = votingMachineInstance.ExecutedProposals({}, { fromBlock: lastCachedBlock, toBlock: latestBlock });
+    const getExecutedProposalEvents = promisify(executeProposalEventWatcher.get.bind(executeProposalEventWatcher));
+    const executedProposalEvents: Arc.ExecutedGenesisProposal[] = await getExecutedProposalEvents();
+    for (let index = 0; index < executedProposalEvents.length; index++) {
+      const event = executedProposalEvents[index];
+      console.log("Proposal executed", event);
+      const { avatarAddress, proposalId, decision, totalReputation, executionState } = event;
+      await store.dispatch(arcActions.onProposalExecuted(avatarAddress, proposalId, executionState, Number(decision), Util.fromWei(totalReputation)));
+    }
 
     console.log("done with executed proposals, now token and reputation balances");
 
     const stakingTokenAddress = await votingMachineInstance.contract.stakingToken();
     const stakingToken = await (await Arc.Utils.requireContract("StandardToken")).at(stakingTokenAddress) as any;
 
-    const daos = (store.getState() as IRootState).arc.daos;
-    Object.keys(daos).forEach(async (avatarAddress) => {
+    const state = (store.getState() as IRootState);
+    const daos = denormalize(state.arc.daos, schemas.daoList, state.arc);
+    for (const avatarAddress of Object.keys(daos)) {
       const daoInstance = await Arc.DAO.at(avatarAddress);
 
-      this.mintEventWatcher = daoInstance.reputation.Mint({}, { fromBlock: lastCachedBlock, toBlock: latestBlock });
-      await this.mintEventWatcher.get(async (error: any, eventsArray: any) => {
-        for (let index = 0; index < eventsArray.length; index++) {
-          const event = eventsArray[index];
-          console.log("Reputation minted for account", event.args._to, "in DAO", avatarAddress);
-          await store.dispatch(arcActions.onReputationChangeEvent(avatarAddress, event.args._to));
-        }
-      });
+      const mintEventWatcher = daoInstance.reputation.Mint({}, { fromBlock: lastCachedBlock, toBlock: latestBlock });
+      const getMintEvents = promisify(mintEventWatcher.get.bind(mintEventWatcher));
+      const mintEvents = await getMintEvents();
+      for (let index = 0; index < mintEvents.length; index++) {
+        const event = mintEvents[index];
+        console.log("Reputation minted for account", event.args._to, "in DAO", avatarAddress);
+        await store.dispatch(arcActions.onReputationChangeEvent(avatarAddress, event.args._to));
+      }
 
-      this.burnEventWatcher = daoInstance.reputation.Burn({}, { fromBlock: lastCachedBlock, toBlock: latestBlock });
-      await this.burnEventWatcher.get(async (error: any, eventsArray: any) => {
-        for (let index = 0; index < eventsArray.length; index++) {
-          const event = eventsArray[index];
-          console.log("Reputation burned from account", event.args._from, "in DAO", avatarAddress);
-          await store.dispatch(arcActions.onReputationChangeEvent(avatarAddress, event.args._from));
-        }
-      });
+      const burnEventWatcher = daoInstance.reputation.Burn({}, { fromBlock: lastCachedBlock, toBlock: latestBlock });
+      const getBurnEvents = promisify(burnEventWatcher.get.bind(burnEventWatcher));
+      const burnEvents = await getBurnEvents();
+      for (let index = 0; index < burnEvents.length; index++) {
+        const event = burnEvents[index];
+        console.log("Reputation burned from account", event.args._from, "in DAO", avatarAddress);
+        await store.dispatch(arcActions.onReputationChangeEvent(avatarAddress, event.args._from));
+      }
 
       const newEthBalance = Util.fromWei(await promisify(global.web3.eth.getBalance)(avatarAddress));
-      await arcActions.onDAOEthBalanceChanged(avatarAddress, newEthBalance);
+      await store.dispatch(arcActions.onDAOEthBalanceChanged(avatarAddress, newEthBalance));
       const newGenBalance = Util.fromWei(await stakingToken.balanceOf(avatarAddress));
-      await arcActions.onDAOGenBalanceChanged(avatarAddress, newGenBalance);
+      await store.dispatch(arcActions.onDAOGenBalanceChanged(avatarAddress, newGenBalance));
 
       // Check all proposals to see if any expired and if so update state and gather redemptions
-      daos[avatarAddress].proposals.forEach((proposal: IProposalState) => {
+      for (const proposal of daos[avatarAddress].proposals) {
         if (checkProposalExpired(proposal) != proposal.state) {
-          arcActions.onProposalExpired(proposal);
+          console.log("Proposal expired", proposal.Id, proposal.title);
+          await store.dispatch(arcActions.onProposalExpired(proposal));
         }
-      });
-    });
+      }
+    }
   }
 
+  console.log("Updating last block read on redis: ", latestBlock);
   redisSet('alchemy-last-block-' + arcjsNetwork, latestBlock);
 }
 
