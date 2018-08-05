d3c77b1a076eb1ac9d98c0777dd6dc0501990c8f
Tibet Sprague <tibet.sprague@gmail.com>
2018-08-05 16:51:26 -0700

    Try and get script that restarts cache dyno to use current app name

--- scripts/restartCacheDynos.js
+++ scripts/restartCacheDynos.js
@@ -1,5 +1,4 @@
 const Heroku = require('heroku-client');
 const heroku = new Heroku({ token: process.env.HEROKU_API_TOKEN });
-const appName = process.env.NODE_ENV == 'production' ? 'daostack-alchemy-client' : 'daostack-alchemy-staging';
-heroku.delete('/apps/' + appName + '/dynos/clock').then(app => {});
+heroku.delete('/apps/' + process.env.HEROKU_APP_NAME + '/dynos/clock').then(app => {});
 
