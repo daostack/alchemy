5b96a237069f89ce783fb4ad68f70c50519c4942
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2018-11-19 19:32:21 +0100

    run ganache quietly in docker

--- entry.sh
+++ entry.sh
@@ -8,7 +8,7 @@ set -e
 # TODO: ganache shoudl run in a separate container
 # this is a quick hack to ahve ganache running and deploy the contracts we need
  # nohup npm run ganacheDb &
-nohup npm explore ganache-cli -- npm start --  -h 0.0.0.0 --networkId 1512051714758 --mnemonic "behave pipe turkey animal voyage dial relief menu blush match jeans general" &
+nohup npm explore ganache-cli -- npm start --  -h 0.0.0.0 --networkId 1512051714758 --quiet --mnemonic "behave pipe turkey animal voyage dial relief menu blush match jeans general" & 
 
 npm run migrate-ganache
 npm run start-docker
