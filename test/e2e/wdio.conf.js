b74e7484b553ae13255556aabb248f99eff36511
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2018-11-19 14:57:56 +0100

    wdio now finds the test

--- test/e2e/wdio.conf.js
+++ test/e2e/wdio.conf.js
@@ -10,7 +10,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/integration/webdriverio.js'
+        './test/e2e/*.js'
     ],
     // Patterns to exclude.
     exclude: [
