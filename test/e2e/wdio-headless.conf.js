c9434d99a6f252e72ebff9a7a31b75a5b8449dd0
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2018-11-20 15:31:22 +0100

    run headless in travis

--- test/e2e/wdio-headless.conf.js
+++ test/e2e/wdio-headless.conf.js
@@ -1,5 +1,17 @@
 config  = require('./wdio.conf.js').config
-config.capabilities.chromeOptions = {
-  args: ['headless', 'disable-gpu']
-}
-exports.config =  config
+// config.capabilities.chromeOptions = {
+//   args: ['headless', 'disable-gpu']
+// }
+exports.config = Object.assign(config, {
+  capabilities: [{
+    // maxInstances can get overwritten per capability. So if you have an in-house Selenium
+    // grid with only 5 firefox instances available you can make sure that not more than
+    // 5 instances get started at a time.
+    maxInstances: 5,
+    //
+    browserName: 'chrome',
+    chromeOptions: {
+        args: ['headless', 'disable-gpu']
+    }
+  }]
+})
