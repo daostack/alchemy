5a61ef8c3e8b0af306a328ec9c9d893f465b8a3b
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2018-11-23 11:31:37 +0100

    typo in entry script

--- entry.sh
+++ entry.sh
@@ -8,7 +8,7 @@ set -e
 # TODO: ganache runs in a separate container and we should use that process
 # this is a quick hack to  play nice with arc.js
 echo "starting ganache..."
-nohup npm explore ganache-cli -- npm start --  -h 0.0.0.0 --networkId 1512051714758 --quiet --mnemonic "behave pipe turkey animal voyage dial relief menu blush match jeans general"   > /dev/null 
+npm explore ganache-cli -- npm start --  -h 0.0.0.0 --networkId 1512051714758 --quiet --mnemonic "behave pipe turkey animal voyage dial relief menu blush match jeans general" > /dev/null &
 echo "ganache started"
 
 echo "deploying contracts..."
