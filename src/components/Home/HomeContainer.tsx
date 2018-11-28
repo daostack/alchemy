7bb558d3e579ad800cb0b1bdfaa56ff5b39844a7
Bo Henderson <bohende@gmail.com>
2018-11-28 17:52:46 +0530

    monorepo checkpoint

--- src/components/Home/HomeContainer.tsx
+++ src/components/Home/HomeContainer.tsx
@@ -49,10 +49,7 @@ class HomeContainer extends React.Component<IProps, null> {
           <h1>Alchemy</h1>
           <h2>Budgeting and resource allocation for decentralized organizations</h2>
           <div className={css.topCta}>
-            { process.env.NODE_ENV == 'production'
-                ? <a href='https://alchemy.daostack.io/dao/0xa3f5411cfc9eee0dd108bf0d07433b6dd99037f1'>View Alchemy</a>
-                : <Link to='/daos'>Participate in Alchemy (MetaMask Required)</Link>
-            }
+            <Link to='/daos'>Participate in Alchemy (MetaMask Required)</Link>
           </div>
           <br/>
           <div className={css.topCta}>
