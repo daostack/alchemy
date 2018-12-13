eb3d880775d8139ce6a2721276a588da70ed4efe
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2018-12-13 09:15:36 +0100

    Arc import changed

--- src/arc.ts
+++ src/arc.ts
@@ -1,9 +1,11 @@
-import * as Arc from '@daostack/client'
+import { Arc } from '@daostack/client'
 const graphqlHttpProvider: string = 'http://127.0.0.1:8000/by-name/daostack/graphql'
 const graphqlWsProvider: string = 'ws://127.0.0.1:8001/by-name/daostack'
 const web3Provider: string = 'http://127.0.0.1:8545'
 
-export const arc = new Arc.Arc({
+console.log('------------')
+console.log(Arc)
+export const arc = new Arc({
   graphqlHttpProvider,
   graphqlWsProvider,
   web3Provider
