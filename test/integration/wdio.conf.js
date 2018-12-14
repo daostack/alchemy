f73f90bc798d235ac763e69b1e49fcc8770fc9ed
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2018-12-14 13:04:31 +0100

    use typescript for test

--- test/integration/wdio.conf.js
+++ test/integration/wdio.conf.js
@@ -10,7 +10,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/integration/*.js'
+        './test/integration/*.ts'
     ],
     // Patterns to exclude.
     exclude: [
@@ -247,4 +247,4 @@ exports.config = {
      */
     // onComplete: function(exitCode, config, capabilities) {
     // }
-}
+};
