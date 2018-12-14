b88ec41947bfe71354f0bbe3d440d96e1cd50bc1
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2018-12-14 13:13:00 +0100

    cleanup

--- src/arc.ts
+++ src/arc.ts
@@ -3,8 +3,6 @@ const graphqlHttpProvider: string = 'http://127.0.0.1:8000/subgraphs/name/daosta
 const graphqlWsProvider: string = 'ws://127.0.0.1:8001/subgraphs/name/daostack'
 const web3Provider: string = 'http://127.0.0.1:8545'
 
-console.log('------------')
-console.log(Arc)
 export const arc = new Arc({
   graphqlHttpProvider,
   graphqlWsProvider,
