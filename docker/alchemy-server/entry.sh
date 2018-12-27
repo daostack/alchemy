b8ec4da92ae5b2b57209e7fdd731f04ac8b19dd8
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2018-12-27 21:07:55 +0100

    add secrets to alchemy-server docker container

--- docker/alchemy-server/entry.sh
+++ docker/alchemy-server/entry.sh
@@ -5,8 +5,9 @@ set -e
 # wait for required containers
 /wait
 
-# /wait-for-it.sh postgres:5432
 # update the database
-node ./server/create-lb-tables.js`
-node ./server/migrate.js`
+cd /alchemy-server
+node ./server/create-lb-tables.js
+node ./server/migrate.js
+
 npm run start
