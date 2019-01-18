447e37c33e52475d5ec953bbe3238182b226e754
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2019-01-18 18:18:01 +0100

    more dbug info in travis

--- scripts/serviceStatus.js
+++ scripts/serviceStatus.js
@@ -1,14 +1,11 @@
 const fetch = require('node-fetch')
 async function main() {
   let response
+  let processExitStatus = 0
   console.log('------------ Service Status --------------------')
 
   const services = [
-    {
-      name: 'Alchemy Application',
-      url: 'http://0.0.0.0:3000',
-    },
-    {
+  {
       name: 'Local Ethereum node [expect status 400]',
       url: 'http://0.0.0.0:8545',
       request: { method: 'post', body: JSON.stringify({"jsonrpc":"2.0","method":"net_listening","params":[],"id":67}), headers: { 'Content-Type': 'application/json' }},
@@ -34,8 +31,12 @@ async function main() {
     {
       name: 'Alchemy server (accounts)',
       url: 'http://127.0.0.1:3001/api/accounts'
-    }
-  ]
+    },
+    {
+      name: 'Alchemy Application',
+      url: 'http://0.0.0.0:3000',
+    },
+    ]
   const output = []
   for (service of services) {
       try {
@@ -46,6 +47,7 @@ async function main() {
           status = 'OK'
         } else  {
           status = 'ERR'
+          processExitStatus = 1
         }
 
 
@@ -57,17 +59,17 @@ async function main() {
   const body = {"jsonrpc":"2.0","method":"net_listening","params":[],"id":67}
   const r = await fetch('http://127.0.0.1:8545', { method: 'post', body: JSON.stringify(body), headers: { 'Content-Type': 'application/json' }})
   console.log(await r.json())
-  return output
+  return [output, processExitStatus]
 }
 
 
 if (require.main === module) {
   main()
     .then((output) => {
-      for (l of output) {
+      for (l of output[0]) {
         console.log(l)
       }
-      process.exit(0)
+      process.exit(output[1])
     })
     .catch((err)  => { console.log(err); process.exit(1); });
 } else {
