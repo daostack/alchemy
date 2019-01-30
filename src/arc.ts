e4739e426d4f8a03640520599df60de579fdbe54
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2019-01-30 10:47:40 +0100

    fix link to graphql server

--- src/arc.ts
+++ src/arc.ts
@@ -1,5 +1,5 @@
 import { Arc } from '@daostack/client'
-const graphqlHttpProvider: string = 'http://127.0.0.1:8000/subgraphs/name/daostack/graphql'
+const graphqlHttpProvider: string = 'http://127.0.0.1:8000/subgraphs/name/daostack'
 const graphqlWsProvider: string = 'ws://127.0.0.1:8001/subgraphs/name/daostack'
 const web3Provider: string = 'http://127.0.0.1:8545'
 
