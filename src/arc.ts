5044704c41c920ba152b9e27b260acd8982840fe
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2019-02-06 22:39:21 +0100

    remove commented line

--- src/arc.ts
+++ src/arc.ts
@@ -1,11 +1,10 @@
 import { Arc } from '@daostack/client'
-// import { getLocalContractAddresses } from './lib/util'
 
 const graphqlHttpProvider: string = 'http://127.0.0.1:8000/subgraphs/name/daostack'
 const graphqlWsProvider: string = 'ws://127.0.0.1:8001/subgraphs/name/daostack'
 const web3HttpProvider: string = 'http://127.0.0.1:8545'
 const web3WsProvider: string = 'ws://127.0.0.1:8545'
-// must use localhost here because of cors issues
+// must use "localhost" here because of cors issues
 // const ipfsProvider: string = '/ip4/localhost/tcp/5001'
 const ipfsProvider: string = 'localhost'
 
