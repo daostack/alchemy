14be52c23f9af304f3f6965e00a08648c37fe8c9
Tibet Sprague <tibet.sprague@gmail.com>
2019-02-20 13:09:04 -0800

    Get account balances working

--- src/components/Account/AccountBalances.tsx
+++ src/components/Account/AccountBalances.tsx
@@ -1,7 +1,7 @@
 import BN = require("bn.js");
 import gql from "graphql-tag";
-
 import * as React from "react";
+import { combineLatest, Observable, of } from "rxjs";
 
 import * as css from "layouts/App.scss";
 
@@ -15,71 +15,35 @@ import Util from "lib/util";
 
 interface Props {
   dao: IDAOState;
+  ethBalance: BN;
+  genAllowance: BN;
+  genBalance: BN;
   member: IMemberState;
 }
-
-const TokenAllowance = (props: {accountAddress: Address, daoAddress: Address}) => {
-  // TODO: move query logic to daostack/client
-  // TODO: we are net filtering by spender (which is the votingMachine associated with the proposal)
-  // but we probably should, in the future.
-  const arc = getArc();
-  const query = gql`{
-    tokenApprovals (where: {owner: "${props.accountAddress}"} ){
-      id
-      contract
-      owner
-      spender
-      value
-    }
-  }`;
-  return <Subscribe observable={arc.getObservable(query)}>{(state: IObservableState<any>) => {
-    if (state.isLoading) {
-      return <div>loading..</div>;
-    } else if (state.error) {
-      return <div>{state.error}</div>;
-    } else {
-      // state.data is a list
-      const approvals = state.data.data.tokenApprovals;
-      if (approvals.length === 0) {
-        return <span>0</span>;
-      } else {
-        const approval = approvals[0]; // TODO: if there are mnay this may be wrong
-        return <span>{Util.fromWei(approval.value)}</span>;
-      }
-    }
-  }}</Subscribe >;
-};
 
 class AccountBalances extends React.Component<Props, null>  {
 
   public render() {
-    const { dao, member } = this.props;
-    // TODO: get ETH balance
-    const currentAccountEthBalance = new BN(0);
-    const currentAccountGenBalance = member.tokens;
-    // TODO: get allowance
-    // const currentAccountGenStakingAllowance = 0
-    // TODO: get external token balance
-    const currentAccountExternalTokenBalance = new BN(0);
+    const { dao, ethBalance, genAllowance, genBalance, member } = this.props;
 
     return (
       <div className={css.balances}>
         <div className={css.userBalance}>
           <div>
-            <AccountBalance tokenSymbol="ETH" balance={currentAccountEthBalance} accountAddress={member.address} />
+            <AccountBalance tokenSymbol="ETH" balance={ethBalance} accountAddress={member.address} />
           </div>
           <div>
-            <AccountBalance tokenSymbol="GEN" balance={currentAccountGenBalance} accountAddress={member.address} />
+            <AccountBalance tokenSymbol="GEN" balance={genBalance} accountAddress={member.address} />
           </div>
           <div>
-            <TokenAllowance accountAddress={member.address} daoAddress={dao.address} /> GEN approved for staking
+            <span>{Util.fromWei(genAllowance)}</span> GEN approved for staking
           </div>
-          { dao && dao.externalTokenAddress
+          {/*TODO: { dao && dao.externalTokenAddress
             ? <div>
                 <AccountBalance tokenSymbol={dao.externalTokenSymbol} balance={currentAccountExternalTokenBalance} accountAddress={member.address} />
               </div>
             : ""
-          }
+          }*/}
         </div>
         { dao
           ? <div className={css.daoBalance}>
@@ -102,50 +66,23 @@ export default (props: { dao: IDAOState, address: Address}) => {
     if (!props.address) {
       return null;
     }
-    // TODO: move query logic to daostack/client
-    const query = gql`{
-      members (where: {address: "${props.address}", dao: "${props.dao.address}"}) {
-        id
-        address
-        dao {
-          id
-        }
-        reputation
-        tokens
-      }
-    }`;
-    const itemMap = (item: any) => {
-      return {
-        address: item.address,
-        // dao: new DAO(item.dao.id, this.context),
-        id: item.id,
-        reputation: new BN(item.reputation),
-        tokens: new BN(item.tokens)
-      };
-    };
     const arc = getArc();
+    const dao = arc.dao(props.dao.address);
 
-    return <Subscribe observable={arc.getObservable(query)}>{(state: IObservableState<any>) => {
+    const observable = combineLatest(
+      dao.member(props.address).state,
+      arc.getBalance(props.address),
+      arc.GENToken().balanceOf(props.address),
+      arc.allowance(props.address)
+    );
+
+    return <Subscribe observable={observable}>{(state: IObservableState<[IMemberState, BN, BN, any]>) => {
         if (state.isLoading) {
           return <div>loading..</div>;
         } else if (state.error) {
           return <div>{state.error}</div>;
         } else {
-          // state.data is a list
-          const members = state.data.data.members;
-          let member: any;
-          if (members.length === 0) {
-            member = {
-              name: "not found",
-              id: "0x",
-              address: "0x",
-              reputation: new BN(0),
-              tokens: new BN(0)
-            };
-          } else {
-            member = itemMap(members[0]);
-          }
-          return <AccountBalances dao={props.dao} member={member} />;
+          return <AccountBalances dao={props.dao} member={state.data[0]} ethBalance={state.data[1]} genBalance={state.data[2]} genAllowance={new BN(state.data[3].amount)} />;
         }
       }
     }</Subscribe>;
