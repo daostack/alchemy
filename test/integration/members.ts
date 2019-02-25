fcb8398ad1375e09237bd6cb1f40fed4e677f9c1
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2019-02-25 11:16:52 +0100

    fix member test

--- test/integration/members.ts
+++ test/integration/members.ts
@@ -1,30 +1,30 @@
-import { getContractAddresses, userAddresses } from './utils'
+import { getContractAddresses, userAddresses } from "./utils";
 
-describe('Members page', () => {
-    let addresses: string[]
-    let daoAddress: string
+describe("Members page", () => {
+    let addresses: string[];
+    let daoAddress: string;
 
     before(() => {
       // global.expect = chai.expect
-      addresses = getContractAddresses()
-      daoAddress = addresses.Avatar.toLowerCase()
-      browser.url(`http://127.0.0.1:3000`)
-    })
+      addresses = getContractAddresses();
+      daoAddress = addresses.Avatar.toLowerCase();
+      browser.url(`http://127.0.0.1:3000`);
+    });
 
-    it('should exist', () => {
-      browser.url(`http://127.0.0.1:3000/dao/${daoAddress}/members`)
-      browser.getTitle().should.be.equal('Alchemy | DAOstack')
+    it("should exist", () => {
+      browser.url(`http://127.0.0.1:3000/dao/${daoAddress}/members`);
+      browser.getTitle().should.be.equal("Alchemy | DAOstack");
 
       /// skip the tour
-      if (browser.isVisible('*[data-test-id="skip-tour"]')) {
-        browser.click('*[data-test-id="skip-tour"]')
+      if (browser.isVisible("*[data-test-id=\"skip-tour\"]")) {
+        browser.click("*[data-test-id=\"skip-tour\"]");
       }
 
       // check if we see a member
-      const address = userAddresses[0]
-      browser.waitForExist(`*[data-test-id="member_${address}"]`)
+      const address = userAddresses[0];
+      browser.waitForExist(`*[data-test-id="member_${address}"]`);
       // TODO: we should see the repuation of this member
       browser.getText(`*[data-test-id="member_${address}"] *[data-test-id="reputation"]`)
-        .should.have.string("0.00") // TODO: should be 16.67 when migration adds founder rep as wei
-    })
-})
+        .should.have.string("16.67"); // TODO: should be 16.67 when migration adds founder rep as wei
+    });
+});
