414a46fae6190d2bc64dfa7ca6c0fe23a09a0ee8
Aaron Brodeur <aaronb@gmail.com>
2019-03-08 12:02:03 -0800

    Added actions to bottom of proposal

--- src/components/Proposal/ProposalContainer.tsx
+++ src/components/Proposal/ProposalContainer.tsx
@@ -399,22 +399,35 @@ class ProposalContainer extends React.Component<IProps, IState> {
             <TransferDetails proposal={proposal} dao={dao} beneficiaryProfile={beneficiaryProfile} detailView={detailView}/>
 
             {this.props.detailView ?
-              <div className={executeButtonClass}>
-                {proposal.stage == IProposalStage.PreBoosted ?
-                  <button className={css.boostProposal} onClick={this.handleClickExecute.bind(this)}>
-                    <img src="/assets/images/Icon/boost.svg"/>
-                    <span> Boost</span>
-                  </button>
-                  :
-                  <button className={css.executeProposal} onClick={this.handleClickExecute.bind(this)}>
-                    <img src="/assets/images/Icon/execute.svg"/>
-                    <span> Execute</span>
-                  </button>
-                }
-              </div>
+                <div className={executeButtonClass}>
+                  {proposal.stage == IProposalStage.PreBoosted ?
+                    <button className={css.boostProposal} onClick={this.handleClickExecute.bind(this)}>
+                      <img src="/assets/images/Icon/boost.svg"/>
+                      <span> Boost</span>
+                    </button>
+                    : executable ?
+                    <button className={css.executeProposal} onClick={this.handleClickExecute.bind(this)}>
+                      <img src="/assets/images/Icon/execute.svg"/>
+                      <span> Execute</span>
+                    </button>
+                    : 
+                    <div>
+                      <VoteBox
+                        buttonsOnly={true}
+                        isVotingNo={isVotingNo}
+                        isVotingYes={isVotingYes}
+                        currentVote={currentAccountVote}
+                        currentAccount={currentAccount}
+                        dao={dao}
+                        proposal={proposal}
+                        voteOnProposal={voteOnProposal}
+                        detailView={detailView}
+                      />
+                    </div>
+                  }
+                </div>
               : " "
             }
-
           </div>
 
           <div className={css.proposalActions + " " + css.clearfix}>
