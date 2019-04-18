539a04d1cc928bf36905b48babd2efada47e4c12
Tibet Sprague <tibet.sprague@gmail.com>
2019-04-18 09:42:46 -0700

    Add environment to sentry tracking

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,8 @@ async function renderApp() {
   library.add(faGithub, faTwitter, faFacebook);
 
   Sentry.init({
-    dsn: "https://748c6f9811fe407ca2853b64bf638690@sentry.io/1419793"
+    dsn: "https://748c6f9811fe407ca2853b64bf638690@sentry.io/1419793",
+    environment: process.env.NODE_ENV
   });
 
   ReactDOM.render(
