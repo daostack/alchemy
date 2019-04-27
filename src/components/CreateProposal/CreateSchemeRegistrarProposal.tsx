12da84a680f05a4476715e06dffc2ac463e8f22b
Aaron Brodeur <aaronb@gmail.com>
2019-04-26 17:51:20 -0700

    Styled scheme registrat proposal creation

--- src/components/CreateProposal/CreateSchemeRegistrarProposal.tsx
+++ src/components/CreateProposal/CreateSchemeRegistrarProposal.tsx
@@ -110,193 +110,215 @@ class CreateProposalContainer extends React.Component<IProps, null> {
           const proposalDescriptions: string[] = [];
 
           return (
-            <div className={css.createProposalWrapper}>
-              <h2>
-                <span>+ New proposal <b>| Scheme Registrar</b></span>
-              </h2>
-              <Formik
-                initialValues={{
-                  description: "",
-                  otherScheme: "",
-                  parametersHash: "",
-                  permissions: {
-                    registerSchemes: false,
-                    changeConstraints: false,
-                    upgradeController: false,
-                    genericCall: false
-                  },
-                  scheme: "",
-                  title: "",
-                  url: ""
-                } as FormValues}
-                validate={(values: FormValues) => {
-                  const errors: any = {};
+            <div className={css.createProposalWrapper + " " + css.schemeRegistrar}>
+                <h2>
+                  <span>+ New proposal <b>| Scheme Registrar</b></span>
+                </h2>
+                <div className={css.schemeRegistrarSidebar}>
+                <button className={css.addScheme + " " + css.selected}>
+                  <span></span>
+                  Add Scheme
+                </button>
+                <div className={css.schemeItem}>
+                  <span className={css.schemeTitle}>Scheme name</span>
+                  <button>
+                    <span></span>
+                    Remove Scheme
+                  </button>
+                  <button>
+                    <span></span>
+                    Edit Scheme
+                  </button>
+                </div>
+              </div>
+              <div className={css.schemeRegistrarForm}>
+                <Formik
+                  initialValues={{
+                    description: "",
+                    otherScheme: "",
+                    parametersHash: "",
+                    permissions: {
+                      registerSchemes: false,
+                      changeConstraints: false,
+                      upgradeController: false,
+                      genericCall: false
+                    },
+                    scheme: "",
+                    title: "",
+                    url: ""
+                  } as FormValues}
+                  validate={(values: FormValues) => {
+                    const errors: any = {};
 
-                  const require = (name: string) => {
-                    if (!(values as any)[name]) {
-                      errors[name] = "Required";
+                    const require = (name: string) => {
+                      if (!(values as any)[name]) {
+                        errors[name] = "Required";
+                      }
+                    };
+
+                    if (values.title.length > 120) {
+                      errors.title = "Title is too long (max 120 characters)";
                     }
-                  };
 
-                  if (values.title.length > 120) {
-                    errors.title = "Title is too long (max 120 characters)";
+                    // TODO: do we want this uniqueness check still?
+                    if (proposalDescriptions.indexOf(values.description) !== -1) {
+                      errors.description = "Must be unique";
+                    }
+  {/*
+                    if (!this.web3.utils.isAddress(values.beneficiary)) {
+                      errors.beneficiary = "Invalid address";
+                    }*/}
+
+                    const pattern = new RegExp("(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})");
+                    if (values.url && !pattern.test(values.url)) {
+                      errors.url = "Invalid URL";
+                    }
+
+                    require("description");
+                    require("title");
+                    require("scheme");
+                    require("parametersHash");
+
+                    return errors;
+                  }}
+                  onSubmit={this.handleSubmit}
+                  render={({
+                    errors,
+                    touched,
+                    handleSubmit,
+                    isSubmitting,
+                    setFieldTouched,
+                    setFieldValue,
+                    values
+                  }: FormikProps<FormValues>) =>
+                    <Form noValidate>
+
+                      <label htmlFor="titleInput">
+                        Title
+                        <ErrorMessage name='title'>{msg => <span className={css.errorMessage}>{msg}</span>}</ErrorMessage>
+                        <div className={css.requiredMarker}>*</div>
+                      </label>
+                      <Field
+                        autoFocus
+                        id="titleInput"
+                        maxLength={120}
+                        placeholder="Summarize your proposal"
+                        name="title"
+                        type="text"
+                        className={touched.title && errors.title ? css.error : null}
+                      />
+
+                      <label htmlFor="descriptionInput">
+                        Description
+                        <div className={css.requiredMarker}>*</div>
+                        <img className={css.infoTooltip} src="/assets/images/Icon/Info.svg"/>
+                        <ErrorMessage name='description'>{msg => <span className={css.errorMessage}>{msg}</span>}</ErrorMessage>
+                      </label>
+                      <Field
+                        component="textarea"
+                        id="descriptionInput"
+                        placeholder="Describe your proposal in greater detail"
+                        name="description"
+                        className={touched.description && errors.description ? css.error : null}
+                      />
+
+                      <label htmlFor="urlInput">
+                        URL
+                        <ErrorMessage name='url'>{msg => <span className={css.errorMessage}>{msg}</span>}</ErrorMessage>
+                      </label>
+                      <Field
+                        id="urlInput"
+                        maxLength={120}
+                        placeholder="Description URL"
+                        name="url"
+                        type="text"
+                        className={touched.url && errors.url ? css.error : null}
+                      />
+
+                      <div className={css.schemeFields}>
+                        <div className={css.schemeSelectContainer}>
+                          <label htmlFor="schemeInput">
+                            Scheme
+                            <ErrorMessage name='scheme'>{msg => <span className={css.errorMessage}>{msg}</span>}</ErrorMessage>
+                            <div className={css.requiredMarker}>*</div>
+                          </label>
+                          <Field
+                            id="schemeInput"
+                            name="scheme"
+                            component="select"
+                            className={css.schemeSelect}
+                          >
+                            <option value={arc.contractAddresses.base.ContributionReward}>Contribution Reward</option>
+                            <option value={arc.contractAddresses.base.SchemeRegistrar}>Scheme Registrar</option>
+                            <option value={arc.contractAddresses.base.GenericScheme}>Generic Scheme</option>
+                            <option value="Other">Other</option>
+                          </Field>
+                          <Field
+                            id="otherSchemeInput"
+                            placeholder="Type name"
+                            name="otherScheme"
+                            className={touched.otherScheme && errors.otherScheme ? css.error : null + " " + css.otherScheme}
+                          />
+                        </div>
+
+                        <div>
+                          <label htmlFor="parametersHashInput">
+                            Parameter Hash
+                            <ErrorMessage name='parametersHash'>{msg => <span className={css.errorMessage}>{msg}</span>}</ErrorMessage>
+                          </label>
+                          <Field
+                            id="parametersHashInput"
+                            placeholder="Insert hash"
+                            name="parametersHash"
+                            className={touched.parametersHash && errors.parametersHash ? css.error : null}
+                          />
+                        </div>
+                        <div className={css.permissions}>
+                          <div className={css.permissionsLabel}>
+                            Permissions
+                          </div>
+                          <div className={css.permissionCheckbox}>
+                            <Field id="registerOtherSchemesInput" type="checkbox" name="permissions.registerSchemes" />
+                            <label htmlFor="registerOtherSchemesInput">
+                              Register other schemes
+                            </label>
+                          </div>
+
+                          <div className={css.permissionCheckbox}>
+                            <Field id="changeConstraintsInput" type="checkbox" name="permissions.changeConstraints" />
+                            <label htmlFor="changeConstraintsInput">
+                              Add/remove global constraints
+                            </label>
+                          </div>
+
+                          <div className={css.permissionCheckbox}>
+                            <Field id="upgradeControllerInput" type="checkbox" name="permissions.upgradeController" />
+                            <label htmlFor="upgradeControllerInput">
+                              Upgrade the controller
+                            </label>
+                          </div>
+
+                          <div className={css.permissionCheckbox}>
+                            <Field id="genericCallInput" type="checkbox" name="permissions.genericCall" />
+                            <label htmlFor="genericCallInput">
+                              Call genericCall on behalf of
+                            </label>
+                          </div>
+                        </div>
+                      </div>
+                      <div className={css.createProposalActions}>
+                        <button className={css.exitProposalCreation} type="button" onClick={this.goBack.bind(this)}>
+                          Cancel
+                        </button>
+                        <button className={css.submitProposal} type="submit" disabled={isSubmitting}>
+                          Submit proposal
+                        </button>
+                      </div>
+                    </Form>
                   }
+                />
 
-                  // TODO: do we want this uniqueness check still?
-                  if (proposalDescriptions.indexOf(values.description) !== -1) {
-                    errors.description = "Must be unique";
-                  }
-{/*
-                  if (!this.web3.utils.isAddress(values.beneficiary)) {
-                    errors.beneficiary = "Invalid address";
-                  }*/}
-
-                  const pattern = new RegExp("(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})");
-                  if (values.url && !pattern.test(values.url)) {
-                    errors.url = "Invalid URL";
-                  }
-
-                  require("description");
-                  require("title");
-                  require("scheme");
-                  require("parametersHash");
-
-                  return errors;
-                }}
-                onSubmit={this.handleSubmit}
-                render={({
-                  errors,
-                  touched,
-                  handleSubmit,
-                  isSubmitting,
-                  setFieldTouched,
-                  setFieldValue,
-                  values
-                }: FormikProps<FormValues>) =>
-                  <Form noValidate>
-
-                    <label htmlFor="titleInput">
-                      Title
-                      <ErrorMessage name='title'>{msg => <span className={css.errorMessage}>{msg}</span>}</ErrorMessage>
-                      <div className={css.requiredMarker}>*</div>
-                    </label>
-                    <Field
-                      autoFocus
-                      id="titleInput"
-                      maxLength={120}
-                      placeholder="Summarize your proposal"
-                      name="title"
-                      type="text"
-                      className={touched.title && errors.title ? css.error : null}
-                    />
-
-                    <label htmlFor="descriptionInput">
-                      Description
-                      <div className={css.requiredMarker}>*</div>
-                      <img className={css.infoTooltip} src="/assets/images/Icon/Info.svg"/>
-                      <ErrorMessage name='description'>{msg => <span className={css.errorMessage}>{msg}</span>}</ErrorMessage>
-                    </label>
-                    <Field
-                      component="textarea"
-                      id="descriptionInput"
-                      placeholder="Describe your proposal in greater detail"
-                      name="description"
-                      className={touched.description && errors.description ? css.error : null}
-                    />
-
-                    <label htmlFor="urlInput">
-                      URL
-                      <ErrorMessage name='url'>{msg => <span className={css.errorMessage}>{msg}</span>}</ErrorMessage>
-                    </label>
-                    <Field
-                      id="urlInput"
-                      maxLength={120}
-                      placeholder="Description URL"
-                      name="url"
-                      type="text"
-                      className={touched.url && errors.url ? css.error : null}
-                    />
-
-                    <div className={css.schemeFields}>
-                      <div className={css.schemeSelectContainer}>
-                        <label htmlFor="schemeInput">
-                          Scheme
-                          <ErrorMessage name='scheme'>{msg => <span className={css.errorMessage}>{msg}</span>}</ErrorMessage>
-                          <div className={css.requiredMarker}>*</div>
-                        </label>
-                        <Field
-                          id="schemeInput"
-                          name="scheme"
-                          component="select"
-                          className={css.schemeSelect}
-                        >
-                          <option value={arc.contractAddresses.base.ContributionReward}>Contribution Reward</option>
-                          <option value={arc.contractAddresses.base.SchemeRegistrar}>Scheme Registrar</option>
-                          <option value={arc.contractAddresses.base.GenericScheme}>Generic Scheme</option>
-                          <option value="Other">Other</option>
-                        </Field>
-                        <Field
-                          id="otherSchemeInput"
-                          placeholder="Type name"
-                          name="otherScheme"
-                          className={touched.otherScheme && errors.otherScheme ? css.error : null}
-                        />
-                      </div>
-
-                      <div>
-                        <label htmlFor="parametersHashInput">
-                          Parameter Hash
-                          <ErrorMessage name='parametersHash'>{msg => <span className={css.errorMessage}>{msg}</span>}</ErrorMessage>
-                        </label>
-                        <Field
-                          id="parametersHashInput"
-                          placeholder="Insert hash"
-                          name="parametersHash"
-                          className={touched.parametersHash && errors.parametersHash ? css.error : null}
-                        />
-                      </div>
-
-                      <div className={css.permissionCheckbox}>
-                        <label htmlFor="registerOtherSchemesInput">
-                          Register other schemes
-                        </label>
-                        <Field id="registerOtherSchemesInput" type="checkbox" name="permissions.registerSchemes" />
-                      </div>
-
-                      <div className={css.permissionCheckbox}>
-                        <label htmlFor="changeConstraintsInput">
-                          Add/remove global constraints
-                        </label>
-                        <Field id="changeConstraintsInput" type="checkbox" name="permissions.changeConstraints" />
-                      </div>
-
-                      <div className={css.permissionCheckbox}>
-                        <label htmlFor="upgradeControllerInput">
-                          Upgrade the controller
-                        </label>
-                        <Field id="upgradeControllerInput" type="checkbox" name="permissions.upgradeController" />
-                      </div>
-
-                      <div className={css.permissionCheckbox}>
-                        <label htmlFor="genericCallInput">
-                          Call genericCall on behalf of
-                        </label>
-                        <Field id="genericCallInput" type="checkbox" name="permissions.genericCall" />
-                      </div>
-                    </div>
-
-                    <div className={css.createProposalActions}>
-                      <button className={css.exitProposalCreation} type="button" onClick={this.goBack.bind(this)}>
-                        Cancel
-                      </button>
-                      <button className={css.submitProposal} type="submit" disabled={isSubmitting}>
-                        Submit proposal
-                      </button>
-                    </div>
-                  </Form>
-                }
-              />
-
+              </div>
             </div>
           );
         } else {
