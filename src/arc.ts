474c01a2cf5f8cc16c9291b2b6a333c40dfc7fd7
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2019-05-02 17:47:24 +0200

    use v16-without-uc on staging

--- src/arc.ts
+++ src/arc.ts
@@ -12,8 +12,8 @@ const settings = {
     contractAddresses: getContractAddresses("private")
   },
   staging: {
-    graphqlHttpProvider: "https://rinkeby.subgraph.daostack.io/subgraphs/name/v16",
-    graphqlWsProvider: "wss://ws.rinkeby.subgraph.daostack.io/subgraphs/name/v16",
+    graphqlHttpProvider: "https://rinkeby.subgraph.daostack.io/subgraphs/name/v16-without-uc",
+    graphqlWsProvider: "wss://ws.rinkeby.subgraph.daostack.io/subgraphs/name/v16-without-uc",
     web3Provider: `wss://rinkeby.infura.io/ws/v3/e0cdf3bfda9b468fa908aa6ab03d5ba2`,
     ipfsProvider: {
       "host": "rinkeby.subgraph.daostack.io",
