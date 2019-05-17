3536cb157b9297e9a17142b55047a6a708cfec95
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2019-05-17 18:44:12 +0200

    first steps towards schemeRegistry

--- src/components/CreateProposal/SchemeForms/CreateDutchXProposal.tsx
+++ src/components/CreateProposal/SchemeForms/CreateDutchXProposal.tsx
@@ -8,6 +8,7 @@ import * as React from "react";
 import { connect } from "react-redux";
 import { IRootState } from "reducers";
 import { showNotification } from "reducers/notifications";
+import { GenericSchemeInfo, GenericSchemeRegistry, IActionSpec } from "../../../genericSchemeRegistry";
 import * as css from "../CreateProposal.scss";
 
 interface IStateProps {
@@ -35,27 +36,20 @@ interface FormValues {
   description: string;
   title: string;
   url: string;
-
   auctionThreshold: number;
   newContractAddress: string; // update mastercopy & Change ETH:USD oracle
   newOwnerAddress: string; // Change DutchX owner
   tokenWhitelist: string[];
   tokenPairThreshold: number;
+  [key: string]: any; // TODO: "allowSyntheticDefaultImports": true in tsconfig.json should render this unecessary. But it is needed anyway
+
 }
 
 interface IState {
-  currentTab: string;
+  actions: IActionSpec[];
+  currentAction: IActionSpec;
+  genericSchemeInfo: GenericSchemeInfo;
 }
-
-const actions: { [ tabName: string]: string } = {
-  "updateMasterCopy": "Update Mastercopy",
-  "changeOracle": "Change ETH:USD Oracle",
-  "changeOwner": "Change DutchX owner",
-  "addTokens": "Add Tokens",
-  "removeTokens": "Remove Tokens",
-  "tokenThreshold": "Update new token pair threshold",
-  "auctionThreshold": "Update new auction threshold"
-};
 
 class CreateDutchXProposalContainer extends React.Component<IProps, IState> {
 
@@ -64,68 +58,62 @@ class CreateDutchXProposalContainer extends React.Component<IProps, IState> {
 
     this.handleSubmit = this.handleSubmit.bind(this);
 
-    this.state = { currentTab: "updateMasterCopy" };
+    const genericSchemeRegistry = new GenericSchemeRegistry();
+    const genericSchemeInfo = genericSchemeRegistry.genericSchemeInfo("dxDAO");
+    const actions = genericSchemeInfo.actions();
+    this.state = {
+      actions,
+      currentAction:  actions[0],
+      genericSchemeInfo
+    };
   }
 
   public async handleSubmit(values: FormValues, { setSubmitting }: any ) {
     if (!(await checkMetaMaskAndWarn(this.props.showNotification))) { return; }
 
-    //const currentTab = this.state.currentTab;
-    const proposalValues = {...values,
-      type: IProposalType.GenericScheme
-      // type: this.state.currentTab === "removeScheme" ? IProposalType.SchemeRegistrarRemove : this.state.currentTab === "addScheme" ? IProposalType.SchemeRegistrarAdd : IProposalType.SchemeRegistrarEdit,
-      // parametersHash: values.parametersHash,
-      // permissions: "0x" + permissions.toString(16).padStart(8, "0"),
-      // scheme: currentTab === "addScheme" ? (values.schemeToAdd === "Other" ? values.otherScheme : values.schemeToAdd) :
-      //         currentTab === "editScheme" ? values.schemeToEdit :
-      //         values.schemeToRemove
-    };
+    // const proposalValues = {...values,
+    //   type: IProposalType.GenericScheme
+    // };
 
+    const currentAction = this.state.currentAction;
+    const callValues = [];
+    for (let input of currentAction.abi.inputs) {
+      callValues.push(values[input.name].value);
+    }
+    const callData = this.state.genericSchemeInfo.encodeABI(currentAction, callValues);
     setSubmitting(false);
-    await this.props.createProposal(this.props.daoAvatarAddress, proposalValues);
+    await this.props.createProposal(
+      this.props.daoAvatarAddress,
+      { type: IProposalType.GenericScheme,
+        callData
+      }
+    );
     this.props.handleClose();
   }
 
   public handleTabClick = (tab: string) => (e: any) => {
-    this.setState({ currentTab: tab });
+    this.setState({ currentAction: this.state.genericSchemeInfo.action(tab) });
   }
 
   public render() {
     const { handleClose } = this.props;
-    const currentTab = this.state.currentTab;
 
-//    const arc = getArc();
-
-    const tabClasses: { [key: string]: any } = Object.keys(actions).reduce((classes: { [key: string]: any }, tab) => {
-      classes[tab] = classNames({
-        [css.tab]: true,
-        [css.selected]: currentTab === tab
-      });
-      return classes;
-    }, {});
-
-    // const addSchemeButtonClass = ;
-    // const editSchemeButtonClass = classNames({
-    //   [css.selected]: currentTab === "editScheme"
-    // });
-    // const removeSchemeButtonClass = classNames({
-    //   [css.selected]: currentTab === "removeScheme"
-    // });
-
-    // const schemeRegistrarFormClass = classNames({
-    //   [css.schemeRegistrarForm]: true,
-    //   [css.addScheme]: currentTab === "addScheme",
-    //   [css.removeScheme]: currentTab === "removeScheme",
-    //   [css.editScheme]: currentTab === "editScheme"
-    // });
+    const actions = this.state.actions;
+    const currentAction = this.state.currentAction;
 
     return (
       <div className={css.createWrapperWithSidebar}>
         <div className={css.sidebar}>
-          {Object.keys(actions).map((tab) =>
-            <button className={tabClasses[tab]} onClick={this.handleTabClick(tab)}>
+          { actions.map((action) =>
+            <button
+              key={action.id}
+              className={classNames({
+                [css.tab]: true,
+                [css.selected]: currentAction.id === action.id
+              })}
+              onClick={this.handleTabClick(action.id)}>
               <span></span>
-              {actions[tab]}
+              { action.label }
             </button>
           )}
         </div>
@@ -146,14 +134,14 @@ class CreateDutchXProposalContainer extends React.Component<IProps, IState> {
             validate={(values: FormValues) => {
               const errors: any = {};
 
-              const require = (name: string) => {
+              const valueIsRequired = (name: string) => {
                 if (!(values as any)[name]) {
                   errors[name] = "Required";
                 }
               };
 
-              require("description");
-              require("title");
+              valueIsRequired("description");
+              valueIsRequired("title");
 
               if (values.title.length > 120) {
                 errors.title = "Title is too long (max 120 characters)";
@@ -164,40 +152,13 @@ class CreateDutchXProposalContainer extends React.Component<IProps, IState> {
                 errors.url = "Invalid URL";
               }
 
-              switch (currentTab) {
-                case "updateMasterCopy":
-                  require("newContractAddress");
-                  break
-                case "changeOracle":
-                  require("newContractAddress");
-                  break;
-                case "changeOwner":
-                  require("newOwnerAddress");
-                  break;
-                case "addTokens":
-                  require("tokenWhitelist");
-                  break;
-                case "tokenThreshold":
-                  require("tokenPairThreshold");
-                  break;
-                case "auctionThreshold":
-                  require("auctionThreshold");
-                  break;
+              for (let input of this.state.currentAction.abi.inputs) {
+                valueIsRequired(input.name);
               }
-
-              {/*if (currentTab === "addScheme" && values.otherScheme && !arc.web3.utils.isAddress(values.otherScheme)) {
-                errors.otherScheme = "Invalid address";
-              }*/}
-
-{/*              const parametersHashPattern = /0x([\da-f]){64}/i;
-              if (currentTab !== "removeScheme" && values.parametersHash && !parametersHashPattern.test(values.parametersHash)) {
-                errors.parametersHash = "Invalid parameters hash";
-              }
-*/}
 
               return errors;
             }}
-            onSubmit={this.handleSubmit}
+            onSubmit={this.handleSubmit.bind(this)}
             render={({
               errors,
               touched,
@@ -250,119 +211,35 @@ class CreateDutchXProposalContainer extends React.Component<IProps, IState> {
                     type="text"
                     className={touched.url && errors.url ? css.error : null}
                   />
-
-                  <div className={tabClasses["updateMasterCopy"]}>
+                  <div key={currentAction.id}
+                       className={classNames({
+                          [css.tab]: true,
+                          [css.selected]: this.state.currentAction.id === currentAction.id
+                        })
+                      }
+                    >
                     <div>
-                      <label htmlFor="newContractAddress">
-                        New Contract Address
+                      <label htmlFor={currentAction.id }>
+                        { currentAction.label }
                         <ErrorMessage name="newContractAddress">{(msg) => <span className={css.errorMessage}>{msg}</span>}</ErrorMessage>
                         <div className={css.requiredMarker}>*</div>
                       </label>
-                      <Field
-                        id="newContractAddress"
-                        placeholder="New contract address"
-                        name="newContractAddress"
-                        className={touched.newContractAddress && errors.newContractAddress ? css.error : null}
-                      />
-                    </div>
-                  </div>
+                      {
+                        currentAction.abi.inputs.map((input: { name: string, type: string}) => {
+                          // TODO: show different field depending on input.type
+                          const name: string = input.name;
+                          return <Field
+                            id={name}
+                            key={name}
+                            placeholder={`${name} --- type is: ${input.type}`}
+                            name={name}
+                            /* why does the next line not wokr? */
+                            /* className={touched[name] && (errors[name] ? css.error : null} */
+                          />;
 
-                  <div className={tabClasses["changeOracle"]}>
-                    <div>
-                      <label htmlFor="newContractAddress">
-                        New Contract Address
-                        <ErrorMessage name="newContractAddress">{(msg) => <span className={css.errorMessage}>{msg}</span>}</ErrorMessage>
-                        <div className={css.requiredMarker}>*</div>
-                      </label>
-                      <Field
-                        id="newContractAddress"
-                        placeholder="skldfjsjh"
-                        name="newContractAddress"
-                        className={touched.newContractAddress && errors.newContractAddress ? css.error : null}
-                      />
-                    </div>
+                        })
+                      }
                   </div>
-
-                  <div className={tabClasses["changeOwner"]}>
-                    <div>
-                      <label htmlFor="newOwnerAddress">
-                        New Owner Address
-                        <ErrorMessage name="newOwnerAddress">{(msg) => <span className={css.errorMessage}>{msg}</span>}</ErrorMessage>
-                        <div className={css.requiredMarker}>*</div>
-                      </label>
-                      <Field
-                        id="newOwnerAddress"
-                        placeholder="skldfjsjh"
-                        name="newOwnerAddress"
-                        className={touched.newContractAddress && errors.newContractAddress ? css.error : null}
-                      />
-                    </div>
-                  </div>
-
-                  <div className={tabClasses["addTokens"]}>
-                    <div>
-                      <label htmlFor="newOwnerAddress">
-                        New Owner Address
-                        <ErrorMessage name="newOwnerAddress">{(msg) => <span className={css.errorMessage}>{msg}</span>}</ErrorMessage>
-                        <div className={css.requiredMarker}>*</div>
-                      </label>
-                      <Field
-                        id="newOwnerAddress"
-                        placeholder="skldfjsjh"
-                        name="newOwnerAddress"
-                        className={touched.newContractAddress && errors.newContractAddress ? css.error : null}
-                      />
-                    </div>
-                  </div>
-
-                  <div className={tabClasses["removeTokens"]}>
-                    <div>
-                      <label htmlFor="newOwnerAddress">
-                        New Owner Address
-                        <ErrorMessage name="newOwnerAddress">{(msg) => <span className={css.errorMessage}>{msg}</span>}</ErrorMessage>
-                        <div className={css.requiredMarker}>*</div>
-                      </label>
-                      <Field
-                        id="newOwnerAddress"
-                        placeholder="skldfjsjh"
-                        name="newOwnerAddress"
-                        className={touched.newContractAddress && errors.newContractAddress ? css.error : null}
-                      />
-                    </div>
-                  </div>
-
-                  <div className={tabClasses["tokenThreshold"]}>
-                    <div>
-                      <label htmlFor="tokenPairThreshold">
-                        New token threshold
-                        <ErrorMessage name="tokenPairThreshold">{(msg) => <span className={css.errorMessage}>{msg}</span>}</ErrorMessage>
-                        <div className={css.requiredMarker}>*</div>
-                      </label>
-                      <Field
-                        id="tokenPairThreshold"
-                        placeholder="skldfjsjh"
-                        name="tokenPairThreshold"
-                        type="number"
-                        className={touched.tokenPairThreshold && errors.tokenPairThreshold ? css.error : null}
-                      />
-                    </div>
-                  </div>
-
-                  <div className={tabClasses["auctionThreshold"]}>
-                    <div>
-                      <label htmlFor="auctionThreshold">
-                        New auction threshold
-                        <ErrorMessage name="auctionThreshold">{(msg) => <span className={css.errorMessage}>{msg}</span>}</ErrorMessage>
-                        <div className={css.requiredMarker}>*</div>
-                      </label>
-                      <Field
-                        id="auctionThreshold"
-                        placeholder="skldfjsjh"
-                        name="auctionThreshold"
-                        type="number"
-                        className={touched.auctionThreshold && errors.auctionThreshold ? css.error : null}
-                      />
-                    </div>
                   </div>
 
                   <div className={css.createProposalActions}>
---
+++ src/genericSchemeRegistry/index.ts
@@ -0,0 +1,80 @@
+const Web3 = require("web3");
+const dxDAOInfo = require("./dxDao.json");
+
+interface IABISpec {
+  constant: boolean;
+  name: string;
+  inputs: Array<{ name: string, type: string}>;
+  outputs: any[];
+  payable: boolean;
+  stateMutability: string;
+  type: string;
+}
+export interface IActionSpec {
+  id: string;
+  label: string;
+  abi: IABISpec;
+  notes: string;
+}
+export interface IGenericSchemeJSON {
+  name: string;
+  addresses: any[];
+  actions: IActionSpec[];
+}
+
+export class GenericSchemeInfo {
+  public specs: IGenericSchemeJSON;
+
+  constructor(info: IGenericSchemeJSON) {
+    this.specs = info;
+  }
+  public actions() {
+    return this.specs.actions;
+  }
+  public action(id: string) {
+    for (let action of this.specs.actions) {
+      if (action.id === id) {
+        return action;
+      }
+    }
+    return;
+  }
+  public actionByFunctionName(name: string) {
+    for (let action of this.specs.actions) {
+        if (action.abi.name === name) {
+          return action;
+        }
+      }
+    return;
+  }
+  public encodeABI(action: IActionSpec, ...values: any[]) {
+    return Web3.eth.abi.encodeFunctionCall(action.abi, values);
+  }
+
+  /*
+   * Tries to find a function corresponding to the calldata among this.actions()
+   * returns: the action, and the decoded values
+   */
+  public decodeCallData(callData: string): { action: IActionSpec, values: {[key: string]: any}} {
+    // TODO: still to be written, sorry :-)
+    const functionName = "startMasterCopyCountdown";
+    const values = { _masterCopy: "0xSomething"};
+    const action = this.actionByFunctionName(functionName);
+    return {action,  values};
+  }
+}
+
+export class GenericSchemeRegistry {
+  /**
+   * Check the address to see if this is a known contract, and if so
+   * return an object with information on how to call it
+   * @param  address an ethereum address
+   * @return an object [specs to be written..]
+   */
+  public genericSchemeInfo(name: string) {
+    if (name === "dxDAO") {
+      return new GenericSchemeInfo(dxDAOInfo);
+    }
+  }
+
+}
