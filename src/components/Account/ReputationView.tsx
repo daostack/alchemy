9591aa4b4e3d894bf8912c6fe0e6f2724a246fdf
Aaron Brodeur <aaronbrodeur@Aarons-MacBook-Pro.local>
2019-05-23 17:26:37 -0700

    Added total reputation per user to reputation view

--- src/components/Account/ReputationView.tsx
+++ src/components/Account/ReputationView.tsx
@@ -2,6 +2,7 @@ import BN = require("bn.js");
 import Util from "lib/util";
 import Tooltip from "rc-tooltip";
 import * as React from "react";
+import * as css from "./ReputationView.scss";
 
 interface IProps {
   daoName?: string;
@@ -17,7 +18,8 @@ export default class ReputationView extends React.Component<IProps, null> {
     return (
       <Tooltip placement="bottom" overlay={<span>{Util.fromWei(reputation).toLocaleString(undefined, {minimumFractionDigits: 0, maximumFractionDigits: 2})} {daoName || ""} Reputation in total</span>}>
         <span data-test-id="reputation">
-          {(totalReputation.gt(new BN(0)) ? 100 * Util.fromWei(reputation) / Util.fromWei(totalReputation) : 0).toLocaleString(undefined, {minimumFractionDigits: 0, maximumFractionDigits: 2})}% {hideSymbol ? "" : "Rep."}
+          <span className={css.reputationAmount}>{Util.fromWei(reputation).toLocaleString(undefined, {minimumFractionDigits: 0, maximumFractionDigits: 2})}</span>
+          ({(totalReputation.gt(new BN(0)) ? 100 * Util.fromWei(reputation) / Util.fromWei(totalReputation) : 0).toLocaleString(undefined, {minimumFractionDigits: 0, maximumFractionDigits: 2})}% {hideSymbol ? "" : "Rep."})
         </span>
       </Tooltip>
     );
