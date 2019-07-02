d16f7fd3f0bc164088a7ad97e9ef4f846ef384c6
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2019-07-02 15:50:35 +0200

    very simple paging

--- src/components/Shared/Subscribe.tsx
+++ src/components/Shared/Subscribe.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Observable, Subscription } from "rxjs";
+import { combineLatest, Observable, Subscription } from "rxjs";
 
 interface IProps {
   observable: Observable<any>;
@@ -11,10 +11,12 @@ export interface IObservableState<IData> {
   data: IData;
   error: Error;
   complete: boolean;
+  fetchMore?: (observable: any) => void;
 }
 
 export default class Subscribe extends React.Component<IProps, IObservableState<object>> {
   public subscription: Subscription;
+  public observable: Observable<any>;
 
   public state: IObservableState<object> = {
     isLoading: true,
@@ -27,13 +29,15 @@ export default class Subscribe extends React.Component<IProps, IObservableState<
   constructor(props: IProps) {
     super(props);
     this.id =  Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    this.observable = this.props.observable;
   }
   public setupSubscription() {
-    this.subscription = this.props.observable.subscribe(
+    this.subscription = this.observable.subscribe(
       (next: object) => {
         this.setState({
           data: next,
           isLoading: false,
+          fetchMore: (observable: any) => this.fetchMore({observable})
       });
       },
       (error: Error) => {
@@ -66,5 +70,19 @@ export default class Subscribe extends React.Component<IProps, IObservableState<
       return children(this.state);
     }
     throw Error(`Children of <Subscribe> must be a function`);
+  }
+
+  public fetchMore(options: {
+    // fetchMoreFunction: () => any
+    // updateQuery: (prevData: any, newData: any) => any
+    observable: any
+  }) {
+    // add more results to the query
+    const combine = (x1: any, x2: any) => {
+      return x1.concat(x2);
+    };
+    this.observable = combineLatest(this.observable, options.observable, combine);
+    this.setupSubscription();
+
   }
 }
