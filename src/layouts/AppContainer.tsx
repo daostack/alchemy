5313004544ffa2c5555d078e7166f53520742608
Doug Kent <github@douglaskent.com>
2019-07-13 23:54:11 -0400

    fix case of icon path

--- src/layouts/AppContainer.tsx
+++ src/layouts/AppContainer.tsx
@@ -194,7 +194,7 @@ class AppContainer extends React.Component<IProps, IState> {
             <div className={css.cookieDisclaimerContainer}>
               <div className={css.cookieDisclaimer}>
                 <div className={css.body}>This website stores cookies on your computer. These cookies are used to collect information about how you interact with our website and allow us to remember you. We use this information in order to improve and customize your browsing experience and for analytics. To find out more about the cookies we use, see our Privacy Policy.</div>
-                <div className={css.accept}><a href="#" onClick={this.handleAccept} className={css.blueButton}><img src="/assets/images/icon/v-white-thick.svg"></img>Accept</a></div>
+                <div className={css.accept}><a href="#" onClick={this.handleAccept} className={css.blueButton}><img src="/assets/images/Icon/v-white-thick.svg"></img>Accept</a></div>
               </div>
             </div>
           }
