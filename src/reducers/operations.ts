a1cb816f96fcfe69fa64a7299bfee4d76c0892c4
Doug Kent <github@douglaskent.com>
2019-07-19 12:18:37 -0400

    more from reducers/operations

--- src/reducers/operations.ts
+++ src/reducers/operations.ts
@@ -1,5 +1,4 @@
-import { Action, Middleware } from "redux";
-import { REHYDRATE, RehydrateAction } from "redux-persist";
+import { Action } from "redux";
 
 /** -- Model -- */
 
@@ -53,7 +52,7 @@ const isOperationsAction = (action: Action): action is OperationsAction =>
 /** -- Reducer -- */
 
 export const operationsReducer =
-  (state: IOperationsState = {}, a: Action) => {
+  (state: IOperationsState = {}, a: Action): IOperationsState => {
     if (isOperationsAction(a)) {
       if (a.type === "Operations/Update") {
         const action = a as IUpdateOperation;
@@ -75,112 +74,3 @@ export const operationsReducer =
 
     return state;
   };
-
-/** -- Effects -- */
-
-const errorType = (error: Error) => {
-  const message = error.message.toLowerCase();
-  if (message.includes("user denied")) {
-    return OperationError.Canceled;
-  } else if (message.includes("revert")) {
-    return OperationError.Reverted;
-  } else if (message.includes("gas")) {
-    return OperationError.OutOfGas;
-  } else {
-    return error.message;
-  }
-};
-
-export const operationsTracker: Middleware =
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  ({ dispatch }) =>
-    (next) => {
-    // Arc.TransactionService.subscribe("TxTracking", (topic, info: TransactionReceiptsEventInfo) => {
-    //   const {
-    //     invocationKey,
-    //     txStage,
-    //     tx,
-    //     error,
-    //     functionName
-    //   } = info;
-
-      //   if (txStage == Arc.TransactionStage.mined && !error) {
-      //     return;
-      //   }
-
-      //   // discard the `txEventContext` property since it's not serializable and irelevent.
-      //   const {txEventContext: _,  ...options} = info.options;
-
-      //   let proposalTitle = options.title;
-      //   if (options.proposalId) {
-      //     const proposal = (_getState() as any as IRootState).arc.proposals[options.proposalId];
-      //     if (proposal) {
-      //       proposalTitle = proposal.title;
-      //     }
-      //   }
-
-      //   dispatch({
-      //     type: "Operations/Update",
-      //     payload: {
-      //       id: `${invocationKey}`,
-      //       operation: {
-      //         txHash: tx,
-      //         error: error ? errorType(error) : undefined,
-      //         status:
-      //           txStage === Arc.TransactionStage.kickoff ?
-      //             OperationStatus.Started :
-      //           txStage === Arc.TransactionStage.sent ?
-      //             OperationStatus.Sent :
-      //             OperationStatus.Complete,
-      //         functionName,
-      //         options,
-      //         proposalTitle
-      //       }
-      //     }
-      //   } as IUpdateOperation);
-      // });
-
-      return (a: any) => {
-        if (a.type === REHYDRATE) {
-        /**
-         * Resubscribe to sent Operations after rehydrating.
-         */
-          const action = a as RehydrateAction;
-          const payload = action.payload;
-
-          if (payload) {
-            const state = payload.operations as IOperationsState;
-            Object.keys(state).forEach(async (id: string) => {
-              if (state[id].status && state[id].status === OperationStatus.Sent && !state[id].error) {
-                try {
-                //const receipt = await Arc.TransactionService.watchForConfirmedTransaction(state[id].txHash);
-                  dispatch({
-                    type: "Operations/Update",
-                    payload: {
-                      id: `${id}`,
-                      operation: {
-                        status: OperationStatus.Complete,
-                      },
-                    },
-                  } as IUpdateOperation);
-                } catch (e) {
-                  console.error(e);
-                  dispatch({
-                    type: "Operations/Update",
-                    payload: {
-                      id: `${id}`,
-                      operation: {
-                        error: errorType(e),
-                      },
-                    },
-                  } as IUpdateOperation);
-                }
-              }
-            });
-          }
-        }
-
-        next(a);
-      };
-    }
-;
