59e703f7484bb9ae892ea883d98997a44e1a7933
Doug Kent <github@douglaskent.com>
2019-07-31 20:33:20 -0400

    requeted review changes

--- src/arc.ts
+++ src/arc.ts
@@ -28,16 +28,16 @@ const web3ConnectProviderOptions =
           key: "pk_live_38A2BD2B1D4E9912",
         },
       }
-      : // (process.env.NODE_ENV === "staging") ?
-      {
-        portis: {
-          id: "aae9cff5-6e61-4b68-82dc-31a5a46c4a86",
-          network: "rinkeby",
-        },
-        fortmatic: {
-          key: "pk_test_659B5B486EF199E4",
-        },
-      });
+      : (process.env.NODE_ENV === "staging") ?
+        {
+          portis: {
+            id: "aae9cff5-6e61-4b68-82dc-31a5a46c4a86",
+            network: "rinkeby",
+          },
+          fortmatic: {
+            key: "pk_test_659B5B486EF199E4",
+          },
+        } : {});
 
 const settings = {
   dev: {
@@ -92,7 +92,6 @@ function getArcSettings(): any {
       break;
     }
     default: {
-      console.log(process.env.NODE_ENV === "development");
       throw Error(`Unknown NODE_ENV environment: "${process.env.NODE_ENV}"`);
     }
   }
