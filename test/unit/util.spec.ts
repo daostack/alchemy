1a469511e097d7962e7734ce98f881cdaea1f600
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2019-08-07 15:18:45 +0200

    add basic unit test for formatToken

---
+++ test/unit/util.spec.ts
@@ -0,0 +1,20 @@
+import { formatTokens } from "lib/util";
+
+const BN = require("bn.js");
+
+describe("Redemptions page", () => {
+
+  it("formatTokens should work as expected", async () => {
+    expect(formatTokens(new BN(0))).toEqual("0");
+    expect(formatTokens(new BN(1))).toEqual("+0");
+    expect(formatTokens(new BN("1000000000000000000"))).toEqual("1");
+    expect(formatTokens(new BN("1100000000000000000"))).toEqual("1.1");
+    expect(formatTokens(new BN("2345000000000000000"))).toEqual("2.34");
+    expect(formatTokens(new BN("2345000000000000000000"))).toEqual("2.34k");
+    expect(formatTokens(new BN("2345000000000000000000000"))).toEqual("2.34M");
+    expect(formatTokens(new BN("2222345000000000000000000000"))).toEqual("2,222.34M");
+    expect(formatTokens(new BN("0999999999999999999"))).toEqual("0.99");
+    expect(formatTokens(new BN("0099999999999999999"))).toEqual("0.09");
+    expect(formatTokens(new BN("0009999999999999999"))).toEqual("+0");
+  });
+});
