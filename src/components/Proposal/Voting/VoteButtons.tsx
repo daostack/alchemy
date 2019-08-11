1405522742c1d8bfbd632d961447293e40967e5d
Doug Kent <github@douglaskent.com>
2019-08-11 10:16:40 -0400

    fix layout when vote buttons are not showing

--- src/components/Proposal/Voting/VoteButtons.tsx
+++ src/components/Proposal/Voting/VoteButtons.tsx
@@ -199,7 +199,7 @@ class VoteButtons extends React.Component<IContainerProps, IState> {
                 </div>
                 :
                 <div className={css.votingDisabled}>
-                  <Tooltip overlay={tipContent}>
+                  <Tooltip placement="bottom" overlay={tipContent}>
                     <span>Voting disabled</span>
                   </Tooltip>
                 </div>
