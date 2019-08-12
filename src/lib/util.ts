0974c7bc0c45b638e6912604af8cce91e1c30847
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2019-08-12 17:29:26 +0200

    simpeler import

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -6,11 +6,10 @@ import {
   IProposalState,
   IRewardState,
   ISchemeState } from "@daostack/client";
-import { GenericSchemeRegistry } from "../genericSchemeRegistry";
+import { GenericSchemeRegistry } from "genericSchemeRegistry";
 import { getArc } from "../arc";
 
 import BN = require("bn.js");
-
 const Web3 = require("web3");
 const tokens = require("data/tokens.json");
 const exchangesList = require("data/exchangesList.json");
