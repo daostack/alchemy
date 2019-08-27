71add46fec5f50403f1bfa3fc24b3b179741e8c6
Doug Kent <github@douglaskent.com>
2019-08-27 14:15:20 -0400

    handle reputation percentage > 53 bits

--- src/components/Account/Reputation.tsx
+++ src/components/Account/Reputation.tsx
@@ -13,16 +13,38 @@ interface IProps {
 
 export default class Reputation extends React.Component<IProps, null> {
   public render() {
+    /**
+     * totalReputation is a BN in WEI
+     * reputation is a BN in WEI
+     */
     const { daoName, hideSymbol, hideTooltip, reputation, totalReputation } = this.props;
-    const PRECISION  = 2; // how many digits behind
+    const PRECISION = 2;
+    let percentageString: string;
     let percentage = 0;
-    if (totalReputation.gt(new BN(0))) {
-      percentage = new BN(100 * 10 ** PRECISION).mul(reputation).div(totalReputation).toNumber() / (10 ** PRECISION);
+
+    if (totalReputation.gtn(0)) {
+      const percentageBn = reputation.muln(10000).div(totalReputation);
+      if (percentageBn.bitLength() > 53) {
+        // eslint-disable-next-line no-console
+        console.log("");
+        percentageString = "NaN";
+      } else {
+        percentage = percentageBn.toNumber() / 100;
+      }
+    } else {
+      // eslint-disable-next-line no-console
+      console.log("totalReputation is <= 0");
+      percentageString = "NaN";
     }
-    let percentageString = percentage.toLocaleString(undefined, {minimumFractionDigits: PRECISION, maximumFractionDigits: PRECISION});
-    if (percentage === 0 && !reputation.isZero()) {
-      percentageString = `+${percentageString}`;
+
+    if (!percentageString) {
+      percentageString = percentage.toLocaleString(undefined, {minimumFractionDigits: PRECISION, maximumFractionDigits: PRECISION});
+      
+      if ((percentage === 0) && !reputation.isZero()) {
+        percentageString = `+${percentageString}`;
+      }
     }
+
     return (
       <Tooltip
         placement="bottom"
