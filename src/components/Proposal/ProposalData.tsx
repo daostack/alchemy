92a5b923e7dda9fe9300101bab25bef2b7344f3b
Tibet Sprague <tibet.sprague@gmail.com>
2019-09-04 17:13:47 -0700

    Get ProposalData render props component working with ProposalCard
    Required allowing for async createObservable functions to be passed to withSubscriptions

--- src/components/Proposal/ProposalData.tsx
+++ src/components/Proposal/ProposalData.tsx
@@ -2,32 +2,22 @@ import { Address, IDAOState, IMemberState, IProposalState, IRewardState, Proposa
 import { getArc } from "arc";
 import BN = require("bn.js");
 import withSubscription, { ISubscriptionProps } from "components/Shared/withSubscription";
+import * as moment from "moment";
 import * as React from "react";
 import { connect } from "react-redux";
 import { IRootState } from "reducers";
+import { closingTime } from "reducers/arcReducer";
 import { IProfileState } from "reducers/profilesReducer";
 import { combineLatest, concat, of, Observable } from "rxjs";
 import { map, mergeMap } from "rxjs/operators";
 
 import * as css from "./ProposalCard.scss";
 
-interface IInjectedProps {
-  expired: boolean;
-  proposal: IProposalState;
-  votes: Vote[],
-  stakes: Stake[],
-  rewards: IRewardState,
-  member: IMemberState,
-  daoEthBalance: BN;
-  currentAccountGenBalance: BN;
-  currentAccountGenAllowance: BN;
-}
-
 interface IExternalProps {
   currentAccountAddress: Address;
   dao: IDAOState;
   proposal: Proposal;
-  children(props: IInjectedProps): JSX.Element;
+  children(props: IInjectedProposalProps): JSX.Element;
 }
 
 interface IStateProps {
@@ -37,6 +27,20 @@ interface IStateProps {
 
 type SubscriptionData = [IProposalState, Vote[], Stake[], IRewardState, IMemberState, BN, BN, BN];
 type IProps = IStateProps & IExternalProps & ISubscriptionProps<SubscriptionData>;
+
+interface IInjectedProposalProps {
+  beneficiaryProfile?: IProfileState;
+  creatorProfile?: IProfileState;
+  currentAccountGenBalance: BN;
+  currentAccountGenAllowance: BN;
+  daoEthBalance: BN;
+  expired: boolean;
+  member: IMemberState,
+  proposal: IProposalState;
+  rewards: IRewardState,
+  stakes: Stake[],
+  votes: Vote[],
+}
 
 const mapStateToProps = (state: IRootState, ownProps: IExternalProps & ISubscriptionProps<SubscriptionData>): IProps => {
   const proposalState = ownProps.data ? ownProps.data[0] : null;
@@ -54,66 +58,85 @@ interface IState {
 
 class ProposalData extends React.Component<IProps, IState> {
 
+  constructor(props: IProps) {
+    super(props);
+
+    this.state = {
+      expired: props.data ? closingTime(props.data[0]).isSameOrBefore(moment()) : false,
+    };
+  }
+
+  componentDidMount() {
+    // Expire proposal in real time
+    if (!this.state.expired) {
+      setTimeout(() => { this.setState({ expired: true })}, closingTime(this.props.data[0]).diff(moment()));
+    }
+  }
+
   render() {
     const [proposal, votes, stakes, rewards, member, daoEthBalance, currentAccountGenBalance, currentAccountGenAllowance] = this.props.data;
+    const { beneficiaryProfile, creatorProfile } = this.props;
 
     return this.props.children({
-      expired: this.state.expired,
-      proposal,
-      votes,
-      stakes,
-      rewards,
-      member,
-      daoEthBalance,
+      beneficiaryProfile,
+      creatorProfile,
       currentAccountGenBalance,
-      currentAccountGenAllowance
+      currentAccountGenAllowance,
+      daoEthBalance,
+      expired: this.state.expired,
+      member,
+      proposal,
+      rewards,
+      stakes,
+      votes,
     });
   };
 }
 
-
 const ConnectedProposalData = connect(mapStateToProps)(ProposalData);
 
 export default withSubscription({
-    wrappedComponent: ConnectedProposalData,
-    // TODO: how do we pass this in?
-    loadingComponent: (props) => <div className={css.loading}>Loading proposal {props.proposal.id.substr(0, 6)} ...</div>,
-    errorComponent: (props) => <div>{props.error.message}</div>,
+  wrappedComponent: ConnectedProposalData,
+  // TODO: how do we pass this in?
+  loadingComponent: (props) => <div className={css.loading}>Loading proposal {props.proposal.id.substr(0, 6)} ...</div>,
+  errorComponent: (props) => <div>{props.error.message}</div>,
 
-    checkForUpdate: (oldProps, newProps) => {
-      return oldProps.currentAccountAddress !== newProps.currentAccountAddress || oldProps.proposal.id !== newProps.proposal.id;
-    },
+  checkForUpdate: (oldProps, newProps) => {
+    return oldProps.currentAccountAddress !== newProps.currentAccountAddress || oldProps.proposal.id !== newProps.proposal.id;
+  },
 
-    createObservable: (props) => {
-      const arc = getArc();
-      const { currentAccountAddress, dao, proposal } = props;
-      const arcDao = arc.dao(dao.address);
+  createObservable: async (props) => {
+    const arc = getArc();
+    const { currentAccountAddress, dao, proposal } = props;
+    const arcDao = arc.dao(dao.address);
 
-      const spender = proposal.staticState.votingMachine;
+    await proposal.fetchStaticState();
+    const spender = proposal.staticState.votingMachine;
 
-      if (currentAccountAddress) {
-        return combineLatest(
-          proposal.state(), // state of the current proposal
-          proposal.votes({where: { voter: currentAccountAddress }}),
-          proposal.stakes({where: { staker: currentAccountAddress }}),
-          proposal.rewards({ where: {beneficiary: currentAccountAddress}})
-            .pipe(map((rewards: Reward[]): Reward => rewards.length === 1 && rewards[0] || null))
-            .pipe(mergeMap(((reward: Reward): Observable<IRewardState> => reward ? reward.state() : of(null)))),
-          arcDao.member(currentAccountAddress).state(),
-          concat(of(new BN("0")), arcDao.ethBalance()),
-          arc.GENToken().balanceOf(currentAccountAddress),
-          arc.allowance(currentAccountAddress, spender),
-        );
-      } else {
-        return combineLatest(
-          proposal.state(), // state of the current proposal
-          of([]), // votes
-          of([]), // stakes
-          of(null), // rewards
-          of(null), // member
-          concat(of(new BN("0")), arcDao.ethBalance()), // dao eth balance
-          of(new BN("0")), // current account gen balance
-          of(null), // current account GEN allowance
-        );
-      }
-    },
+    if (currentAccountAddress) {
+      return combineLatest(
+        proposal.state(), // state of the current proposal
+        proposal.votes({where: { voter: currentAccountAddress }}),
+        proposal.stakes({where: { staker: currentAccountAddress }}),
+        proposal.rewards({ where: {beneficiary: currentAccountAddress}})
+          .pipe(map((rewards: Reward[]): Reward => rewards.length === 1 && rewards[0] || null))
+          .pipe(mergeMap(((reward: Reward): Observable<IRewardState> => reward ? reward.state() : of(null)))),
+        arcDao.member(currentAccountAddress).state(),
+        concat(of(new BN("0")), arcDao.ethBalance()),
+        arc.GENToken().balanceOf(currentAccountAddress),
+        arc.allowance(currentAccountAddress, spender),
+      );
+    } else {
+      return combineLatest(
+        proposal.state(), // state of the current proposal
+        of([]), // votes
+        of([]), // stakes
+        of(null), // rewards
+        of(null), // member
+        concat(of(new BN("0")), arcDao.ethBalance()), // dao eth balance
+        of(new BN("0")), // current account gen balance
+        of(null), // current account GEN allowance
+      );
+    }
+  },
+});
