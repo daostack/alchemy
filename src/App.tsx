399fc8cf86fe3b11a5b85c90d052ab6985cbd815
Doug Kent <github@douglaskent.com>
2019-09-13 11:06:33 -0400

    loop until connected

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,19 @@ import { Provider } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import { ConnectedRouter } from "react-router-redux";
 import { ThroughProvider } from "react-through";
+import { sleep } from "lib/util";
 import { history, default as store } from "./configureStore";
 import * as css from "./layouts/App.scss";
 
-export class App extends React.Component<{}, {arcIsInitialized: boolean}> {
+export class App extends React.Component<{}, {
+  arcIsInitialized: boolean;
+  retryingArc: boolean;
+}> {
   constructor(props: {}) {
     super(props);
     this.state = {
       arcIsInitialized: false,
+      retryingArc: false,
     };
   }
 
@@ -22,14 +27,19 @@ export class App extends React.Component<{}, {arcIsInitialized: boolean}> {
     // Do this here because we need to have initialized Arc first.  This will
     // not create a provider for the app, rather will just initialize Arc with a
     // readonly provider with no account, internal only to it.
-    const initialized = await initializeArc()
-      .catch ((err): boolean => {
+    initializeArc()
+      .then(async (success: boolean) => {
+        while (!success) {
+          this.setState({ retryingArc: true });
+          await sleep(5000);
+          success = await initializeArc();
+        }
+        this.setState({ arcIsInitialized: true });
+      })
+      .catch ((err): void => {
         // eslint-disable-next-line no-console
         console.log(err);
-        return false;
       });
-
-    this.setState({ arcIsInitialized: initialized });
 
     let GOOGLE_ANALYTICS_ID: string;
     switch (process.env.NODE_ENV) {
@@ -51,7 +61,14 @@ export class App extends React.Component<{}, {arcIsInitialized: boolean}> {
 
   public render(): any {
     if (!this.state.arcIsInitialized) {
-      return <div className={css.loading}><Loading/></div>;
+      return (
+        <div className={css.waitingToInitContainer}>
+          { this.state.retryingArc ? 
+            <div className={css.waitingToInitMessage}>Waiting to connect to the blockchain.  If this is taking a while, please ensure that you have a good internet connection.</div> : ""
+          }
+          <div className={css.loading}><Loading/></div>
+        </div>
+      );
     } else  {
       return (
         <Provider store={store}>
