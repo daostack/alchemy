2e695f61a211d4d665aa1265a25ed1420592a016
Doug Kent <github@douglaskent.com>
2019-09-24 21:10:17 -0400

    restore tour actions

--- src/actions/uiActions.ts
+++ src/actions/uiActions.ts
@@ -3,6 +3,18 @@ import * as Redux from "redux";
 import { IRootState } from "reducers";
 import { ActionTypes } from "reducers/uiReducer";
 
+export function showTour() {
+  return (dispatch: Redux.Dispatch<any, any>, _getState: () => IRootState) => {
+    dispatch({ type: ActionTypes.SHOW_TOUR });
+  };
+}
+
+export function hideTour() {
+  return (dispatch: Redux.Dispatch<any, any>, _getState: () => IRootState) => {
+    dispatch({ type: ActionTypes.HIDE_TOUR });
+  };
+}
+
 export function showMenu() {
   return (dispatch: Redux.Dispatch<any, any>, _getState: () => IRootState) => {
     dispatch({ type: ActionTypes.SHOW_MENU });
