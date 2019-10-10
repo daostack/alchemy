121e13ec0c34f5e8557df4e68f5b6731f9f9a738
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2019-10-10 10:11:11 +0200

    remove comment

--- src/components/Account/Reputation.tsx
+++ src/components/Account/Reputation.tsx
@@ -32,8 +32,6 @@ export default class Reputation extends React.Component<IProps, null> {
         percentage = percentageBn.toNumber() / 100;
       }
     } else {
-      // eslint-disable-next-line no-console
-      // console.error("totalReputation is <= 0");
       percentageString = "0";
     }
 
