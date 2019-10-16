6988400352eb5f9b5f803a5b9f17e163a9e3ffdb
Doug Kent <dkent600@users.noreply.github.com>
2019-10-16 11:00:52 -0400

    disable New Proposal buttons when GP is not yet active (#1164)
    * handle base buttons, except for SchemeRegistrar
    
    * base button, not SchemeRegistrar
    
    * handle SchemeRegistrar special case
    
    * remove span
    
    * fix UGenericScheme special case
    
    * one getSchemeIsActive function to rule them all

--- src/components/Scheme/SchemeContainer.tsx
+++ src/components/Scheme/SchemeContainer.tsx
@@ -7,7 +7,7 @@ import { enableWalletProvider, getArc } from "arc";
 import * as classNames from "classnames";
 import Loading from "components/Shared/Loading";
 import withSubscription, { ISubscriptionProps } from "components/Shared/withSubscription";
-import { schemeName} from "lib/util";
+import { schemeName, getSchemeIsActive} from "lib/util";
 import * as React from "react";
 import { BreadcrumbsItem } from "react-breadcrumbs-dynamic";
 import { Link, Route, RouteComponentProps, Switch } from "react-router-dom";
@@ -72,6 +72,8 @@ class SchemeContainer extends React.Component<IProps, null> {
       return <ReputationFromToken {...this.props} daoAvatarAddress={daoAvatarAddress} schemeState={schemeState} />;
     }
 
+    const isActive = getSchemeIsActive(schemeState);
+
     const proposalsTabClass = classNames({
       [css.proposals]: true,
       [css.active]: !this.props.location.pathname.includes("info"),
@@ -93,10 +95,14 @@ class SchemeContainer extends React.Component<IProps, null> {
           <div className={css.schemeMenu}>
             <Link className={proposalsTabClass} to={`/dao/${daoAvatarAddress}/scheme/${schemeId}/proposals/`}>Proposals</Link>
             <Link className={infoTabClass} to={`/dao/${daoAvatarAddress}/scheme/${schemeId}/info/`}>Info</Link>
-            <a className={css.createProposal}
-              data-test-id="createProposal"
-              href="javascript:void(0)"
-              onClick={this.handleNewProposal}
+            <a className={
+              classNames({
+                [css.createProposal]: true,
+                [css.disabled]: !isActive,
+              })}
+            data-test-id="createProposal"
+            href="javascript:void(0)"
+            onClick={isActive ? this.handleNewProposal : null}
             >+ New proposal</a>
           </div>
         </Sticky>
@@ -106,7 +112,7 @@ class SchemeContainer extends React.Component<IProps, null> {
             render={(props) => <SchemeInfoPage {...props} daoAvatarAddress={daoAvatarAddress} scheme={schemeState} />} />
 
           <Route path="/dao/:daoAvatarAddress/scheme/:schemeId"
-            render={(props) => <SchemeProposalsPage {...props} currentAccountAddress={currentAccountAddress} scheme={schemeState} />} />
+            render={(props) => <SchemeProposalsPage {...props} isActive={isActive} currentAccountAddress={currentAccountAddress} scheme={schemeState} />} />
         </Switch>
       </div>
     );
