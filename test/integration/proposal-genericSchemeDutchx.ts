ae125f3b86c7e289651298f2a93bc122c5415522
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2019-10-30 14:56:54 +0100

    subgraphv31, fix tests, cleanup scheme info

--- test/integration/proposal-genericSchemeDutchx.ts
+++ test/integration/proposal-genericSchemeDutchx.ts
@@ -7,7 +7,7 @@ describe("Proposals", () => {
 
   beforeEach(async () => {
     const arc = getArc();
-    const daos = await arc.daos({ where: { name: "Crapulous Musician"}}).pipe(first()).toPromise();
+    const daos = await arc.daos({ where: { name: "DAO For Testing"}}).pipe(first()).toPromise();
     const dao = daos[0];
     daoAddress = dao.id;
     if (!daoAddress) {
