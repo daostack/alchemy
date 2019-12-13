abd5d6ebfdf950d9e24a982b555ddcdccb376262
mrrobot16 <hmgaar@gmail.com>
2019-12-13 15:58:28 -0500

    Added similar test to proposalGenericScheme

--- test/integration/proposal-genericScheme.ts
+++ test/integration/proposal-genericScheme.ts
@@ -49,4 +49,38 @@ describe("Proposals", () => {
 
   });
 
+  it("Fill out a generic scheme proposal form and export it", async () => {
+    const url = `/dao/${daoAddress}/`;
+    await browser.url(url);
+
+    const schemeCard = await $("[data-test-id=\"schemeCard-GenericScheme\"]");
+    await schemeCard.click();
+
+    const createProposalButton = await $("a[data-test-id=\"createProposal\"]");
+    await createProposalButton.waitForExist();
+
+    await createProposalButton.click();
+
+    const titleInput = await $("*[id=\"titleInput\"]");
+    await titleInput.waitForExist();
+
+    const title = uuid();
+    await titleInput.setValue(title);
+
+    // using uuid value so that the test will pass also if there is already a proposal with this description
+    // (which must be unique).
+    const descriptionInput = await $(".mde-text");
+    await descriptionInput.setValue(`https://this.must.be/a/valid/url${uuid()}`);
+
+    const callDataInput = await $("*[id=\"callDataInput\"]");
+    await callDataInput.setValue("0x5fB320886aF629122736c0e1a5c94dCE841EA37B");
+
+    const valueInput = await $("*[id=\"valueInput\"]");
+    await valueInput.setValue("0");
+
+    const exportProposalSubmitButton = await $("*[id=\"export-proposal\"]");
+    await exportProposalSubmitButton.click();
+
+  });
+  
 });
