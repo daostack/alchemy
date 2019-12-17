625ef1eafd78826f6e09c8e5a9af9c559954dae0
Edson Ayllon <eayllon1@gmail.com>
2019-12-16 21:33:30 -0500

    Updated tests for Bounties

--- test/integration/proposal-genericSchemeStandardBounties.ts
+++ test/integration/proposal-genericSchemeStandardBounties.ts
@@ -12,9 +12,12 @@ describe("Proposals", () => {
     const dao = daos[0];
     daoAddress = dao.id;
 
+    if (!daoAddress) {
+      throw Error("Could not find a DAO with this name");
+    }
   });
 
-  it("Create a Standard Bounties Generic Scheme proposal, vote for it, stake on it", async () => {
+  it("Create a Standard Bounties Generic Scheme proposal, issue and contribute", async () => {
     const url = `/dao/${daoAddress}/`;
     await browser.url(url);
 
@@ -26,8 +29,8 @@ describe("Proposals", () => {
 
     await createProposalButton.click();
 
-    const contributeTab = await $("*[data-test-id=\"action-tab-contribute\"]");
-    await contributeTab.click();
+    const issueTab = await $("*[data-test-id=\"action-tab-issueAndContribute\"]");
+    await issueTab.click();
 
     const titleInput = await $("*[id=\"titleInput\"]");
     await titleInput.waitForExist();
@@ -39,13 +42,28 @@ describe("Proposals", () => {
     await descriptionInput.setValue(`https://this.must.be/a/valid/url${uuid()}`);
 
     const senderInput = await $("*[data-test-id=\"_sender\"]");
-    await senderInput.setValue("0x5fB320886aF629122736c0e1a5c94dCE841EA37B");
+    await senderInput.setValue(`${daoAddress}`);
 
-    const bountyIdInput = await $("*[data-test-id=\"_bountyId\"]");
-    await bountyIdInput.setValue(2);
+    const issuerInput = await $("*[data-test-id=\"_issuers\"]");
+    await issuerInput.setValue(`${daoAddress}`);
 
-    const tokenAmountInput = await $("*[data-test-id=\"_aamountr\"]");
-    await tokenAmountInput.setValue(100000000);
+    const approversInput = await $("*[data-test-id=\"_approvers\"]");
+    await approversInput.setValue(`${daoAddress}`);
+
+    const dataInput = await $("*[data-test-id=\"_data\"]");
+    await dataInput.setValue("QmVPxQTuoaULSbLuS62FBp2oWvCKrTDPktS7es2XxsM1To");
+
+    const deadlineInput = await $("*[data-test-id=\"_deadline\"]");
+    await deadlineInput.setValue(7288366910);
+
+    const tokenInput = await $("*[data-test-id=\"_token\"]");
+    await tokenInput.setValue("0x0000000000000000000000000000000000000000");
+
+    const tokenVersionInput = await $("*[data-test-id=\"_tokenVersion"]");
+    await tokenVersionInput.setValue(0);
+
+    const depositAmountInput = await $("*[data-test-id=\"_depositAmount"]");
+    await depositAmountInput.setValue(0);
 
     const createProposalSubmitButton = await $("*[type=\"submit\"]");
     await createProposalSubmitButton.click();
