a01cb174c0104058ac0c57165cd42602cf9b9ffd
Tibet Sprague <tibet.sprague@gmail.com>
2019-12-17 17:02:08 -0800

    Update banner text on not connected feed page

--- src/components/Feed/FeedPage.tsx
+++ src/components/Feed/FeedPage.tsx
@@ -67,7 +67,7 @@ class FeedPage extends React.Component<IProps, null> {
     if (!currentAccountAddress) {
       return <div className={css.emptyFeedBanner} data-test-id="not-logged-in-banner">
         <img src="/assets/images/unplugged.svg" />
-        <h1>Excuse me, who are you?</h1>
+        <h1>Hi there! Have we met before?</h1>
         <h3>Please Log In to see your personal feed</h3>
         <button onClick={this.handleConnect} className={css.connectButton}>
           <span>Log In</span>
