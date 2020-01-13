e108c83cb79f26a5da478c04e98ffb74f18f81ca
Doug Kent <github@douglaskent.com>
2020-01-13 08:11:32 -0500

    try for fixing repfromtoken

--- src/actions/arcActions.ts
+++ src/actions/arcActions.ts
@@ -184,7 +184,8 @@ export function redeemReputationFromToken(scheme: Scheme, addressToRedeem: strin
 
       // send the transaction and get notifications
       if (reputationFromTokenScheme) {
-        reputationFromTokenScheme.redeem(addressToRedeem).subscribe(...observer);
+        const agreementHash = await reputationFromTokenScheme.getAgreementHash();
+        reputationFromTokenScheme.redeem(addressToRedeem, agreementHash).subscribe(...observer);
       }
     }
   };
