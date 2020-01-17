0531796f478731f7cd1e320b67f59263ce428b85
cbrzn <cesarbrazon10@gmail.com>
2020-01-17 22:05:38 +0100

    Removing format on sidebarmenu file

--- src/layouts/SidebarMenu.tsx
+++ src/layouts/SidebarMenu.tsx
@@ -57,16 +57,17 @@ const mapStateToProps = (state: IRootState, ownProps: IExternalProps): IExternal
 };
 
 class SidebarMenu extends React.Component<IProps, IStateProps> {
+
   constructor(props: IProps) {
     super(props);
   }
 
   private handleCloseMenu = (_event: any): void => {
     this.props.hideMenu();
-  };
+  }
 
   public daoMenu() {
-    const [dao, { hasNewPosts }] = this.props.data;
+    const [ dao, { hasNewPosts } ] = this.props.data ;
 
     const daoHoldingsAddress = "https://etherscan.io/tokenholdings?a=" + dao.address;
     const bgPattern = GeoPattern.generate(dao.address + dao.name);
@@ -75,71 +76,45 @@ class SidebarMenu extends React.Component<IProps, IStateProps> {
       <div>
         <div className={css.daoName}>
           <Link to={"/dao/" + dao.address} onClick={this.handleCloseMenu}>
-            <b className={css.daoIcon} style={{ backgroundImage: bgPattern.toDataUrl() }} />
-            <em />
+            <b className={css.daoIcon} style={{ backgroundImage: bgPattern.toDataUrl() }}></b>
+            <em></em>
             <span>{dao.name}</span>
           </Link>
         </div>
         <div className={css.daoDescription}>
-          {dao.name === "dxDAO" ? (
+          {dao.name === "dxDAO" ?
             <p>
               By submitting a proposal, you agree to be bound by the&nbsp;
-              <a
-                href="https://cloudflare-ipfs.com/ipfs/QmRQhXUKKfUCgsAf5jre18T3bz5921fSfvnZCB5rR8mCKj"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Participation Agreement
-              </a>
-              , which includes the terms of participation in the dxDAO
+              <a href="https://cloudflare-ipfs.com/ipfs/QmRQhXUKKfUCgsAf5jre18T3bz5921fSfvnZCB5rR8mCKj" target="_blank" rel="noopener noreferrer">Participation Agreement</a>, which includes the terms of participation in the dxDAO
             </p>
-          ) : dao.name === "Meme" ? (
-            <p>
-              <a
-                href="https://docs.google.com/document/d/1iJZfjmOK1eZHq-flmVF_44dZWNsN-Z2KAeLqW3pLQo8"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn how to MemeDAO
-              </a>
-            </p>
-          ) : dao.name === "ETHBerlin dHack.io" ? (
-            <p>
-              For more info join our TG group -
-              <a href="https://t.me/dhack0" target="_blank" rel="noopener noreferrer">
-                t.me/dhack0
-              </a>
-            </p>
-          ) : dao.name === "Identity" ? (
-            <p>
-              A curated registry of identities on the Ethereum blockchain.&nbsp;
-              <a
-                href="https://docs.google.com/document/d/1_aS41bvA6D83aTPv6QNehR3PfIRHJKkELnU76Sds5Xk"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                How to register.
-              </a>
-            </p>
-          ) : (
-            <p>Anyone can make a proposal to the DAO! Click the button on the top right.</p>
-          )}
+            : dao.name === "Meme" ?
+              <p><a href="https://docs.google.com/document/d/1iJZfjmOK1eZHq-flmVF_44dZWNsN-Z2KAeLqW3pLQo8" target="_blank" rel="noopener noreferrer">Learn how to MemeDAO</a></p>
+              : dao.name === "ETHBerlin dHack.io" ?
+                <p>
+                For more info join our TG group -
+                  <a href="https://t.me/dhack0" target="_blank" rel="noopener noreferrer">t.me/dhack0</a>
+                </p>
+                : dao.name === "Identity" ?
+                  <p>
+                A curated registry of identities on the Ethereum blockchain.&nbsp;
+                    <a href="https://docs.google.com/document/d/1_aS41bvA6D83aTPv6QNehR3PfIRHJKkELnU76Sds5Xk" target="_blank" rel="noopener noreferrer">How to register.</a>
+                  </p>
+                  : <p>Anyone can make a proposal to the DAO! Click the button on the top right.</p>
+          }
         </div>
         <div className={css.followButton}><FollowButton id={dao.address} type="daos" style="white" /></div>
         <div className={css.daoNavigation}>
-          <span className={css.daoNavHeading}>
-            <b>Menu</b>
-          </span>
+          <span className={css.daoNavHeading}><b>Menu</b></span>
           <ul>
             <li>
               <Link to={"/dao/" + dao.address} onClick={this.handleCloseMenu}>
                 <span className={css.menuDot} />
-                <span
-                  className={classNames({
+                <span className={
+                  classNames({
                     [css.notification]: true,
                     [css.homeNotification]: true,
-                  })}
-                />
+                  })
+                }></span>
                 <img src="/assets/images/Icon/menu/home.svg" />
                 Home
               </Link>
@@ -148,26 +123,26 @@ class SidebarMenu extends React.Component<IProps, IStateProps> {
               <TrainingTooltip placement="right" overlay={"List of entities (DAOs and individuals) that have voting power in the DAO"}>
                 <Link to={"/dao/" + dao.address + "/members/"} onClick={this.handleCloseMenu}>
                   <span className={css.menuDot} />
-                  <span
-                    className={classNames({
+                  <span className={
+                    classNames({
                       [css.notification]: true,
                       [css.holdersNotification]: true,
-                    })}
-                  />
+                    })
+                  }></span>
                   <img src="/assets/images/Icon/menu/holders.svg" />
-                  Reputation Holders
+                Reputation Holders
                 </Link>
               </TrainingTooltip>
             </li>
             <li>
               <Link to={"/dao/" + dao.address + "/history/"} onClick={this.handleCloseMenu}>
                 <span className={css.menuDot} />
-                <span
-                  className={classNames({
+                <span className={
+                  classNames({
                     [css.notification]: true,
                     [css.historyNotification]: true,
-                  })}
-                />
+                  })
+                }></span>
                 <img src="/assets/images/Icon/menu/history.svg" />
                 History
               </Link>
@@ -175,20 +150,19 @@ class SidebarMenu extends React.Component<IProps, IStateProps> {
             <li>
               <TrainingTooltip placement="right" overlay={"Space designated for general questions, statements and comments"}>
                 <Link to={"/dao/" + dao.address + "/discussion/"} onClick={this.handleCloseMenu}>
-                  <span
-                    className={classNames({
+                  <span className={
+                    classNames({
                       [css.menuDot]: true,
                       [css.red]: hasNewPosts,
-                    })}
-                  />
-                  <span
-                    className={classNames({
+                    })} />
+                  <span className={
+                    classNames({
                       [css.notification]: true,
                       [css.discussionNotification]: true,
-                    })}
-                  />
+                    })
+                  }></span>
                   <img src="/assets/images/Icon/menu/chat.svg" />
-                  DAO Wall
+                DAO Wall
                 </Link>
               </TrainingTooltip>
             </li>
@@ -223,8 +197,7 @@ class SidebarMenu extends React.Component<IProps, IStateProps> {
           </span>
           <ul>
             <SubscribedEthBalance dao={dao} />
-
-            {Object.keys(supportedTokens()).map(tokenAddress => {
+            {Object.keys(supportedTokens()).map((tokenAddress) => {
               return <SubscribedTokenBalance tokenAddress={tokenAddress} dao={dao} key={"token_" + tokenAddress} />;
             })}
           </ul>
@@ -244,38 +217,32 @@ class SidebarMenu extends React.Component<IProps, IStateProps> {
     return (
       <div className={sidebarClass}>
         <div className={css.menuContent}>
-          {this.props.daoAvatarAddress && this.props.data ? this.daoMenu() : ""}
+          { this.props.daoAvatarAddress && this.props.data ? this.daoMenu() : ""}
 
           <div className={css.siteLinksWrapper}>
             <ul>
-              <li>
-                <Link to="/" onClick={this.handleCloseMenu}>
-                  Home
-                </Link>
-              </li>
+              <li><Link to="/" onClick={this.handleCloseMenu}>Home</Link></li>
               <li>
                 <TrainingTooltip overlay="List of all DAOs accessible by Alchemy" placement="right">
-                  <Link to="/daos" onClick={this.handleCloseMenu}>
-                    All DAOs
-                  </Link>
+                  <Link to="/daos" onClick={this.handleCloseMenu}>All DAOs</Link>
                 </TrainingTooltip>
               </li>
               <li>
                 <a>$ Buy GEN</a>
                 <ul>
-                  <div className={css.diamond} />
-                  {getExchangesList().map((item: any) => {
-                    return (
-                      <li key={item.name}>
-                        <a href={item.url} target="_blank" rel="noopener noreferrer">
-                          <b>
-                            <img src={item.logo} />
-                          </b>
-                          <span>{item.name}</span>
-                        </a>
-                      </li>
-                    );
-                  })}
+                  <div className={css.diamond}></div>
+                  {
+                    getExchangesList().map((item: any) => {
+                      return (
+                        <li key={item.name}>
+                          <a href={item.url} target="_blank" rel="noopener noreferrer">
+                            <b><img src={item.logo} /></b>
+                            <span>{item.name}</span>
+                          </a>
+                        </li>
+                      );
+                    })
+                  }
                 </ul>
               </li>
               <li><a href="https://daostack.zendesk.com/hc" target="_blank" rel="noopener noreferrer">Help Center</a></li>
@@ -283,12 +250,7 @@ class SidebarMenu extends React.Component<IProps, IStateProps> {
               <li><Link to="/privacy-policy" target="_blank" rel="noopener noreferrer">Privacy Policy</Link></li>
               <li className={css.daoStack}>
                 <a href="http://daostack.io" target="_blank" rel="noopener noreferrer">
-                  <img
-                    src={
-                      this.props.daoAvatarAddress ? "/assets/images/Icon/dao-logo.svg" : "/assets/images/Icon/dao-logo-gray.svg"
-                    }
-                  />{" "}
-                  DAOstack
+                  <img src={this.props.daoAvatarAddress ? "/assets/images/Icon/dao-logo.svg" : "/assets/images/Icon/dao-logo-gray.svg"} /> DAOstack
                 </a>
               </li>
             </ul>
@@ -300,17 +262,13 @@ class SidebarMenu extends React.Component<IProps, IStateProps> {
 }
 
 /***** DAO ETH Balance *****/
-interface IEthProps extends ISubscriptionProps<BN | null> {
+interface IEthProps extends ISubscriptionProps<BN|null> {
   dao: IDAOState;
 }
 
 const ETHBalance = (props: IEthProps) => {
   const { data } = props;
-  return (
-    <li key="ETH">
-      <strong>{formatTokens(data)}</strong> ETH
-    </li>
-  );
+  return <li key="ETH"><strong>{formatTokens(data)}</strong> ETH</li>;
 };
 
 const SubscribedEthBalance = withSubscription({
@@ -322,10 +280,7 @@ const SubscribedEthBalance = withSubscription({
   },
   createObservable: (props: IEthProps) => {
     const arc = getArc();
-    return arc
-      .dao(props.dao.address)
-      .ethBalance()
-      .pipe(ethErrorHandler());
+    return arc.dao(props.dao.address).ethBalance().pipe(ethErrorHandler());
   },
 });
 
@@ -359,10 +314,7 @@ const SubscribedTokenBalance = withSubscription({
     // prime the cache: get all members fo this DAO -
     const daoState = props.dao;
 
-    await daoState.dao
-      .members({ first: 1000, skip: 0 })
-      .pipe(first())
-      .toPromise();
+    await daoState.dao.members({ first: 1000, skip: 0 }).pipe(first()).toPromise();
 
     const arc = getArc();
     const token = new Token(props.tokenAddress, arc);
@@ -373,40 +325,27 @@ const SubscribedTokenBalance = withSubscription({
 const SubscribedSidebarMenu = withSubscription({
   wrappedComponent: SidebarMenu,
   checkForUpdate: ["daoAvatarAddress"],
-  loadingComponent: (
-    <div className={css.loading}>
-      <Loading />
-    </div>
-  ),
+  loadingComponent: <div className={css.loading}><Loading/></div>,
   createObservable: (props: IProps) => {
     if (props.daoAvatarAddress) {
       const lastAccessDate = localStorage.getItem(`daoWallEntryDate_${props.daoAvatarAddress}`) || "0";
 
-      const promise = axios
-        .get(
-          `https://disqus.com/api/3.0/threads/listPosts.json?api_key=KVISHbDLtTycaGw5eoR8aQpBYN8bcVixONCXifYcih5CXanTLq0PpLh2cGPBkM4v&forum=${
-            process.env.DISQUS_SITE
-          }&thread:ident=${props.daoAvatarAddress}&since=${lastAccessDate}&limit=1&order=asc`
-        )
-        .then(
-          (response: AxiosResponse<any>): IHasNewPosts => {
-            if (response.status) {
-              const posts = response.data.response;
-              return { hasNewPosts: posts && posts.length };
-            } else {
-              // eslint-disable-next-line no-console
-              console.error(`request for disqus posts failed: ${response.statusText}`);
-              return { hasNewPosts: false };
-            }
-          }
-        )
-        .catch(
-          (error: Error): IHasNewPosts => {
+      const promise = axios.get(`https://disqus.com/api/3.0/threads/listPosts.json?api_key=KVISHbDLtTycaGw5eoR8aQpBYN8bcVixONCXifYcih5CXanTLq0PpLh2cGPBkM4v&forum=${process.env.DISQUS_SITE}&thread:ident=${props.daoAvatarAddress}&since=${lastAccessDate}&limit=1&order=asc`)
+        .then((response: AxiosResponse<any>): IHasNewPosts => {
+          if (response.status) {
+            const posts = response.data.response;
+            return { hasNewPosts : posts && posts.length };
+          } else {
             // eslint-disable-next-line no-console
-            console.error(`request for disqus posts failed: ${error.message}`);
-            return { hasNewPosts: false };
+            console.error(`request for disqus posts failed: ${response.statusText}`);
+            return { hasNewPosts : false };
           }
-        );
+        })
+        .catch((error: Error): IHasNewPosts => {
+          // eslint-disable-next-line no-console
+          console.error(`request for disqus posts failed: ${error.message}`);
+          return { hasNewPosts : false };
+        });
 
       const arc = getArc();
       return combineLatest(arc.dao(props.daoAvatarAddress).state({ subscribe: true }), from(promise));
@@ -416,7 +355,4 @@ const SubscribedSidebarMenu = withSubscription({
   },
 });
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(SubscribedSidebarMenu);
+export default connect(mapStateToProps, mapDispatchToProps)(SubscribedSidebarMenu);
