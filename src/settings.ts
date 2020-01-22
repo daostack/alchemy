cde2189f74bea03fb9184ed78074fc3a25e8c813
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2020-01-22 14:10:10 +0100

    update ipfs endspoints

--- src/settings.ts
+++ src/settings.ts
@@ -4,7 +4,7 @@ export const settings = {
     graphqlWsProvider: "ws://127.0.0.1:8001/subgraphs/name/daostack",
     web3Provider: "ws://127.0.0.1:8545",
     web3ProviderRead: "ws://127.0.0.1:8545",
-    ipfsProvider: "localhost",
+    ipfsProvider: "http://127.0.0.1:5001/api/v0",
     txSenderServiceUrl: "https://tx-sender-service.herokuapp.com/send-tx",
   },
   staging: {
@@ -12,12 +12,7 @@ export const settings = {
     graphqlWsProvider:  process.env.ARC_GRAPHQLWSPROVIDER || "wss://api.thegraph.com/subgraphs/name/daostack/v36_8_rinkeby",
     web3Provider:  process.env.ARC_WEB3PROVIDER || "wss://rinkeby.infura.io/ws/v3/e0cdf3bfda9b468fa908aa6ab03d5ba2",
     web3ProviderRead:  process.env.ARC_WEB3PROVIDERREAD || "wss://rinkeby.infura.io/ws/v3/e0cdf3bfda9b468fa908aa6ab03d5ba2",
-    ipfsProvider: process.env.ARC_IPFSPROVIDER || {
-      "host": process.env.ARC_IPFSPROVIDER_HOST || "api.thegraph.com",
-      "port": process.env.ARC_IPFSPROVIDER_PORT || "443",
-      "protocol": process.env.ARC_IPFSPROVIDER_PROTOCOL || "https",
-      "api-path": process.env.ARC_IPFSPROVIDER_API_PATH || "/ipfs-daostack/api/v0/",
-    },
+    ipfsProvider: process.env.ARC_IPFSPROVIDER || "https://api.thegraph.com:443/ipfs-daostack/api/v0",
     txSenderServiceUrl: "https://tx-sender-service.herokuapp.com/send-tx",
   },
   production: {
@@ -25,12 +20,7 @@ export const settings = {
     graphqlWsProvider: process.env.ARC_GRAPHQLWSPROVIDER || "wss://api.thegraph.com/subgraphs/name/daostack/v36_8",
     web3Provider: process.env.ARC_WEB3PROVIDER || "wss://mainnet.infura.io/ws/v3/e0cdf3bfda9b468fa908aa6ab03d5ba2",
     web3ProviderRead: process.env.ARC_WEB3PROVIDERREAD || "wss://mainnet.infura.io/ws/v3/e0cdf3bfda9b468fa908aa6ab03d5ba2",
-    ipfsProvider: process.env.ARC_IPFSPROVIDER || {
-      "host": process.env.ARC_IPFSPROVIDER_HOST || "api.thegraph.com",
-      "port": process.env.ARC_IPFSPROVIDER_PORT || "443",
-      "protocol": process.env.ARC_IPFSPROVIDER_PROTOCOL || "https",
-      "api-path": process.env.ARC_IPFSPROVIDER_API_PATH || "/ipfs-daostack/api/v0/",
-    },
+    ipfsProvider: process.env.ARC_IPFSPROVIDER || "https://api.thegraph.com:443/ipfs-daostack/api/v0",
     // txSenderServiceUrl: "https://tx-sender-service-mainnet.herokuapp.com/send-tx",
     txSenderServiceUrl: "",
   },
