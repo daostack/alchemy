e51bc1b990ecc0ed7c787a2446539f619ae48d8c
Tibet Sprague <tibet.sprague@gmail.com>
2020-02-11 11:27:12 -0800

    Fix mixpanel tracking and add tracking of xdai alchemy

--- src/lib/analytics.ts
+++ src/lib/analytics.ts
@@ -3,12 +3,13 @@ import * as Mixpanel from "mixpanel-browser";
 // const doTracking = process.env.NODE_ENV === "staging" || process.env.NODE_ENV === "production";
 const doTracking = (
   (process.env.NODE_ENV === "staging" || process.env.NODE_ENV === "production") &&
-  (window.location.origin in [
+  [
     "https://alchemy.daostack.io",
+    "https://alchemy-xdai.daostack.io/",
     "https://alchemy-staging-rinkeby.herokuapp.com/",
     "http://127.0.0.1:3000",
     "http://localhost:3000",
-  ])
+  ].includes(window.location.origin)
 );
 
 const actions = {
