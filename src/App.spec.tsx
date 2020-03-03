b3537fd5b6773d88d8d182e9a0a09d0901393749
cbrzn <cesarbrazon10@gmail.com>
2020-03-03 21:34:51 +0100

    unit test passes

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,9 +1,9 @@
-// import * as React from "react";
-// import {render} from "react-dom";
-// import { App } from "./App";
+import * as React from "react";
+import {render} from "react-dom";
+import { App } from "./App";
+jest.mock('@dorgtech/daosmind');
 
 it.skip("renders without crashing", async () => {
-  // const div = document.createElement("div");
-  // render(<App />, div);
-  expect(true).toBe(true);
+  const div = document.createElement("div");
+  render(<App />, div);
 });
