1b31850e6518bdab07015c76620cca188963169c
tspoff@gmail.com <tspoff@gmail.com>
2020-03-08 13:13:35 -0500

    Preserve ts file formatting

--- src/genericSchemeRegistry/index.ts
+++ src/genericSchemeRegistry/index.ts
@@ -6,42 +6,42 @@ const Web3 = require("web3");
 const namehash = require("eth-ens-namehash");
 const dutchXInfo = require("./schemes/DutchX.json");
 const gpInfo = require("./schemes/GenesisProtocol.json");
-const ensRegistrarInfo = require("./schemes/ENSRegistrar.json");
+const ensRegistrarInfo = require("./schemes/EnsRegistrar.json");
 const ensRegistryInfo = require("./schemes/ENSRegistry.json");
 const ensPublicResolverInfo = require("./schemes/ENSPublicResolver.json");
 const registryLookupInfo = require("./schemes/RegistryLookup.json");
 
 const KNOWNSCHEMES = [
   dutchXInfo,
-  ensRegistryInfo,
   ensRegistrarInfo,
+  ensRegistryInfo,
   ensPublicResolverInfo,
   gpInfo,
-  registryLookupInfo
+  registryLookupInfo,
 ];
 
-const SCHEMEADDRESSES: { [network: string]: { [address: string]: any } } = {
+const SCHEMEADDRESSES: {[network: string]: { [address: string]: any}} = {
   main: {},
   rinkeby: {},
   xdai: {},
-  ganache: {}
+  ganache: {},
 };
 
 for (const schemeInfo of KNOWNSCHEMES) {
   for (const network of Object.keys(SCHEMEADDRESSES)) {
-    const networkId = (network === "ganache" && "private") || network;
+    const networkId = (network === "ganache" && "private" || network);
     const addresses = schemeInfo.addresses[networkId];
     if (addresses) {
       for (const address of addresses) {
         SCHEMEADDRESSES[network][address.toLowerCase()] = schemeInfo;
       }
-    }
+    } 
   }
 }
 interface IABISpec {
   constant: boolean;
   name: string;
-  inputs: { name: string; type: string }[];
+  inputs: { name: string; type: string}[];
   outputs: any[];
   payable: boolean;
   stateMutability: string;
@@ -89,11 +89,9 @@ export class ActionField {
    * the value to pass to the contract call (as calculated from the user's input data)
    * @return [description]
    */
-  public callValue(userValue: string | string[]) {
+  public callValue(userValue: string|string[]) {
     if (Array.isArray(userValue)) {
-      userValue = userValue.map((val: string) =>
-        Object.prototype.hasOwnProperty.call(val, "trim") ? val.trim() : val
-      );
+      userValue = userValue.map((val: string) => Object.prototype.hasOwnProperty.call(val, "trim") ? val.trim() : val);
     } else if (Object.prototype.hasOwnProperty.call(userValue, "trim")) {
       userValue = userValue.trim();
     }
@@ -103,9 +101,7 @@ export class ActionField {
     }
 
     if (this.decimals) {
-      return new BN(userValue as string)
-        .mul(new BN(10).pow(new BN(this.decimals)))
-        .toString();
+      return (new BN(userValue as string).mul(new BN(10).pow(new BN(this.decimals)))).toString();
     }
 
     switch (this.transformation) {
@@ -146,9 +142,7 @@ export class Action implements IActionSpec {
 
   constructor(options: IActionSpec) {
     if (this.fields && this.abi.inputs.length !== this.fields.length) {
-      throw Error(
-        "Error parsing action: the number if abi.inputs should equal the number of fields"
-      );
+      throw Error("Error parsing action: the number if abi.inputs should equal the number of fields");
     }
     this.description = options.description;
     this.id = options.id;
@@ -160,14 +154,12 @@ export class Action implements IActionSpec {
 
   public getFields(): ActionField[] {
     const result: ActionField[] = [];
-    for (let i = 0; i < this.abi.inputs.length; i++) {
-      result.push(
-        new ActionField({
-          name: this.abi.inputs[i].name,
-          type: this.abi.inputs[i].type,
-          ...this.fields[i]
-        })
-      );
+    for (let i = 0; i <  this.abi.inputs.length; i++) {
+      result.push(new ActionField({
+        name: this.abi.inputs[i].name,
+        type: this.abi.inputs[i].type,
+        ...this.fields[i],
+      }));
     }
     return result;
   }
@@ -184,7 +176,7 @@ export class GenericSchemeInfo {
     this.specs = info;
   }
   public actions() {
-    return this.specs.actions.map(spec => new Action(spec));
+    return this.specs.actions.map((spec) => new Action(spec));
   }
   public action(id: string) {
     for (const action of this.specs.actions) {
@@ -203,7 +195,7 @@ export class GenericSchemeInfo {
     return;
   }
   public encodeABI(action: IActionSpec, values: any[]) {
-    return new Web3().eth.abi.encodeFunctionCall(action.abi, values);
+    return (new Web3()).eth.abi.encodeFunctionCall(action.abi, values);
   }
 
   /*
@@ -211,15 +203,11 @@ export class GenericSchemeInfo {
    * returns: an object containing the action, and the decoded values.
    * It returns 'undefined' if the action could not be found
    */
-  public decodeCallData(
-    callData: string
-  ): { action: IActionSpec; values: any[] } {
+  public decodeCallData(callData: string): { action: IActionSpec; values: any[]} {
     const web3 = new Web3();
-    let action: undefined | IActionSpec;
+    let action: undefined|IActionSpec;
     for (const act of this.actions()) {
-      const encodedFunctionSignature = web3.eth.abi.encodeFunctionSignature(
-        act.abi
-      );
+      const encodedFunctionSignature = web3.eth.abi.encodeFunctionSignature(act.abi);
       if (callData.startsWith(encodedFunctionSignature)) {
         action = act;
         break;
@@ -229,24 +217,21 @@ export class GenericSchemeInfo {
       throw Error("No action matching these callData could be found");
     }
     // we've found our function, now we can decode the parameters
-    const decodedParams = web3.eth.abi.decodeParameters(
-      action.abi.inputs,
-      "0x" + callData.slice(2 + 8)
-    );
-    const values = [];
+    const decodedParams = web3.eth.abi
+      .decodeParameters(action.abi.inputs, "0x" + callData.slice(2 + 8));
+    const values =  [];
     for (const inputSpec of action.abi.inputs) {
       values.push(decodedParams[inputSpec.name]);
     }
 
     if (action) {
-      return { action, values };
+      return { action,  values};
     } else {
-      throw Error(
-        "Could not find a known action that corresponds with these callData"
-      );
+      throw Error("Could not find a known action that corresponds with these callData");
     }
   }
 }
+
 
 export class GenericSchemeRegistry {
   /**
@@ -264,4 +249,5 @@ export class GenericSchemeRegistry {
       return new GenericSchemeInfo(spec);
     }
   }
+
 }
