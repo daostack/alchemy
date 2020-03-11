aff79c0a2c49d6b477b9536be7fb3823976a4559
Doug Kent <github@douglaskent.com>
2020-03-11 13:01:54 -0600

    less code

--- src/components/Shared/Loading.tsx
+++ src/components/Shared/Loading.tsx
@@ -1,8 +1,3 @@
 import * as React from "react";
 
-export default class Loading extends React.Component {
-
-  public render(): RenderOutput {
-    return <img src="/assets/images/spinnyBusyIcon.gif" height="200px" width="200px"></img>;
-  }
-}
+export default () => <img src="/assets/images/spinnyBusyIcon.gif" height="200px" width="200px"></img>;
