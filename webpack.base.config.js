6d3051522d0c92706689f3b915ff2ff945ad8801
cbrzn <cesarbrazon10@gmail.com>
2020-03-12 02:09:15 +0100

    using mini css extract plugin to make styles work with dao creator

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -1,37 +1,38 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const ENV = process.env.NODE_ENV || 'development';
-const isProd = ENV === 'production';
-const isDev = ENV === 'development';
+const ENV = process.env.NODE_ENV || "development";
+const isProd = ENV === "production";
+const isDev = ENV === "development";
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const basePath = process.cwd();
 
 module.exports = {
-  devtool: 'eval',
+  devtool: "eval",
 
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
     extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
 
     alias: {
-      arc: path.resolve(basePath, 'src/arc'),
-      actions: path.resolve(basePath, 'src/actions'),
-      components: path.resolve(basePath, 'src/components'),
-      constants: path.resolve(basePath, 'src/constants'),
-      data: path.resolve(basePath, 'data'),
-      genericSchemeRegistry: path.resolve(basePath, 'src/genericSchemeRegistry'),
-      crxRegistry: path.resolve(basePath, 'src/crxRegistry'),
-      layouts: path.resolve(basePath, 'src/layouts'),
-      lib: path.resolve(basePath, 'src/lib'),
-      pages: path.resolve(basePath, 'src/pages'),
-      reducers: path.resolve(basePath, 'src/reducers'),
-      selectors: path.resolve(basePath, 'src/selectors'),
-      src: path.resolve(basePath, 'src'),
-      'ipfs-api': 'ipfs-api/dist',
-      'bn.js': 'bn.js/lib/bn.js'
-    },
+      arc: path.resolve(basePath, "src/arc"),
+      actions: path.resolve(basePath, "src/actions"),
+      components: path.resolve(basePath, "src/components"),
+      constants: path.resolve(basePath, "src/constants"),
+      data: path.resolve(basePath, "data"),
+      genericSchemeRegistry: path.resolve(basePath, "src/genericSchemeRegistry"),
+      crxRegistry: path.resolve(basePath, "src/crxRegistry"),
+      layouts: path.resolve(basePath, "src/layouts"),
+      lib: path.resolve(basePath, "src/lib"),
+      pages: path.resolve(basePath, "src/pages"),
+      reducers: path.resolve(basePath, "src/reducers"),
+      selectors: path.resolve(basePath, "src/selectors"),
+      src: path.resolve(basePath, "src"),
+      "ipfs-api": "ipfs-api/dist",
+      "bn.js": "bn.js/lib/bn.js"
+    }
   },
 
   module: {
@@ -39,7 +40,7 @@ module.exports = {
       // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
       {
         test: /\.tsx?$/,
-        loader: ['react-hot-loader/webpack', "awesome-typescript-loader"],
+        loader: ["react-hot-loader/webpack", "awesome-typescript-loader"],
         exclude: [/node_modules/, /\.spec\.ts$/]
       },
 
@@ -63,7 +64,7 @@ module.exports = {
           /node_modules\/subscriptions-transport-ws/,
           /node_modules\/xhr2-cookies/,
           /node_modules\/zen-observable-ts/,
-          /node_modules\/@dorgtech/,
+          /node_modules\/@dorgtech/
         ]
       },
 
@@ -76,7 +77,7 @@ module.exports = {
           /node_modules\/mdbreact\/dist\/css\/mdb.css/,
           /node_modules\/@dorgtech\/daocreator-ui\/dist/
         ],
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
       },
 
       // CSS handling
@@ -85,45 +86,51 @@ module.exports = {
         include: /client/,
         exclude: /node_modules/,
         use: [
-          'style-loader',
-          { // translates CSS into CommonJS (css-loader) and automatically generates TypeScript types
-            loader: 'typings-for-css-modules-loader',
+          "style-loader",
+          {
+            // translates CSS into CommonJS (css-loader) and automatically generates TypeScript types
+            loader: "typings-for-css-modules-loader",
             options: {
               camelCase: true,
-              localIdentName: '[name]__[local]___[hash:base64:5]',
+              localIdentName: "[name]__[local]___[hash:base64:5]",
               minimize: isProd,
               modules: true,
               namedExport: true,
               sourceMap: true
             }
-          },
-        ],
+          }
+        ]
       },
 
       // Images & fonts
       {
         test: /\.(png|jpg|gif|mp4|ogg|svg|woff|woff2|ttf|eot|ico)$/,
-        loader: 'url-loader',
+        exclude: [
+          /node_modules\/@fortawesome\/fontawesome-free\/css\/all.min.css/,
+          /node_modules\/bootstrap-css-only\/css\/bootstrap.min.css/,
+          /node_modules\/mdbreact\/dist\/css\/mdb.css/,
+        ],
+        loader: "url-loader",
         options: {
           limit: 10000 // For assets smaller than 10k inline them as data urls, otherwise use regular file loader
         }
-      },
-
+      }
     ]
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html'
+      template: "src/index.html"
     }),
     new webpack.DefinePlugin({
-      'VERSION': JSON.stringify(require('./package.json').version)
+      VERSION: JSON.stringify(require("./package.json").version)
     }),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new MiniCssExtractPlugin()
   ],
   node: {
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty'
+    fs: "empty",
+    net: "empty",
+    tls: "empty"
   }
 };
