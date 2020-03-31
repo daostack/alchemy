91c8458bfc81d11faa54f808ba36d7185d3c1dad
cbrzn <cesarbrazon10@gmail.com>
2020-03-31 19:03:44 +0200

    trying to mangle just graphql

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -39,7 +39,9 @@ const config = merge(baseConfig, {
            * This is because graphql has an error with the minified bundles when the process doesn't has the env variable NODE_ENV=production
            * More info: https://github.com/graphql/graphql-js/issues/1182
            */
-          mangle: process.env.NODE_ENV === "production"
+          mangle: {
+            keep_fnames: process.env.NODE_ENV === "production" ? false : /node_modules\/graphql/
+          }
         }
       })
     ],
