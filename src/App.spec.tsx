03af9357beb4c75be68e8799d69560b5d841c1a1
Doug Kent <github@douglaskent.com>
2020-05-14 10:17:43 -0400

    remove App.spec.ts

--- src/App.spec.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import {render} from "react-dom";
-import { App } from "./App";
-
-it.skip("renders without crashing", async () => {
-  const div = document.createElement("div");
-  render(<App />, div);
-});
