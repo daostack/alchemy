f35df4eba80fe0e4ad7c82966fd8cadf184fff3c
Doug Kent <github@douglaskent.com>
2020-05-18 09:43:43 -0400

    fix disqusConfig initialization

--- src/components/Dao/DaoLandingPage.tsx
+++ src/components/Dao/DaoLandingPage.tsx
@@ -34,11 +34,6 @@ export default class DaoLandingPage extends React.Component<IProps, IStateProps>
   }
 
   public componentDidMount() {
-    this.disqusConfig = {
-      url: `${process.env.BASE_URL}/dao/${this.props.daoState.address}/discussion`,
-      identifier: this.props.daoState.address,
-      title: "Discuss " + this.props.daoState.name,
-    };
 
     Analytics.track("Page View", {
       "Page Name": Page.DAOLanding,
@@ -57,6 +52,10 @@ export default class DaoLandingPage extends React.Component<IProps, IStateProps>
 
   public render() {
     const daoState = this.props.daoState;
+
+    this.disqusConfig.url = `${process.env.BASE_URL}/dao/${this.props.daoState.address}/discussion`;
+    this.disqusConfig.identifier = this.props.daoState.address;
+    this.disqusConfig.title = "Discuss " + this.props.daoState.name;
 
     return (
       <div className={css.landingPage}>
