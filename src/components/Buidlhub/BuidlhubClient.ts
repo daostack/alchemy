55cd84bbb33433cfcaa6d499366b10e21b6f5a59
mdcoon <mdcoon1@yahoo.com>
2020-06-17 15:02:47 -0400

    Fixed linter problems

--- src/components/Buidlhub/BuidlhubClient.ts
+++ src/components/Buidlhub/BuidlhubClient.ts
@@ -1,79 +1,77 @@
-import axios from 'axios';
+import axios from "axios";
 
-const DEFAULT_ENDPOINT = 'https://daostack.buidlhub.com/register';
+const DEFAULT_ENDPOINT = "https://daostack.buidlhub.com/register";
 
 export interface IBuidlhubClient {
-    endpoint: string,
-    register(props:any): any
+  endpoint: string;
+  register(props: any): any;
 }
 
 export default class BuidlhubClient implements IBuidlhubClient {
-    endpoint:string;
+  endpoint: string;
 
-    constructor(props:any = {}) {
-        let endpoint:string = props.endpoint || DEFAULT_ENDPOINT;
-        this.endpoint = endpoint;
-        
+  constructor(props: any = {}) {
+    const endpoint: string = props.endpoint || DEFAULT_ENDPOINT;
+    this.endpoint = endpoint;
+
+  }
+
+  async register(props: any = {}) {
+    this._validatePropsExist(props, ["email", "walletAddress"]);
+
+    const { email, walletAddress } = props;
+
+    const r = await this._post( {
+      walletAddress,
+      email,
+    });
+    return r;
+  }
+
+  private _validatePropsExist(props: any, requiredProps: any) {
+    for (const propertyName of requiredProps) {
+      const propertyValue = props[propertyName];
+      if (!propertyValue) {
+        throw new Error(`${propertyName} is required`);
+      }
+    }
+  }
+
+  async _post(body: any) {
+    const url: string = this.endpoint;
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json;charset=UTF-8",
+        "x-referrer": window?.location?.hostname,
+      },
+      data: JSON.stringify(body),
+    };
+
+    const r = await this._fetchWithRetry(url, options);
+    const data: any = r.data;//response.json();
+
+    if (data.error) {
+      throw new Error(data.error);
     }
 
-    async register(props:any = {}) {
-        this._validatePropsExist(props, ['email', 'walletAddress'])
+    return {data};
+  }
 
-        const { email, walletAddress } = props;
+  async _fetchWithRetry(url: string, options: any, maxAttempts = 3) {
+    let lastError = null;
+    for (let i = 0; i < maxAttempts; i++) {
+      try {
+        return await axios(url, options); //fetch(url, options);
+      } catch (error) {
+        lastError = error;
+      }
 
-        let r = await this._post( {
-            walletAddress,
-            email
-        });
-        return r;
+      // sleep before retry
+      await new Promise(resolve => setTimeout(resolve, 250));
     }
-
-    private _validatePropsExist(props:any, requiredProps:any) {
-        for (const propertyName of requiredProps) {
-            const propertyValue = props[propertyName];
-            if (!propertyValue) {
-                throw new Error(`${propertyName} is required`);
-            }
-        }
-    }
-
-    async _post(body:any) {
-        const url:string = this.endpoint;
-
-        const options = {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json;charset=UTF-8',
-                'x-referrer': window?.location?.hostname,
-            },
-            data: JSON.stringify(body)
-        };
-
-        const r = await this._fetchWithRetry(url, options);
-        console.log("Fetch response", r.data);
-
-       const data:any = r.data;//response.json();
-
-        if (data.error) {
-            throw new Error(data.error);
-        }
-
-        return {data};
-    }
-
-    async _fetchWithRetry(url:string, options:any, maxAttempts = 3) {
-        let lastError = null;
-        for (let i = 0; i < maxAttempts; i++) {
-            try {
-                return await axios(url, options); //fetch(url, options);
-            } catch (error) {
-                lastError = error;
-            }
-
-            // sleep before retry
-            await new Promise(resolve => setTimeout(resolve, 250));
-        }
-        throw lastError;
-    }
+    throw lastError;
+  }
 }
--- src/components/Buidlhub/Registration.tsx
+++ src/components/Buidlhub/Registration.tsx
@@ -1,175 +1,174 @@
-import * as css from './Registration.scss';
-import * as React from 'react'
-import {connect} from 'react-redux';
+import * as css from "./Registration.scss";
+import * as React from "react";
+import {connect} from "react-redux";
 import { IRootState } from "reducers";
-import cn from 'classnames';
-import BuidlhubClient,{IBuidlhubClient} from './BuidlhubClient';
+import cn from "classnames";
+import BuidlhubClient, {IBuidlhubClient} from "./BuidlhubClient";
 
 type IExternalProps = {
-    wallet: string 
+  wallet: string;
 };
-  
+
 type IProps = IExternalProps;
 
 interface IStateProps {
-    wallet: string 
+  wallet: string;
 }
 
 interface IState {
-    sending: boolean,
-    error: string,
-    email: string,
-    dirty: boolean,
-    registered: boolean
+  sending: boolean;
+  error: string;
+  email: string;
+  dirty: boolean;
+  registered: boolean;
 }
 
-class Registration extends React.Component<IProps,IState> {
-    state = {
-        sending: false,
-        error: "",
-        email: "",
-        dirty: false,
-        registered: false
-    }
+class Registration extends React.Component<IProps, IState> {
+  state = {
+    sending: false,
+    error: "",
+    email: "",
+    dirty: false,
+    registered: false,
+  }
 
-    private sendToBHub = async (email:string) => {
-        
-        this.setState({
-            sending: true
-        }, async () => {
-            try {
-                if(this.props.wallet && email) {
-                    let client:IBuidlhubClient = new BuidlhubClient();
-                    let r = await client.register({
-                        email,
-                        walletAddress: this.props.wallet
-                    });
-                    
-                    if(r.data.error) {
-                        this.setState({
-                            sending: false,
-                            error: r.data.error
-                        })
-                    } else {
-                        this.setState({
-                            sending: false,
-                            registered: true
-                        })
-                    }
-                } else {
-                    this.setState({
-                        error: "Missing required parameters",
-                        sending: false
-                    })
-                }
-            } catch (e) {
-                console.log(e);
-                this.setState({
-                    sending: false,
-                    error: e.message
-                })
-            }
-        });
-    }
+  private sendToBHub = async (email: string) => {
 
-    private emailChanged = (val:string) => {
-        this.setState({
-            dirty: true,
-            email: val
-        })
-    }
+    this.setState({
+      sending: true,
+    }, async () => {
+      try {
+        if (this.props.wallet && email) {
+          const client: IBuidlhubClient = new BuidlhubClient();
+          const r = await client.register({
+            email,
+            walletAddress: this.props.wallet,
+          });
 
-    private keyUp = (e:any) => {
-        if(e.keyCode === 13 && this.state.email.length > 0) {
-            this.sendToBHub(this.state.email);
+          if (r.data.error) {
+            this.setState({
+              sending: false,
+              error: r.data.error,
+            });
+          } else {
+            this.setState({
+              sending: false,
+              registered: true,
+            });
+          }
+        } else {
+          this.setState({
+            error: "Missing required parameters",
+            sending: false,
+          });
         }
+      } catch (e) {
+        this.setState({
+          sending: false,
+          error: e.message,
+        });
+      }
+    });
+  }
+
+  private emailChanged = (val: string) => {
+    this.setState({
+      dirty: true,
+      email: val,
+    });
+  }
+
+  private keyUp = (e: any) => {
+    if (e.keyCode === 13 && this.state.email.length > 0) {
+      this.sendToBHub(this.state.email);
     }
+  }
 
-    
-    render() {
 
-        
+  render() {
 
-        let disabled = this.state.email.length === 0;
-        return (
-            <div className={css.bhubRegContainer}>
-                <div className={css.bhubIcon}>
-                    <img src="/assets/images/daostack-bhub-logo.png" width="200"/>
-                </div>
-                <div className={css.description}>
-                    BUIDLHub will send a daily summary of proposals made in DAOs you 
-                    follow or those where you have reputation. Provide your email below 
-                    to start the process.
-                </div>
 
-                {
-                    !this.state.sending && !this.state.registered &&
-                    <EmailInput sendToBHub={this.sendToBHub}
-                            onChange={this.emailChanged}
-                            onKeyUp={this.keyUp}
-                            email={this.state.email} 
-                            disabled={disabled} />
-                }
-                {
-                    this.state.sending &&
-                    <div style={{width: "100%"}}>
-                        <i className={cn("fa fa-spinner fa-spin")} />
-                        <span style={{paddingLeft: "10px"}}>loading...</span>
-                    </div>
-                }
-                {
-                    this.state.registered &&
-                    <div className={css.success}>
-                        <i className={cn("fa fa-check")} />
-                        <span className={css.message}>
-                            Thank you! Check your inbox for a message from BUIDLHub. 
-                            You must complete the process described in the email to receive 
-                            proposal notifications.
-                        </span>
-                    </div>
-                }
-                {
-                    this.state.error.length > 0 &&
-                    <div className={css.error}>
-                        {this.state.error}
-                    </div>
-                }
-                
-                
-            </div>
-        )
-    }
-}
 
-const EmailInput  = (props:any) => {
-    let bClass = props.disabled?css.sendButtonDisabled:css.sendButton;
+    const disabled = this.state.email.length === 0;
     return (
-
-        <div className={css.inputWrapper}>
-            <input className={css.emailInput} 
-                    type="text" 
-                    placeholder="email" 
-                    onChange={e=>props.onChange(e.target.value)}
-                    value={props.email}
-                    onKeyUp={props.onKeyUp} />
-
-            <div className={css.sendButtonWrapper}>
-
-                <button className={bClass} 
-                    disabled={props.disabled}
-                    onClick={e=>props.sendToBHub(props.email)}>Send</button>
-
-            </div>
+      <div className={css.bhubRegContainer}>
+        <div className={css.bhubIcon}>
+          <img src="/assets/images/daostack-bhub-logo.png" width="200"/>
         </div>
-    )
+        <div className={css.description}>
+                    BUIDLHub will send a daily summary of proposals made in DAOs you
+                    follow or those where you have reputation. Provide your email below
+                    to start the process.
+        </div>
+
+        {
+          !this.state.sending && !this.state.registered &&
+                    <EmailInput sendToBHub={this.sendToBHub}
+                      onChange={this.emailChanged}
+                      onKeyUp={this.keyUp}
+                      email={this.state.email}
+                      disabled={disabled} />
+        }
+        {
+          this.state.sending &&
+                    <div style={{width: "100%"}}>
+                      <i className={cn("fa fa-spinner fa-spin")} />
+                      <span style={{paddingLeft: "10px"}}>loading...</span>
+                    </div>
+        }
+        {
+          this.state.registered &&
+                    <div className={css.success}>
+                      <i className={cn("fa fa-check")} />
+                      <span className={css.message}>
+                            Thank you! Check your inbox for a message from BUIDLHub.
+                            You must complete the process described in the email to receive
+                            proposal notifications.
+                      </span>
+                    </div>
+        }
+        {
+          this.state.error.length > 0 &&
+                    <div className={css.error}>
+                      {this.state.error}
+                    </div>
+        }
+
+
+      </div>
+    );
+  }
 }
 
-const s2p = (state : IRootState):IStateProps => {
-    
-    return {
-        wallet: state.web3.currentAccountAddress
-    }
-}
+const EmailInput = (props: any) => {
+  const bClass = props.disabled?css.sendButtonDisabled : css.sendButton;
+  return (
+
+    <div className={css.inputWrapper}>
+      <input className={css.emailInput}
+        type="text"
+        placeholder="email"
+        onChange={e => props.onChange(e.target.value)}
+        value={props.email}
+        onKeyUp={props.onKeyUp} />
+
+      <div className={css.sendButtonWrapper}>
+
+        <button className={bClass}
+          disabled={props.disabled}
+          onClick={() => props.sendToBHub(props.email)}>Send</button>
+
+      </div>
+    </div>
+  );
+};
+
+const s2p = (state: IRootState): IStateProps => {
+
+  return {
+    wallet: state.web3.currentAccountAddress,
+  };
+};
 
 export default connect(s2p)(Registration);
 
