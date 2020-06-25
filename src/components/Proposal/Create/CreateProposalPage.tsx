d7737150e71606bfedd705808ab85cb88b41cfa6
Roie Natan <roie.natan@gmail.com>
2020-06-25 19:19:44 +0300

    fix travis failure

--- src/components/Proposal/Create/CreateProposalPage.tsx
+++ src/components/Proposal/Create/CreateProposalPage.tsx
@@ -69,7 +69,6 @@ class CreateProposalPage extends React.Component<IProps, IStateProps> {
   public async componentDidMount() {
     document.addEventListener("keydown", this.handleKeyPress, false);
     (document.getElementsByTagName("BODY")[0] as HTMLElement).style.overflow = "hidden";
-    document.body.addEventListener("touchmove", this.preventDefault, { passive: false }); // Prevent scrolling iOS
 
     Analytics.track("Page View", {
       "Page Name": Page.CreateProposal,
@@ -94,7 +93,6 @@ class CreateProposalPage extends React.Component<IProps, IStateProps> {
   public componentWillUnmount(){
     document.removeEventListener("keydown", this.handleKeyPress, false);
     (document.getElementsByTagName("BODY")[0] as HTMLElement).style.overflow = "unset";
-    document.body.removeEventListener("touchmove", this.preventDefault, { passive: false }); // Enable scrolling iOS
   }
 
   private handleKeyPress = (e: any) => {
