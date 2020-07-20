9e312557b218f4a16bc92279f5fb7ace450b37bd
Doug Kent <github@douglaskent.com>
2020-07-20 07:02:54 -0400

    edit comments

--- src/components/Shared/FormModalBase.tsx
+++ src/components/Shared/FormModalBase.tsx
@@ -2,26 +2,32 @@ import * as React from "react";
 import { saveModalFormEntries, importUrlValues, restoreModalFormEntries, exportUrl } from "lib/proposalUtils";
 import { showNotification as ShowNotification, NotificationStatus } from "reducers/notifications";
 /**
- * base class for modals that want to persist their values to localStorage and support loading from
- * querystring params in a url.
+ * base class for modals that have a single form and they want to persist the form's values to sessionStorage,
+ * hydrate the form from url querystring params, and copy form values to the clipboadrd.
  */
 export abstract class FormModalBase<TProps, TState> extends React.Component<TProps, TState> {
+
   /**
-   * subclass must implement this and keep it up-to-date with the values that it wants to be persisted
+   * subclass must implement this and keep it up-to-date with the form values
    */
   protected abstract valuesToPersist: object;
+
   /**
-   *
    * @param props The subclass's props
    * @param formName The unique name of the form to be persisted
+   * @param showNotification For notifying when the form values are copied to the clipbaord
    */
   constructor(props: TProps, private formName: string, private showNotification: typeof ShowNotification) {
     super(props);
   }
 
+  /**
+   * pull form values from the url, if present, and sessionStorage, if present
+   * @param defaults Input field names and their default values and types
+   */
   protected hydrateInitialFormValues = <TDefaults, >(defaults: TDefaults): TDefaults => {
     /**
-     * subclass must have used exportUrl to make importUrlValues work
+     * subclass must have used sendFormValuesToClipboard (or exportUrl) to make importUrlValues work
      */
     const values = importUrlValues<TDefaults>(defaults);
     return Object.assign(values, restoreModalFormEntries(this.formName));
@@ -33,6 +39,9 @@ export abstract class FormModalBase<TProps, TState> extends React.Component<TPro
   }
 
   public componentWillUnmount(): void {
+    /**
+     * save values to the clipboard.  Note this happens after Submit too.
+     */
     saveModalFormEntries(this.formName, this.valuesToPersist);
   }
 }
