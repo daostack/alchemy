f3f75674c9730bf413356b0380ac8b25c39ba93b
Doug Kent <github@douglaskent.com>
2020-08-04 09:38:12 -0400

    refactor to be more efficient with callback

--- src/components/Proposal/Create/PluginForms/ResetFormButton.tsx
+++ src/components/Proposal/Create/PluginForms/ResetFormButton.tsx
@@ -1,23 +1,31 @@
 import * as React from "react";
 import * as css from "./ResetFormButton.scss";
+import i18next from "i18next";
 
-export default (props: {
+export default class ResetFormButton extends React.Component<{
   resetToDefaults: () => void,
   isSubmitting: boolean,
-}): JSX.Element => {
-  return (
-    <button className={css.resetFormButton} type="button" disabled={props.isSubmitting}
-    // eslint-disable-next-line react/jsx-no-bind
-      onClick={(): void => {
-      /**
-       * timeout for the case where clicking the button causes a focus change
-       * that triggers validation, which causes this operation not to work
-       */
-        setTimeout(() => {
-          props.resetToDefaults();
-        }, 0);
-      }}>
-    Clear entries
-    </button>
-  );
-};
+}, null> {
+
+  private handleClick = (): void => {
+    /**
+     * timeout for the case where clicking the button causes a focus change
+     * that triggers validation, which causes this operation not to work
+     */
+    setTimeout(() => {
+      this.props.resetToDefaults();
+    }, 0);
+  };
+
+  public render(): RenderOutput {
+    return (
+      <button
+        className={css.resetFormButton}
+        type="button"
+        disabled={this.props.isSubmitting}
+        onClick={this.handleClick}>
+        {i18next.t("Clear entries")}
+      </button>
+    );
+  }
+}
