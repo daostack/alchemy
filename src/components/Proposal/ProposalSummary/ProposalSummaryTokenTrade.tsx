b2ec716b366603d5e4f42b9be6e27b20f3ba20d1
Nestor Amesty <nestor09amesty@gmail.com>
2020-08-11 03:28:11 +0200

    TokenTrade proposal summary fixed

--- src/components/Proposal/ProposalSummary/ProposalSummaryTokenTrade.tsx
+++ src/components/Proposal/ProposalSummary/ProposalSummaryTokenTrade.tsx
@@ -1,12 +1,12 @@
 import { IDAOState, ITokenTradeProposalState } from "@daostack/arc.js";
 import classNames from "classnames";
+import * as React from "react";
 import AccountPopup from "components/Account/AccountPopup";
 import AccountProfileName from "components/Account/AccountProfileName";
-import * as React from "react";
 import { IProfileState } from "reducers/profilesReducer";
 
 import * as css from "./ProposalSummary.scss";
-import TradeString from "../TradeString";
+import { tokenDetails, formatTokens, toWei } from "lib/util";
 
 interface IProps {
   beneficiaryProfile?: IProfileState;
@@ -26,20 +26,55 @@ export default class ProposalSummaryTokenTrade extends React.Component<IProps> {
 
     const { beneficiaryProfile, proposalState, daoState, detailView, transactionModal } = this.props;
 
+    let receiveToken;
+    let sendToken;
+
+    if (proposalState.sendTokenAddress && proposalState.sendTokenAmount) {
+      const tokenData = tokenDetails(proposalState.sendTokenAddress);
+      sendToken = formatTokens(toWei(Number(proposalState.sendTokenAmount)), tokenData ? tokenData["symbol"] : "?", tokenData ? tokenData["decimals"] : 18);
+    }
+
+    if (proposalState.receiveTokenAddress && proposalState.receiveTokenAmount) {
+      const tokenData = tokenDetails(proposalState.receiveTokenAddress);
+      receiveToken = formatTokens(toWei(Number(proposalState.receiveTokenAmount)), tokenData ? tokenData["symbol"] : "?", tokenData ? tokenData["decimals"] : 18);
+    }
+
     const proposalSummaryClass = classNames({
       [css.detailView]: detailView,
       [css.transactionModal]: transactionModal,
       [css.proposalSummary]: true,
     });
+
     return (
       <div className={proposalSummaryClass}>
-        <span className={css.transferType}><TradeString proposalState={proposalState}/></span>
-        <strong className={css.transferAmount}></strong>
-        <img className={css.transferIcon} src="/assets/images/Icon/Transfer.svg" />
-        <AccountPopup accountAddress={proposalState.beneficiary} daoState={daoState} width={12} />
-        <strong>
-          <AccountProfileName accountAddress={proposalState.beneficiary} accountProfile={beneficiaryProfile} daoAvatarAddress={daoState.address}/>
-        </strong>
+        <span className={css.transferType}>
+          { sendToken &&
+        <>
+          <strong>Sends to DAO:</strong><br/>
+          <AccountPopup accountAddress={proposalState.beneficiary} daoState={daoState} width={12} />
+          <strong>
+            <AccountProfileName accountAddress={proposalState.beneficiary} accountProfile={beneficiaryProfile} daoAvatarAddress={daoState.address}/>
+          </strong>
+          <strong className={css.transferAmount}></strong>
+          <img className={css.transferIcon} src="/assets/images/Icon/Transfer.svg" />
+          {receiveToken}
+          <br/>
+        </>
+          }
+          { receiveToken &&
+        <>
+          <br/>
+          <strong>Receives from DAO:</strong><br/>
+          {receiveToken}
+          <strong className={css.transferAmount}></strong>
+          <img className={css.transferIcon} src="/assets/images/Icon/Transfer.svg" />
+          <AccountPopup accountAddress={proposalState.beneficiary} daoState={daoState} width={12} />
+          <strong>
+            <AccountProfileName accountAddress={proposalState.beneficiary} accountProfile={beneficiaryProfile} daoAvatarAddress={daoState.address}/>
+          </strong>
+        </>
+          }
+        </span>
       </div>
     );
 
