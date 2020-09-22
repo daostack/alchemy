01c6407615927779fa75a60595e64152b406c7a6
Roie Natan <roie.natan@gmail.com>
2020-09-22 12:42:03 +0300

    run all tests

--- test/integration/wdio.conf.js
+++ test/integration/wdio.conf.js
@@ -10,7 +10,7 @@ exports.config = {
   // directory is where your package.json resides, so `wdio` will be called from there.
   //
   specs: [
-    "./test/integration/rewards.ts",
+    "./test/integration/*.ts",
   ],
   // Patterns to exclude.
   exclude: [
