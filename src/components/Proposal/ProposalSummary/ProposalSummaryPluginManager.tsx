38e23dc5d919b691cb64b555817591bc4e08f558
Roie Natan <roie.natan@gmail.com>
2020-10-06 16:42:36 +0300

    see PR description for detalis

--- src/components/Proposal/ProposalSummary/ProposalSummaryPluginManager.tsx
+++ src/components/Proposal/ProposalSummary/ProposalSummaryPluginManager.tsx
@@ -1,14 +1,15 @@
-import { IDAOState, IPluginManagerProposalState, NULL_ADDRESS } from "@daostack/arc.js";
+import { IDAOState, IPluginManagerProposalState, NULL_ADDRESS, IGenesisProtocolParams } from "@daostack/arc.js";
 import classNames from "classnames";
-import { copyToClipboard, getNetworkName, linkToEtherScan } from "lib/util";
+import { copyToClipboard, getNetworkName, linkToEtherScan, toBaseUnit } from "lib/util";
 import { pluginNameAndAddress } from "lib/pluginUtils";
 import * as React from "react";
 import { NotificationStatus, showNotification } from "reducers/notifications";
 import { IProfileState } from "reducers/profilesReducer";
 import { connect } from "react-redux";
 import * as css from "./ProposalSummary.scss";
-import * as moment from "moment";
 import { GenericPluginRegistry } from "genericPluginRegistry";
+import { PLUGIN_NAMES } from "lib/pluginUtils";
+import { renderGpParams } from "components/Plugin/PluginInfoPage";
 
 interface IDispatchProps {
   showNotification: typeof showNotification;
@@ -32,6 +33,29 @@ const mapDispatchToProps = {
   showNotification,
 };
 
+/**
+ * Given an array of voting params values from the decoded data, returns IGenesisProtocolParams object
+ * @param {Array<any>} votingParams
+ * @returns {IGenesisProtocolParams}
+ */
+const mapVoteParamsToGenesisParams = (votingParams: Array<any>): IGenesisProtocolParams => {
+  const genesisProtocolParams: IGenesisProtocolParams = {
+    activationTime: votingParams[10],
+    boostedVotePeriodLimit: votingParams[2],
+    daoBountyConst: votingParams[9],
+    limitExponentValue: 0, // This is not included in the decoded data
+    minimumDaoBounty: toBaseUnit(votingParams[8], 18),
+    preBoostedVotePeriodLimit: votingParams[3],
+    proposingRepReward: toBaseUnit(votingParams[6], 18),
+    queuedVoteRequiredPercentage: votingParams[0],
+    queuedVotePeriodLimit: votingParams[1],
+    quietEndingPeriod: votingParams[5],
+    thresholdConst: votingParams[4],
+    votersReputationLossRatio: votingParams[7],
+  };
+  return genesisProtocolParams;
+};
+
 class ProposalSummary extends React.Component<IProps, IState> {
 
   constructor(props: IProps) {
@@ -46,7 +70,7 @@ class ProposalSummary extends React.Component<IProps, IState> {
     this.props.showNotification(NotificationStatus.Success, "Copied to clipboard!");
   };
 
-  public async componentDidMount (): Promise<void> {
+  public async componentDidMount(): Promise<void> {
     this.setState({
       network: (await getNetworkName()).toLowerCase(),
     });
@@ -57,25 +81,30 @@ class ProposalSummary extends React.Component<IProps, IState> {
     let pluginName = proposalState.pluginToRegisterName;
     const decodedData = proposalState.pluginToRegisterDecodedData;
     let votingParams;
+    let genesisProtocolParams: IGenesisProtocolParams;
     let contractToCall;
-    if (pluginName && decodedData && pluginName !== "ReputationFromToken"){ // pluginName && decodedData ---> means the plugin is Add or Replace (not Remove)
+    if (pluginName && decodedData && pluginName !== "ReputationFromToken") { // pluginName && decodedData ---> means the plugin is Add or Replace (not Remove)
       votingParams = decodedData.params[2].value;
-      if (pluginName === "GenericScheme"){
+      if (pluginName === "GenericScheme") {
         contractToCall = decodedData.params[5].value;
         const genericPluginRegistry = new GenericPluginRegistry();
         const genericPluginInfo = genericPluginRegistry.getPluginInfo(decodedData.params[5].value);
-        if (genericPluginInfo){
+        if (genericPluginInfo) {
           pluginName = genericPluginInfo.specs.name;
         } else {
           pluginName = "Blockchain Interaction";
         }
       }
-      else if (pluginName === "ContributionRewardExt"){
+      else if (pluginName === "ContributionRewardExt") {
         pluginName = decodedData.params[7].value; // Rewarder name
       }
-      else if (pluginName === "SchemeFactory"){
-        pluginName = "Plugin Manager";
+      else {
+        pluginName = PLUGIN_NAMES[pluginName as keyof typeof PLUGIN_NAMES];
+        if (pluginName === undefined) {
+          pluginName = "Unknown plugin name";
+        }
       }
+      genesisProtocolParams = mapVoteParamsToGenesisParams(votingParams);
     }
 
     const proposalSummaryClass = classNames({
@@ -93,114 +122,77 @@ class ProposalSummary extends React.Component<IProps, IState> {
         { proposalState.pluginToRemove !== NULL_ADDRESS && !isReplace ?
           <div>
             <span className={css.summaryTitle}>
-              <img src="/assets/images/Icon/delete.svg"/>&nbsp;
+              <img src="/assets/images/Icon/delete.svg" />&nbsp;
                   Remove Plugin&nbsp;
               <a href={linkToEtherScan(proposalState.pluginToRemove)} target="_blank" rel="noopener noreferrer">{pluginNameAndAddress(proposalState.pluginToRemove)}</a>
             </span>
-            { detailView ?
+            {detailView &&
               <div className={css.summaryDetails}>
-                <table><tbody>
-                  <tr>
-                    <th>
-                          Address:
-                      <a href={linkToEtherScan(proposalState.pluginToRemove)} target="_blank" rel="noopener noreferrer">
-                        <img src="/assets/images/Icon/Link-blue.svg"/>
+                <div className={css.detailsRowsContainer}>
+                  <div>Address:</div>
+                  <div>
+                    <a href={linkToEtherScan(proposalState.pluginToRemove)} target="_blank" rel="noopener noreferrer" style={{ marginRight: "5px" }}>
+                      <img src="/assets/images/Icon/Link-blue.svg" />
+                    </a>
+                    {proposalState.pluginToRemove}
+                  </div>
+                </div>
+              </div>}
+          </div>
+          : pluginName &&
+          <div>
+            <span className={css.summaryTitle}>
+              <b className={css.pluginRegisterIcon}>{isReplace ? <img src="/assets/images/Icon/edit-sm.svg" /> : "+"}</b>&nbsp;
+              {isReplace ? "Replace" : "Add"} Plugin&nbsp;
+              {isReplace ? pluginNameAndAddress(proposalState.pluginToRemove) : pluginName}
+            </span>
+            {detailView &&
+              <div className={css.summaryDetails}>
+                <div className={css.detailsRowsContainer}>
+                  {isReplace && <React.Fragment>
+                    <div>Address:</div>
+                    <div>
+                      <a href={linkToEtherScan(proposalState.pluginToRemove)} target="_blank" rel="noopener noreferrer" style={{ marginRight: "5px" }}>
+                        <img src="/assets/images/Icon/Link-blue.svg" />
                       </a>
-                    </th>
-                    <td>{proposalState.pluginToRemove}</td>
-                  </tr>
-                </tbody></table>
+                      {proposalState.pluginToRemove}
+                    </div>
+                  </React.Fragment>}
+                  <div>Name:</div>
+                  <div>{pluginName}</div>
+                  <div>Version:</div>
+                  <div>{proposalState.pluginToRegisterPackageVersion.join(".")}</div>
+                  <div>Init Calldata:</div>
+                  <div>
+                    {proposalState.pluginToRegisterData.substr(0, 10)}...
+                    <img className={css.copyButton} src="/assets/images/Icon/Copy-blue.svg" onClick={this.copyToClipboardHandler(proposalState.pluginToRegisterData)} />
+                  </div>
+                  <div>Permissions:</div>
+                  <div>
+                    {
+                      // eslint-disable-next-line no-bitwise
+                      permissions & 2 ? <div>Register other Plugins</div> : ""
+                    }
+                    {
+                      // eslint-disable-next-line no-bitwise
+                      permissions & 4 ? <div>Change constraints</div> : ""
+                    }
+                    {
+                      // eslint-disable-next-line no-bitwise
+                      permissions & 8 ? <div>Upgrade the controller</div> : ""
+                    }
+                    {
+                      // eslint-disable-next-line no-bitwise
+                      permissions & 16 ? <div>Call genericCall on behalf of</div> : ""
+                    }
+                    <div>Mint or burn reputation</div>
+                  </div>
+                  {renderGpParams(genesisProtocolParams)}
+                  {contractToCall && <React.Fragment><div>Contract to Call:</div><div>{contractToCall}</div></React.Fragment>}
+                </div>
               </div>
-              : ""
             }
           </div>
-          : pluginName ?
-            <div>
-              <span className={css.summaryTitle}>
-                <b className={css.pluginRegisterIcon}>{isReplace ? <img src="/assets/images/Icon/edit-sm.svg"/> : "+"}</b>&nbsp;
-                {isReplace ? "Replace" : "Add"} Plugin&nbsp;
-                {isReplace ? pluginNameAndAddress(proposalState.pluginToRemove) : pluginName}
-                {isReplace ? " With " + pluginName : ""}
-              </span>
-              { detailView ?
-                <div className={css.summaryDetails}>
-                  <table>
-                    <tbody>
-                      {isReplace ?
-                        <tr>
-                          <th>
-                                Address:
-                            <a href={linkToEtherScan(proposalState.pluginToRemove)} target="_blank" rel="noopener noreferrer">
-                              <img src="/assets/images/Icon/Link-blue.svg"/>
-                            </a>
-                          </th>
-                          <td>{proposalState.pluginToRemove}</td>
-                        </tr>
-                        : <></>}
-                      <tr>
-                        <th>Name:</th>
-                        <td>{pluginName}</td>
-                      </tr>
-                      <tr>
-                        <th>Version:</th>
-                        <td>{proposalState.pluginToRegisterPackageVersion.join(".")}</td>
-                      </tr>
-                      <tr>
-                        <th>Init Calldata:</th>
-                        <td>
-                          {proposalState.pluginToRegisterData.substr(0, 10)}...
-                          <img className={css.copyButton} src="/assets/images/Icon/Copy-blue.svg" onClick={this.copyToClipboardHandler(proposalState.pluginToRegisterData)} />
-                        </td>
-                      </tr>
-
-                      { votingParams && (
-                        <React.Fragment>
-                          <tr><th>Queued Vote Required:</th><td>{votingParams[0]}%</td></tr>
-                          <tr><th>Queued Vote Period Limit:</th><td>{votingParams[1]}</td></tr>
-                          <tr><th>Boosted Vote Period Limit:</th><td>{votingParams[2]}</td></tr>
-                          <tr><th>Pre-Boosted Vote Period Limit:</th><td>{votingParams[3]}</td></tr>
-                          <tr><th>Threshold Const:</th><td>{votingParams[4]}</td></tr>
-                          <tr><th>Quiet Ending Period:</th><td>{votingParams[5]}</td></tr>
-                          <tr><th>Proposing Reputation Reward:</th><td>{votingParams[6]}</td></tr>
-                          <tr><th>Voters Reputation Loss Ratio:</th><td>{votingParams[7]}%</td></tr>
-                          <tr><th>Minimum DAO Bounty:</th><td>{votingParams[8]}</td></tr>
-                          <tr><th>DAO Bounty Const:</th><td>{votingParams[9]}</td></tr>
-                          <tr><th>Activation Time:</th><td>{ moment.unix(votingParams[10]).format("YYYY-MM-DD HH:mm")}</td></tr>
-                          {contractToCall && <tr><th>Contract to Call:</th><td>{contractToCall}</td></tr>}
-                        </React.Fragment>)
-                      }
-                      <tr>
-                        <th>Permissions:</th>
-                        <td>
-                          {
-                            // eslint-disable-next-line no-bitwise
-                            permissions & 2 ? <div>Register other Plugins</div> : ""
-                          }
-                          {
-                            // eslint-disable-next-line no-bitwise
-                            permissions & 4 ? <div>Change constraints</div> : ""
-                          }
-                          {
-                            // eslint-disable-next-line no-bitwise
-                            permissions & 8 ? <div>Upgrade the controller</div> : ""
-                          }
-                          {
-                            // eslint-disable-next-line no-bitwise
-                            permissions & 16 ? <div>Call genericCall on behalf of</div> : ""
-                          }
-                          {
-                            <div>Mint or burn reputation</div>
-                          }
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-                : ""
-              }
-            </div>
-            :
-            ""
         }
       </div>
     );
