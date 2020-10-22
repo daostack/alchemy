25af2c56d5d2269355516f45352629d0f25c2421
Roie Natan <roie.natan@gmail.com>
2020-10-22 19:01:45 +0300

    Add Raw call data

--- src/components/Proposal/ProposalSummary/ProposalSummaryUnknownGenericPlugin.tsx
+++ src/components/Proposal/ProposalSummary/ProposalSummaryUnknownGenericPlugin.tsx
@@ -1,9 +1,11 @@
 import { IGenericPluginProposalState } from "@daostack/arc.js";
 import classNames from "classnames";
-import { linkToEtherScan, formatTokens, baseTokenName } from "lib/util";
+import { linkToEtherScan, formatTokens, baseTokenName, truncateWithEllipses } from "lib/util";
 import * as React from "react";
 import { IProfileState } from "reducers/profilesReducer";
 import * as css from "./ProposalSummary.scss";
+import CopyToClipboard from "components/Shared/CopyToClipboard";
+import i18next from "i18next";
 
 interface IProps {
   beneficiaryProfile?: IProfileState;
@@ -20,6 +22,15 @@ export default class ProposalSummary extends React.Component<IProps, IState> {
 
   constructor(props: IProps) {
     super(props);
+  }
+
+  private rawCallData(proposalState: IGenericPluginProposalState) {
+    return <>
+      <p>{i18next.t("Raw call data")}:</p>
+      <pre>
+        {truncateWithEllipses(proposalState.callData, 66)}<CopyToClipboard value={proposalState.callData} />
+      </pre>
+    </>;
   }
 
   public render(): RenderOutput {
@@ -46,6 +57,7 @@ export default class ProposalSummary extends React.Component<IProps, IState> {
             <pre><a href={linkToEtherScan(proposalState.contractToCall)} target="_blank" rel="noopener noreferrer">{proposalState.contractToCall}</a></pre>
             sending to contract:
             <pre className={sendsETH ? css.warning : ""}>{formatTokens(proposalState.value)} {baseTokenName()}</pre>
+            {this.rawCallData(proposalState)}
           </div>
           : ""
         }
