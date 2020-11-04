5cf56db7e180bd25633146114b4691ccae66b769
Oren Sokolowsky <oren@daostack.io>
2020-11-05 01:23:43 +0200

    graphql error handling with retry

--- src/arc.ts
+++ src/arc.ts
@@ -145,6 +145,23 @@ export async function initializeArc(provider?: any): Promise<boolean> {
 
     arcSettings.graphqlRetryLink = retryLink;
 
+    const graphqlErrHandler = (event) => {
+     if (event.graphQLErrors) {
+       event.graphQLErrors.map((err: any) =>
+         console.log(
+           `[graphql error]: message: ${err.message}, location: ${err.locations}, path: ${err.path}`
+         )
+       )
+       //// retry the request, returning the new observable
+       return event.forward(event.operation);
+     }
+     if (event.networkError) {
+       console.log(`[network error]: ${event.networkError}`)
+     }
+   }
+
+   arcSettings.graphqlErrHandler = graphqlErrHandler;
+
     // if there is no existing arc, we create a new one
     if ((window as any).arc) {
       arc = (window as any).arc;
