694ad005d61e8eeeb5962152d20d1bb0b3658ab4
Oren Sokolowsky <oren@daostack.io>
2020-12-21 02:18:30 +0200

    competition test

--- test/integration/proposal-competition.e2e.ts
+++ test/integration/proposal-competition.e2e.ts
@@ -9,15 +9,10 @@ describe("Proposals", () => {
 
   beforeEach(async () => {
     // we need to find a DAO with a competition scheme
-    // TODO: create a test_env with a nameed DAO so we can find it consistently
     arc = await getArc();
-    await arc.fetchContractInfos();
-    const ARC_VERSION = "0.0.1-rc.43";
-    const contributionRewardExtContract = await arc.getContractInfoByName("ContributionRewardExt", ARC_VERSION);
-
     // find the corresponding scheme object
     const contributionRewardExts = await arc
-      .schemes({where: {address: contributionRewardExtContract.address}}).pipe(first()).toPromise();
+      .schemes({where: {name: `ContributionRewardExt` }}).pipe(first()).toPromise();
 
     const contributionRewardExt = contributionRewardExts[0];
     const contributionRewardExtState = await contributionRewardExt.state().pipe(first()).toPromise();
