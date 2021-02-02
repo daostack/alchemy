dc140a51af9d2e5677fd632c99ec81d25e48de7f
roienatan <34843014+roienatan@users.noreply.github.com>
2021-02-02 23:50:40 +0200

    Better approach to calculate a proposal status (#2408)
    * better approach to calculate a proposal status
    
    * another condition to Failed status
    
    * update changelog ; update 'Executed' condition
    
    * update subgraph v49 ; update changelog

--- src/lib/proposalHelpers.ts
+++ src/lib/proposalHelpers.ts
@@ -85,29 +85,29 @@ export const closingTime = (proposal: IProposalState, proposalsPage?: boolean) =
  * @returns {ProposalStatus}
  */
 export const calculateProposalStatus = (proposal: IProposalState): IProposalStatus => {
-  const { winningOutcome, executedAt, boostedAt } = proposal;
-  const endDateMoment = moment(closingTime(proposal));
-  const now = new Date();
-  const complete = endDateMoment.diff(now) <= 0 ? true : false;
+  const { winningOutcome } = proposal;
+  const stage = castProposalStageToNumberRepresentation(String(proposal.stage));
 
-  if (String(winningOutcome) === "Pass") {
-    if (!boostedAt && !complete) {
-      return IProposalStatus.Failing;
+  if (stage === IProposalStage.ExpiredInQueue || (stage === IProposalStage.Queued && closingTime(proposal) <= moment())) {
+    return IProposalStatus.Failed;
+  }
+
+  if (stage === IProposalStage.Executed) {
+    if (proposal.genericSchemeMultiCall) {
+      if (!proposal.genericSchemeMultiCall.executed) {
+        return IProposalStatus.Executable;
+      }
     }
-    if (!complete) {
-      return IProposalStatus.Passing;
-    }
-    if (executedAt) {
+    if (String(winningOutcome) === "Pass") {
       return IProposalStatus.Executed;
-    }
-    return IProposalStatus.Executable;
-
-  } else {
-    if (!complete) {
-      return IProposalStatus.Failing;
     }
     return IProposalStatus.Failed;
   }
+
+  if (String(winningOutcome) === "Pass") {
+    return IProposalStatus.Passing;
+  }
+  return IProposalStatus.Failing;
 };
 
 export function proposalExpired(proposal: IProposalState) {
